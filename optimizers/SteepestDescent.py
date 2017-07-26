f74301826376fc313791d1654e030495f6404598
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-07-26 08:04:31 +0200

    made alpha an attribute

--- optimizers/SteepestDescent.py
+++ optimizers/SteepestDescent.py
@@ -4,11 +4,13 @@ from optimizers.Optimizer import Optimizer
 
 class SteepestDescent(Optimizer):
 
-    def __init__(self, geometry):
-        super(SteepestDescent, self).__init__(geometry)
+    def __init__(self, geometry, **kwargs):
+        self.alpha = -0.05
+        super(SteepestDescent, self).__init__(geometry, **kwargs)
+
+        assert(self.alpha < 0), "Alpha should be negative!"
 
     def optimize(self):
-        alpha = -0.05
-        step = alpha*self.forces[-1]
+        step = self.alpha*self.forces[-1]
         new_coords = self.geometry.coords + step
         self.geometry.coords = new_coords
