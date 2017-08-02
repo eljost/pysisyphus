e8a25d157ff3a8459424bde5038dd7b69bd94e3b
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-08-02 17:00:30 +0200

    global-bfgs seems to work for now

--- optimizers/BFGS.py
+++ optimizers/BFGS.py
@@ -7,35 +7,67 @@ from pysisyphus.optimizers.BacktrackingOptimizer import BacktrackingOptimizer
 class BFGS(BacktrackingOptimizer):
 
     def __init__(self, geometry, **kwargs):
-        super(BFGS, self).__init__(geometry, **kwargs)
+        super(BFGS, self).__init__(geometry, alpha=1.0, **kwargs)
 
+        self.reset_hessian()
+        self.eye = self.inv_hessian.copy()
+
+    def reset_hessian(self):
         self.inv_hessian = np.eye(self.geometry.coords.size)
 
     def optimize(self):
         # Calculate initial forces in the first iteration
         if len(self.forces) is 0:
+            last_coords = self.geometry.coords
             last_forces = self.geometry.forces
+            last_energy = self.geometry.energy
         else:
+            last_coords = self.coords[-1]
             last_forces = self.forces[-1]
+            last_energies = self.energies[-1]
 
         steps = np.dot(self.inv_hessian, last_forces)
         steps = self.scale_by_max_step(steps)
+        steps *= self.alpha
 
-        new_coords = self.coords + self.alpha * steps
-
-        #coords_hold = self.coords.copy()
-        #forces_hold = forces[-1].copy()
-
-        self.forces.append(self.geometry.forces)
-        self.energies.append(self.geometry.energy)
-        forces = self.forces[-1]
-
+        new_coords = last_coords + steps
+        self.geometry.coords = new_coords
 
         # !!!!!!!!!
         # fit_rigid
         # !!!!!!!!!
 
-        #import sys; sys.exit()
+        new_forces = self.geometry.forces
+        new_energy = self.geometry.energy
+        skip = self.backtrack(new_forces, last_forces)
 
+        if skip:
+            self.reset_hessian()
+            self.geometry.coords = last_coords
+            return None
+        else:
+            self.geometry.coords = last_coords
+            self.geometry.energy = new_energy
+
+            self.forces.append(new_forces)
+            sigma = new_coords - last_coords
+            forces_diff = -new_forces - (-last_forces)
+            rho = 1.0 / np.vdot(forces_diff, sigma)
+            if np.array_equal(self.inv_hessian, self.eye):
+                self.inv_hessian = (np.vdot(forces_diff, sigma) /
+                                    np.vdot(forces_diff, forces_diff) *
+                                    self.eye
+                )
+            # Inverse hessian update
+            A = (self.eye -
+                 sigma[:,np.newaxis] * forces_diff[np.newaxis,:] * rho
+            )
+            B = (self.eye -
+                 forces_diff[:,np.newaxis] * sigma[np.newaxis,:] * rho
+            )
+            self.inv_hessian = (
+                    np.dot(A, np.dot(self.inv_hessian, B)) +
+                    sigma[:,np.newaxis] * sigma[np.newaxis,:] * rho
+            )
 
         return steps
