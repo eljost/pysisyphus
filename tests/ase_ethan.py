bfbe15187370c7bad8f258cf3fcb205904f636e3
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-08-02 17:02:40 +0200

    added two test for orca using bfgs and fire

--- tests/ase_ethan.py
+++ tests/ase_ethan.py
@@ -1,9 +1,10 @@
 from ase import io
 from ase.calculators.orca import ORCA
 from ase.optimize.fire import FIRE
+from ase.optimize.bfgs import BFGS
 
 #Optimise molecule
-ethan = io.read('ethan.xyz')
+ethan = io.read('xyz_files/ethan.xyz')
 orca_calc= ORCA(
     label="orca",
     maxiter=200,
@@ -12,5 +13,7 @@ orca_calc= ORCA(
 )
 ethan.set_calculator(orca_calc)
 
-opt = FIRE(ethan, trajectory='ethan.traj')
-opt.run(fmax=0.00241)
+#opt = FIRE(ethan, trajectory='ethan.traj')
+#opt.run(fmax=0.00241)
+opt = BFGS(ethan, trajectory='ethan.traj')
+opt.run(fmax=0.0005)
