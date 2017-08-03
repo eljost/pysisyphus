7439d82a68a7fcbce574dbdb2a0f3cb33317bb34
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-08-03 13:56:10 +0200

    cleanup and changed the default value of k and made k an argument to the NEB class

--- cos/NEB.py
+++ cos/NEB.py
@@ -11,8 +11,10 @@ from pysisyphus.cos.ChainOfStates import ChainOfStates
 
 class NEB(ChainOfStates):
 
-    def __init__(self, images):
+    def __init__(self, images, k=0.01):
         super(NEB, self).__init__(images)
+
+        self.k = k
 
         self.perp_forces = list()
         self.par_forces = list()
@@ -24,16 +26,16 @@ class NEB(ChainOfStates):
         return np.array(par_forces).flatten()
 
     def get_parallel_forces(self, i):
-        k = 0.1
         if (i is 0) or (i is len(self.images) - 1):
-            return k * self.get_tangent(i)
+            return self.k * self.get_tangent(i)
 
-        prev_image = self.images[i-1].coords
-        image = self.images[i].coords
-        next_image = self.images[i+1].coords
-        return (k * (np.linalg.norm(next_image-image) -
-                np.linalg.norm(image-prev_image)) *
-                self.get_tangent(i)
+        prev_coords = self.images[i-1].coords
+        ith_coords = self.images[i].coords
+        next_coords = self.images[i+1].coords
+        return (self.k
+                * (np.linalg.norm(next_coords-ith_coords)
+                -  np.linalg.norm(ith_coords-prev_coords)
+                ) * self.get_tangent(i)
         )
 
     @property
@@ -46,13 +48,13 @@ class NEB(ChainOfStates):
         if self._forces is None:
             [image.calc_energy_and_forces() for image in self.images]
 
-        perp_forces = np.array(
-            [self.get_perpendicular_forces(i) for i in indices]
-        )
         par_forces = np.array(
             [self.get_parallel_forces(i) for i in indices]
         )
-        total_forces = perp_forces + par_forces
+        perp_forces = np.array(
+            [self.get_perpendicular_forces(i) for i in indices]
+        )
+        total_forces = par_forces + perp_forces
         self._forces = total_forces.flatten()
 
         return self._forces
