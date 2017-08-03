09a1f098d81e98cde28bc6b954b194b4c25a8793
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-08-03 14:08:17 +0200

    added base class for analyitcal 2d potentials that uses sympy

--- calculators/AnaPot3.py
+++ calculators/AnaPot3.py
@@ -1,36 +1,12 @@
-import numpy as np
-from sympy import symbols, diff, lambdify
-
-from pysisyphus.calculators.Calculator import Calculator
+from pysisyphus.calculators.AnaPotBase import AnaPotBase
 
 # [1] http://www.cims.nyu.edu/~eve2/string_jcp_simplified07.pdf
 
-class AnaPot3(Calculator):
+class AnaPot3(AnaPotBase):
 
     def __init__(self): 
-        super(AnaPot3, self).__init__()
-        x, y = symbols("x y")
-        V = (1 - x**2 - y**2)**2 + (y**2) / (x**2 + y**2)
-        dVdx = diff(V, x)
-        dVdy = diff(V, y)
-        self.V = lambdify((x, y), V, "numpy")
-        self.dVdx = lambdify((x, y), dVdx, "numpy")
-        self.dVdy = lambdify((x, y), dVdy, "numpy")
-
-    def get_energy(self, atoms, coords):
-        x, y, z = coords
-        energy = self.V(x, y)
-        return {"energy": energy}
-
-    def get_forces(self, atoms, coords):
-        x, y, z = coords
-        dVdx = self.dVdx(x, y)
-        dVdy = self.dVdy(x, y)
-        dVdz = np.zeros_like(dVdx)
-        forces = -np.array((dVdx, dVdy, dVdz))
-        results = self.get_energy(atoms, coords)
-        results["forces"] = forces
-        return results
+        V_str = "(1 - x**2 - y**2)**2 + (y**2) / (x**2 + y**2)"
+        super(AnaPot3, self).__init__(V_str=V_str)
 
     def __str__(self):
         return "AnaPot3 calculator"
---
+++ calculators/AnaPotBase.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sympy import symbols, diff, lambdify, sympify
+
+from pysisyphus.calculators.Calculator import Calculator
+
+class AnaPotBase(Calculator):
+
+    def __init__(self, V_str): 
+        super(AnaPotBase, self).__init__()
+        x, y = symbols("x y")
+        V = sympify(V_str)#(1 - x**2 - y**2)**2 + (y**2) / (x**2 + y**2)
+        dVdx = diff(V, x)
+        dVdy = diff(V, y)
+        self.V = lambdify((x, y), V, "numpy")
+        self.dVdx = lambdify((x, y), dVdx, "numpy")
+        self.dVdy = lambdify((x, y), dVdy, "numpy")
+
+    def get_energy(self, atoms, coords):
+        x, y, z = coords
+        energy = self.V(x, y)
+        return {"energy": energy}
+
+    def get_forces(self, atoms, coords):
+        x, y, z = coords
+        dVdx = self.dVdx(x, y)
+        dVdy = self.dVdy(x, y)
+        dVdz = np.zeros_like(dVdx)
+        forces = -np.array((dVdx, dVdy, dVdz))
+        results = self.get_energy(atoms, coords)
+        results["forces"] = forces
+        return results
