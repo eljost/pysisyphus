e730e00ee06685f4c12359598e81a41447c35f23
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2017-08-07 14:08:42 +0200

    further work

--- irc/IMKMod.py
+++ irc/IMKMod.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import warnings
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,64 +9,96 @@ from pysisyphus.calculators.MullerBrownPot import MullerBrownPot
 from pysisyphus.calculators.AnaPot import AnaPot
 from pysisyphus.Geometry import Geometry
 
-# http://pubs.acs.org/doi/pdf/10.1021/ja00295a002
+warnings.simplefilter('ignore', np.RankWarning)
 
-def imk_irc(geometry, desired_step=0.5, line_step_size=0.025):
-    all_irc_coords = [geometry.coords.copy(), ]
+# [1] http://pubs.acs.org/doi/pdf/10.1021/ja00295a002
 
-    for i in range(15):
-        print(f"ITER {i}")
+
+def parabolic_fit(xs, ys):
+    fit = np.polyfit(xs, ys, deg=2)
+    fit = np.poly1d(fit)
+    minima = fit.deriv().r
+    real_minima = minima[minima.imag==0].real
+
+    return real_minima
+
+
+def imk_mod(geometry, desired_step=0.15, line_step_size=0.025, fit_points=3):
+    all_irc_coords = list()
+
+    last_energy = None
+    i = 0
+    while True:
         # Get gradient at Q0
         grad_0 = -geometry.forces
+        energy_0 = geometry.energy
+
+        if last_energy and (energy_0 > last_energy):
+            print("Iteration {:04d}: Energy incresed!".format(i))
+            break
+
+        all_irc_coords.append(geometry.coords)
+
         grad_0_norm = np.linalg.norm(grad_0)
         step_size = np.linalg.norm(desired_step) / grad_0_norm
         step = -step_size * grad_0
-        print("step", step)
+
+        # Step to intermediate point Q1
         coords_1 = geometry.coords + step
-        #print(step)
-        # Step to intermediate point
         geometry.coords = coords_1
+
         # Get gradient at Q1
         grad_1 = -geometry.forces
-        print("grad_1", grad_1)
+        energy_1 = geometry.energy
+        grad_1_norm = np.linalg.norm(grad_1)
 
         # Determine bisector
-        print("g0_norm", grad_0_norm)
-        grad_1_norm = np.linalg.norm(grad_1)
-        print("g1_norm", grad_1_norm)
         D = grad_0/grad_0_norm - grad_1/grad_1_norm
-        print("D", D)
 
-        js = np.arange(7)
+        line_xs = [0, ]
+        line_energies = [energy_1, ]
+
+        line_step_size_thresh = 2*line_step_size
+        # Try to find a useful point by projecting grad_1 on D.
+        step_D1 = np.dot(grad_1, D) * D * line_step_size
+        step_D1_norm = np.linalg.norm(step_D1)
+        if step_D1_norm < line_step_size_thresh:
+            geometry.coords = coords_1 + step_D1
+            step_D1_energy = geometry.energy
+            line_xs.append(step_D1_norm)
+            line_energies.append(step_D1_energy)
+        # Otherwise just take a step along D.
+        else:
+            step_D2 = line_step_size * D
+            geometry.coords = coords_1 + step_D2
+            step_D2_norm = np.linalg.norm(step_D2)
+            line_xs.append(step_D2_norm)
+            step_D2_energy = geometry.energy
+            line_energies.append(step_D2_energy)
+
+        real_minimum = parabolic_fit(line_xs, line_energies)
+        print("real_minimum", real_minimum)
+        #if abs(real_minimum) > abs(line_step_size_thresh):
+
+        """
+        line_steps = [j * D for j in range(3)]
         line_energies = list()
-        # Store Q1 to be used in the linesearch
-        coords_backup = geometry.coords.copy()
-        for j in js:
-            line_step = j*line_step_size*D
-            line_search_coords = coords_backup + line_step
+        for line_step in line_steps:
+            line_search_coords = coords_1 + line_step
             geometry.coords = line_search_coords
             energy = geometry.energy
-            print("line_step", line_step)
-            print("j", j, "energy", energy)
             line_energies.append(energy)
+        line_xs = np.linalg.norm(line_steps, axis=-1)
         line_energies = np.array(line_energies)
-        fit = np.polyfit(js, line_energies, deg=2)
-        fit = np.poly1d(fit)
-        print("fit")
-        print(fit)
-        crit = fit.deriv().r
-        print("crit", crit)
-        r_crit = crit[crit.imag==0].real
-        print("r_crit", r_crit)
-        test = fit.deriv(2)(r_crit) 
-        print("test", test)
-        irc_coords = coords_backup + (r_crit[0]*line_step_size*D)
-        all_irc_coords.append(irc_coords)
+
+        real_minimum = parabolic_fit(line_xs, line_energies)
+        """
+
+        irc_coords = coords_1 + (real_minimum*line_step_size*D)
         geometry.coords = irc_coords
 
-        print("New_step", step_size)
-
-        print()
+        last_energy = energy_0
+        i += 1
 
     return np.array(all_irc_coords)
 
@@ -72,19 +106,19 @@ def imk_irc(geometry, desired_step=0.5, line_step_size=0.025):
 def run():
     #ts_coords = np.array((-0.822, 0.624, 0.))
     atoms = ("H", )
-    #calc, ts_coords = (MullerBrownPot(), np.array((-0.845041, 0.663752, 0.)))
-    #xlim = (-1.75, 1.25)
-    #ylim = (-0.5, 2.25)
-    #levels=(-150, -15, 40)
+    calc, ts_coords = (MullerBrownPot(), np.array((-0.845041, 0.663752, 0.)))
+    xlim = (-1.75, 1.25)
+    ylim = (-0.5, 2.25)
+    levels=(-150, -15, 40)
 
-    xlim = (-2, 2.5)
-    ylim = (0, 5)
-    levels = (-3, 4, 80)
-    calc, ts_coords = (AnaPot(), np.array((0.6906, 1.5491, 0.)))
+    #xlim = (-2, 2.5)
+    #ylim = (0, 5)
+    #levels = (-3, 4, 80)
+    #calc, ts_coords = (AnaPot(), np.array((0.6906, 1.5491, 0.)))
     geometry = Geometry(atoms, ts_coords)
     geometry.set_calculator(calc)
 
-    aic = imk_irc(geometry)
+    aic = imk_mod(geometry)
 
     fig, ax = plt.subplots(figsize=(8,8))
 
