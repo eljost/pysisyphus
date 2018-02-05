1bec714a11a0962517eb1adb006637b68f35aa3f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-02-05 17:12:24 +0100

    hacky recognition of dihedrals

--- pysisyphus/InternalCoordinates.py
+++ pysisyphus/InternalCoordinates.py
@@ -35,50 +35,77 @@ def get_bond_indices(geom, factor=1.3):
     bond_flags = cdm <= cov_rad_sums
     bond_indices = atom_indices[bond_flags]
     logging.warning("No check for hydrogen bonds or disconnected fragments!")
+    # Set of sets with frozenset?
     return bond_indices
 
 
+def sort_by_central(set1, set2):
+    """Determines a common index in two sets and returns a length 3
+    tuple with the central index at the middle position and the two
+    terminal indices as first and last indices."""
+    central_set = set1 & set2
+    union = set1 | set2
+    assert len(central_set) == 1
+    terminal1, terminal2 = union - central_set
+    (central, ) = central_set
+    return (terminal1, central, terminal2), central
+
+
 def get_bending_indices(bond_indices):
-    #for bi1 in bond_indices:
+    bond_sets = {frozenset(bi) for bi in bond_indices}
     bending_indices = list()
-    for i in range(len(bond_indices)):
-        #for bi2 in bond_indices:
-        for j in range(i, len(bond_indices)):
-            bi1 = bond_indices[i]
-            bi2 = bond_indices[j]
-            bond_set1 = set(bi1)
-            bond_set2 = set(bi2)
-            union = bond_set1 | bond_set2
-            if len(union) == 3:
-                central_set = bond_set1 & bond_set2
-                assert len(central_set) == 1
-                terminal1, terminal2 = union - central_set
-                (central, ) = central_set
-                bending_indices.append((terminal1, central, terminal2))
-    logging.warning("No check for (nearly) linear angles!")
+    for bond_set1, bond_set2 in itertools.combinations(bond_sets, 2):
+        union = bond_set1 | bond_set2
+        if len(union) == 3:
+            as_tpl, _ = sort_by_central(bond_set1, bond_set2)
+            bending_indices.append(as_tpl)
+    logging.warning("No check for (nearly) linear angles!"
+                    "No additional orthogonal bending coordinates.")
     return np.array(bending_indices, dtype=int)
+
+
+def get_dihedral_indices(bond_inds, bend_inds):
+    dihedral_inds = list()
+    dihedral_sets = list()
+    for bond, bend in itertools.product(bond_inds, bend_inds):
+        central = bend[1]
+        bes = set((bend[0], bend[2]))
+        bois = set(bond)
+        if (len(bes & bois) == 1) and (central not in bois):
+            (intersect,)  = set(bond) & set(bend)
+            intersect_ind = list(bond).index(intersect)
+            term_ind = 0 if intersect_ind else 1
+            terminal = bond[term_ind]
+            if intersect == bend[0]:
+                dihedral_ind = [terminal] + list(bend)
+            else:
+                dihedral_ind = list(bend) + [terminal]
+            dihedral_set = set(dihedral_ind)
+            if dihedral_set not in dihedral_sets:
+                dihedral_inds.append(dihedral_ind)
+                dihedral_sets.append(dihedral_set)
+    logging.warning("No check for dihedrals near 180° or -180°!")
+    return np.array(dihedral_inds)
+
+
 
 if __name__ == "__main__":
     h2o_geom = geom_from_library("h2o.xyz") 
     h2o_inds = get_bond_indices(h2o_geom)
     assert len(h2o_inds) == 2
     h2o_bends = get_bending_indices(h2o_inds)
-    print(h2o_bends)
     assert len(h2o_bends) == 1
 
-    """
     benzene_geom = geom_from_library("benzene_bp86sto3g_opt.xyz")
     benzene_inds = get_bond_indices(benzene_geom)
     assert len(benzene_inds) == 12
     benzene_bends = get_bending_indices(benzene_inds)
-    print(benzene_bends)
-    """
 
     # See [2] for geometry
     fluorethylene = geom_from_library("fluorethylene.xyz")
     fe_inds = get_bond_indices(fluorethylene)
     assert len(fe_inds) == 5
     fe_bends = get_bending_indices(fe_inds)
-    print(fe_bends+1, len(fe_bends))
     assert len(fe_bends) == 6
-
+    fe_dihedrals = get_dihedral_indices(fe_inds, fe_bends)
+    print(fe_dihedrals)
