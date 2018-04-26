6b2da4ccc399fc686aa88e057dee937fe9a918e0
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-04-26 11:56:44 +0200

    added a separate logger for WFOWrapper

--- pysisyphus/calculators/__init__.py
+++ pysisyphus/calculators/__init__.py
@@ -17,3 +17,11 @@ fmt_str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 formatter = logging.Formatter(fmt_str)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+logger = logging.getLogger("wfoverlap")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("wfoverlap.log", mode="w")
+fmt_str = "%(asctime)s - %(message)s"
+formatter = logging.Formatter(fmt_str)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
