8d6f2414101b61e91598da5c40ee42ca93e21f3f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-05-14 09:46:45 +0200

    test wfowrapper compare with orca and H2

--- tests_staging/test_orca_ch2s_wfo/test_orca_ch2s_wfo.py
+++ tests_staging/test_orca_ch2s_wfo/test_orca_ch2s_wfo.py
@@ -8,19 +8,42 @@ from pysisyphus.calculators.ORCA import ORCA
 
 np.set_printoptions(precision=4, suppress=True)
 GEOM = geom_from_library("ch2s_bp86def2sv_opt.xyz")
+#GEOM = geom_from_library("h2.xyz")
 
 
 def self_compare_base(geom, wfo_basis):
     calc_kwargs = {
         "keywords": "BP86 def2-SVP",
         "blocks": "%tddft nroots 2 maxdim 5 end",
-        "track": False,
+        "track": True,
         "wfo_basis": wfo_basis,
         "wfo_charge": 0,
     }
 
     orca = ORCA(**calc_kwargs)
     orca.run_calculation(geom.atoms, geom.coords)
+    # cis = "calculator_0.000.orca.cis"
+    # gbw = "calculator_0.000.orca.gbw"
+    # out = "calculator_0.000.orca.out"
+    # orca.cis = cis
+    # orca.gbw = gbw
+    # orca.out = out
+
+    orca.store_wfo_data(geom.atoms, geom.coords)
+    wfow = orca.wfow
+    wfow.compare(wfow)
+
+    # mo_coeffs = orca.parse_gbw(gbw)
+    # print(mo_coeffs)
+    # print(mo_coeffs.shape)
+    #mos = fake_turbo_mos(coeffs)
+    #orca.parse_mo_numbers(out)
+
+    # coeffs = orca.parse_cis(cis)
+    # print(coeffs)
+    # print(coeffs[0])
+    # print(coeffs.shape)
+
     #wfow = turbo.wfow
     #wfow.compare(wfow)
     #turbo.root = 0
