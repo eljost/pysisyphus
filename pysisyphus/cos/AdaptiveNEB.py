4b00b36fb5e59d3a2498476379b53550311e232f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-05-28 09:12:27 +0200

    added documentation

--- pysisyphus/cos/AdaptiveNEB.py
+++ pysisyphus/cos/AdaptiveNEB.py
@@ -7,14 +7,29 @@ from pysisyphus.cos.NEB import NEB
 
 # [1] https://aip.scitation.org/doi/pdf/10.1063/1.1495401
 
-# I need a way to add calculators to the new images ...
-# or just reuse the old ones?
-
 # See /scratch/projekte/biaryl/me_cn/cycloadd22/guess01/neb2
 
 class AdaptiveNEB(NEB):
 
     def __init__(self, images, adapt_fact=.25, adapt_between=1, **kwargs):
+        """Adaptive Nudged Elastic Band.
+
+        Parameters
+        ----------
+        images : list of Geometry objects
+            Images of the band.
+        adapt_fact : positive float
+            Factor that is used to decide wether to adapt. The inital
+            threshold is calculated by multiplying the RMS force of the
+            band with this factor. When the RMS of the force falls
+            below this threshold adaption takes place.
+        adapat_between : positive integer
+            Number of images to interpolate between the highest energy
+            image and its neighbours. The number of starting images
+            must be higher or equal then 2*adapt_between+3, as we
+            reuse/transfer the calculators from the starting images onto
+            the new ones.
+        """
         kwargs["fix_ends"] = True
         super().__init__(images, **kwargs)
 
@@ -26,18 +41,48 @@ class AdaptiveNEB(NEB):
 
 
     def rms(self, arr):
+        """Root mean square
+
+        Returns the root mean square of the given array.
+
+        Parameters
+        ----------
+        arr : iterable of numbers
+
+        Returns
+        -------
+        rms : float
+            Root mean square of the given array.
+        """
         return np.sqrt(np.mean(np.square(arr)))
 
     def update_adapt_thresh(self, forces):
-        """Base on RMS forces."""
+        """Update the adaption threshold.
+
+        Parameters
+        ----------
+        forces : np.array
+            Forces of the previous optimization cycle.
+        """
         old_thresh = self.adapt_thresh
 
         self.adapt_thresh = self.rms(forces) * self.adapt_fact
         self.log(f"Updating adapt_thres. Old thresh was {old_thresh:}. "
                  f"New threshold is {self.adapt_thresh:.03f}")
-        self.level += 1
 
     def adapt_this_cycle(self, forces):
+        """Decide wether to adapt.
+
+        Parameters
+        ----------
+        forces : np.array
+            Forces of the previous optimization cycle.
+
+        Returns
+        -------
+        adapt : bool
+            Flag that indicates if adaption should take place in this cycle.
+        """
         cur_rms = self.rms(forces)
         adapt = cur_rms <= self.adapt_thresh
         self.log(f"Current RMS of forces is {cur_rms:03f}. Current thresh "
@@ -45,41 +90,51 @@ class AdaptiveNEB(NEB):
         return adapt
 
     def prepare_opt_cycle(self, *args, **kwargs):
+        """Check for adaption and adapt if needed.
+
+        See ChainOfStates.prepare_opt_cycle for a complete docstring.
+        """
         super().prepare_opt_cycle(*args, **kwargs)
 
-        # Check if track = True
-        img = self.images[0]
-        if hasattr(img, "track") and (img.track == True):
+        # Transferring Calculators including WFOWrapper objects
+        # in excited state calculations may be problematic.
+        # A problem would be the interpolation between two images with
+        # different roots. What root should be taken for the interpolated
+        # image? Additionally we probably would have to shift around the
+        # iterations stored in the WFOWrapper.
+        img0 = self.images[0]
+        if hasattr(img0, "track") and (img0.track == True):
             raise Exception("track = True and interpolating new images "
                 "may give problems with excited state tracking, so this "
                 "is disabled for now.")
 
+        # Initialize adapt_thresh
         if not self.adapt_thresh:
             self.update_adapt_thresh(self.forces_list[-1])
 
         if not self.adapt_this_cycle(self.forces_list[-1]):
             return
 
+        # Adapation from here on
+        # Determine highest energy index and image (HEI)
         hei_index = self.get_hei_index(self.energies_list[-1])
+        self.log(f"Index of highest energy image is {hei_index}")
         if (hei_index == 0) or (hei_index == len(self.images)-1):
-            raise Exception("Cant adapt, HEI is first or last!")
-
+            self.log("Cant adapt, HEI is first or last!")
+            return False
         prev_index = hei_index - 1
         next_index = hei_index + 1
         prev_image = self.images[prev_index]
         hei_image = self.images[hei_index]
         next_image = self.images[next_index]
 
-        self.log(f"Index of highest energy image is {hei_index}")
-        # Reset adapt_thresh so it will be set again in the beginning
-        # of the next iteration.
-        self.adapt_thresh = None
-
+        # Interpolation of new images between previous neighbour
+        # and the HEI.
         new_images_1 = self.interpolate_between(prev_index, hei_index,
                                                 self.adapt_between)
+        # Between next neighbour and the HEI.
         new_images_2 = self.interpolate_between(hei_index, next_index,
                                                 self.adapt_between)
-
         all_new_images = ([prev_image] + new_images_1
                           + [hei_image]
                           + new_images_2 + [next_image])
@@ -94,4 +149,9 @@ class AdaptiveNEB(NEB):
         for new_image, calc in zip(all_new_images, calcs):
             new_image.set_calculator(calc)
         self.images = all_new_images
+
+        # Reset adapt_thresh so it will be set again in the beginning
+        # of the next iteration.
+        self.adapt_thresh = None
+        self.level += 1
         return True
