de05eb7ed0919a425ccd85dee517be83f80469ca
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2018-10-10 09:37:16 +0200

    added warning when config file couldn't be found

--- pysisyphus/config.py
+++ pysisyphus/config.py
@@ -3,4 +3,7 @@ import pathlib
 
 home = pathlib.Path.home()
 Config = configparser.ConfigParser()
-Config.read(home / ".pysisyphusrc")
+config_fn = home / ".pysisyphusrc"
+read_fns = Config.read(config_fn)
+if read_fns == []:
+    print(f"Couldn't read configuration file. Expected it at '{config_fn}'.")
