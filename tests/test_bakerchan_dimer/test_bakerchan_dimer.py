ffac11ad8a87f9eaf329a8110996529d8fede984
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-03-20 20:30:08 +0100

    tweaked dimer tests

--- tests/test_bakerchan_dimer/test_bakerchan_dimer.py
+++ tests/test_bakerchan_dimer/test_bakerchan_dimer.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 import os
+import sys
+import time
 
 import cloudpickle
 from natsort import natsorted
@@ -29,38 +31,19 @@ def make_N_init_dict():
     return N_dict
 
 
+def get_N_10_11_15_dict():
+    pickle_path = Path("/scratch/programme/pysisyphus/tests/test_bakerchan_dimer/10_11_N_init.pickle")
+    with open(pickle_path, "rb") as handle:
+        N_inits = cloudpickle.load(handle)
+    return N_inits
+
+
 def run():
+    start = time.time()
     np.random.seed(20180325)
     xyz_path = Path("/scratch/Code/parsezmat/")
     xyzs = natsorted(xyz_path.glob("*.xyz"))
     geoms = [geom_from_xyz_file(fn) for fn in xyzs]
-    baker_dict = {
-        "01_hcn.xyz" : (0, 1, -92.24604),
-        "02_hcch.xyz": (0, 1, -76.29343),
-        "03_h2co.xyz": (0, 1, -113.05003),
-        "04_ch3o.xyz": (0, 2, -113.69365),
-        "05_cyclopropyl.xyz": (0, 2, -115.72100),
-        "06_bicyclobutane.xyz": (0, 1, -153.90494),
-        "07_bicyclobutane.xyz": (0, 1, -153.89754),
-        "08_formyloxyethyl.xyz": (0, 2, -264.64757),
-        "09_parentdieslalder.xyz": (0, 1, -231.60321),
-        "10_tetrazine.xyz": (0, 1, -292.81026),
-        "11_trans_butadiene.xyz": (0, 1, -154.05046),
-        "12_ethane_h2_abstraction.xyz": (0, 1, -78.54323),
-        "13_hf_abstraction.xyz": (0, 1, -176.98453),
-        "14_vinyl_alcohol.xyz": (0, 1, -151.91310),
-        "15_hocl.xyz": (0, 1, -569.89752),
-        "16_h2po4_anion.xyz": (-1, 1, -637.92388),
-        "17_claisen.xyz": (0, 1, -267.23859),
-        "18_silyene_insertion.xyz": (0, 1, -367.20778),
-        "19_hnccs.xyz": (0, 1, -525.43040),
-        "20_hconh3_cation.xyz": (+1, 1, -168.24752),
-        "21_acrolein_rot.xyz": (0, 1, -189.67574),
-        "22_hconhoh.xyz": (0, 1, -242.25529),
-        "23_hcn_h2.xyz": (0, 1, -93.31114),
-        "24_h2cnh.xyz": (0, 1, -93.33296),
-        "25_hcnh2.xyz": (0, 1, -93.28172),
-    }
 
     baker_dict = {
         "01": ("01_hcn.xyz", 0, 1, -92.24604),
@@ -72,12 +55,12 @@ def run():
         "07": ("07_bicyclobutane.xyz", 0, 1, -153.89754),
         "08": ("08_formyloxyethyl.xyz", 0, 2, -264.64757),
         "09": ("09_parentdieslalder.xyz", 0, 1, -231.60321),
-        "10": ("10_tetrazine.xyz", 0, 1, -292.81026),
-        "11": ("11_trans_butadiene.xyz", 0, 1, -154.05046),
+        # "10": ("10_tetrazine.xyz", 0, 1, -292.81026),
+        # "11": ("11_trans_butadiene.xyz", 0, 1, -154.05046),
         "12": ("12_ethane_h2_abstraction.xyz", 0, 1, -78.54323),
         "13": ("13_hf_abstraction.xyz", 0, 1, -176.98453),
         "14": ("14_vinyl_alcohol.xyz", 0, 1, -151.91310),
-        "15": ("15_hocl.xyz", 0, 1, -569.89752),
+        # "15": ("15_hocl.xyz", 0, 1, -569.89752),
         "16": ("16_h2po4_anion.xyz", -1, 1, -637.92388),
         "17": ("17_claisen.xyz", 0, 1, -267.23859),
         "18": ("18_silyene_insertion.xyz", 0, 1, -367.20778),
@@ -91,12 +74,14 @@ def run():
     }
 
     dimer_kwargs = {
-        "max_step": 0.5,
+        "max_step": 0.1,
         # 1e-2 Angstroem in bohr
         "dR_base": 0.0189,
         "rot_opt": "lbfgs",
         # "trans_opt": "mb",
         "trans_opt": "lbfgs",
+        # "trans_memory": 10, # bad idea
+        "trans_memory": 3,
         "angle_tol": 5,
         "f_thresh": 1e-3,
         "max_cycles": 50,
@@ -106,6 +91,8 @@ def run():
     }
 
     N_init_dict = make_N_init_dict()
+    N_init_10_11_15 = get_N_10_11_15_dict()
+    N_init_dict.update(N_init_10_11_15)
     results_list = list()
     # for f, g in zip(xyzs, geoms):
     for num in baker_dict.keys():
@@ -125,8 +112,18 @@ def run():
             print(f"\t{f_}: {ts_en:.6f} ({den:.6f}), {conved}, {evals_}")
         print()
         print()
-        with open("results_list.pickle", "wb") as handle:
-            cloudpickle.dump(results_list, handle)
+        sys.stdout.flush()
+    results_list.append(dimer_kwargs)
+
+    dimer_kwargs["time"] = time.time()
+    hash_ = hash(frozenset(dimer_kwargs.items()))
+    pickle_fn = f"results_{hash_}.pickle"
+    with open(pickle_fn, "wb") as handle:
+        cloudpickle.dump(results_list, handle)
+    print(f"Save pickled results to {pickle_fn}.")
+    end = time.time()
+    duration = end - start
+    print(f"Whole run took {duration:.0f} seconds.")
 
 
 def run_geom(stem, geom, charge, mult, dimer_kwargs=None):
--- tests/test_dimer/test_dimer.py
+++ tests/test_dimer/test_dimer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import itertools as it
+from pprint import pprint
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -9,6 +10,7 @@ import numpy as np
 from pysisyphus.calculators.AnaPot import AnaPot
 from pysisyphus.calculators.AnaPotCBM import AnaPotCBM
 from pysisyphus.calculators.Gaussian16 import Gaussian16
+from pysisyphus.calculators.XTB import XTB
 from pysisyphus.Geometry import Geometry
 from pysisyphus.helpers import geom_from_library
 from pysisyphus.tsoptimizers.dimer import dimer_method
@@ -89,6 +91,25 @@ def test_anapot(trans_opt, trans_memory):
     return result
 
 
+def test_anapot_rot():
+    geoms = get_geoms()
+    calc_getter = AnaPot
+    dimer_kwargs = {
+        "ana_2dpot": True,
+        "restrict_step": "max",
+        "angle_tol": 0.5,
+        "f_thresh": 1e-4,
+        "rot_opt": "mb",
+        "f_tran_mod": False,
+        "rot_type": "direct",
+        # "rot_f_thresh": 0.1,
+        "multiple_translations": False,
+        # "max_cycles": 1,
+    }
+    result = dimer_method(geoms, calc_getter, **dimer_kwargs)
+    return result
+
+
 def anapot_tester():
     trans_opts = ("lbfgs", "mb")
     trans_memories = range(2, 8)
@@ -96,6 +117,7 @@ def anapot_tester():
     # trans_memories = (4, )
     results = dict()
     true_ts = (0.61173, 1.49297, 0.)
+    """
     ref_cycles = {
         'lbfgs_2': 9,
         'lbfgs_3': 9,
@@ -110,14 +132,30 @@ def anapot_tester():
         'mb_6': 5,
         'mb_7': 5,
     }
+    """
+    ref_force_evals = {
+        'lbfgs_2': 32,
+        'lbfgs_3': 32,
+        'lbfgs_4': 26,
+        'lbfgs_5': 26,
+        'lbfgs_6': 26,
+        'lbfgs_7': 26,
+        'mb_2': 21,
+        'mb_3': 21,
+        'mb_4': 21,
+        'mb_5': 21,
+        'mb_6': 21,
+        'mb_7': 21
+    }
+    result = dict()
     for to, tm in it.product(trans_opts, trans_memories):
         dimer_result = test_anapot(to, tm)
         key = f"{to}_{tm}"
-        results[key] = len(dimer_result.dimer_cycles)
+        result[key] = dimer_result.force_evals
         np.testing.assert_allclose(dimer_result.geom0.coords, true_ts, atol=1e-4)
-        assert ref_cycles[key] == len(dimer_result.dimer_cycles)
-    print(results)
+        assert dimer_result.force_evals == ref_force_evals[key]
     # plot_dimer_cycles(dimer_result.dimer_cycles, pot=AnaPot(), true_ts=true_ts[:2])
+    pprint(result)
 
 
 def test_anapot_cbm_rot():
@@ -260,9 +298,56 @@ def hcn_tester():
     print(results)
 
 
+def test_baker_16():
+    calc_kwargs = {
+        "route": "HF/3-21G",
+        "pal": 4,
+        "mem": 1000,
+        "charge": -1,
+        "mult": 1,
+    }
+    def calc_getter():
+        return Gaussian16(**calc_kwargs)
+        # return XTB(**calc_kwargs)
+
+    geom = geom_from_library("baker_ts/16_h2po4_anion.xyz")
+    geom.set_calculator(calc_getter())
+    geoms = [geom, ]
+
+    downhill = geom_from_library("baker_ts/16_h2po4_anion_downhill.xyz")
+    N_init = geom.coords - downhill.coords
+    N_init /= np.linalg.norm(N_init)
+
+    dimer_kwargs = {
+        "max_step": 0.5,
+        # 1e-2 Angstroem in bohr
+        "dR_base": 0.0189,
+        "rot_opt": "lbfgs",
+        "trans_opt": "lbfgs",
+        # "trans_opt": "mb",
+        # "trans_memory": 10,
+        "angle_tol": 5,
+        "f_thresh": 1e-3,
+        "max_cycles": 50,
+        "f_tran_mod": True,
+        "rot_type": "fourier",
+        "multiple_translations": True,
+        "N_init": N_init,
+        "max_cycles": 15,
+        "rot_f_thresh": 1.5e-3,
+    }
+
+    # 55 cycles; no multi trans, no f_tran_mod
+
+    # import pdb; pdb.set_trace()
+    dimer_result = dimer_method(geoms, calc_getter, **dimer_kwargs)
+    return dimer_result
+
 if __name__ == "__main__":
-    anapot_tester()
+    # test_anapot_rot()
+    # anapot_tester()
     # test_anapot_cbm_rot()
     # hcn_tester()
     # plot_anapotcbm_curvature()
     # test_anapotcbm()
+    test_baker_16()
