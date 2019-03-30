60f00b0b177aafbd70b153d721c0d95a3fb2d42a
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2019-03-30 21:36:30 +0100

    simplified optimier loggin output

--- pysisyphus/optimizers/__init__.py
+++ pysisyphus/optimizers/__init__.py
@@ -17,6 +17,7 @@ logger.setLevel(logging.DEBUG)
 # delay = True prevents creation of empty logfiles
 handler = logging.FileHandler("optimizer.log", mode="w", delay=True)
 fmt_str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+fmt_str = "%(message)s"
 formatter = logging.Formatter(fmt_str)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
