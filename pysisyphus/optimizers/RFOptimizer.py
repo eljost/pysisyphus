added much more logging. trust radius is only increased if step norm reaches 80% of trust radius. propably fixed calculation of reduction ratio

pysisyphus/optimizers/RFOptimizer.py
@@ -3,12 +3,14 @@
 # [1] http://aip.scitation.org/doi/10.1063/1.1515483 Optimization review
 # [2] https://doi.org/10.1063/1.450914 Trust region method
 # [3] 10.1007/978-0-387-40065-5 Numerical optimization
+# [4] 10.1007/s00214-016-1847-3 Explorations of some refinements
 
 
 import numpy as np
 from scipy.optimize import minimize
 
 from pysisyphus.optimizers.Optimizer import Optimizer
+
 
 class RFOptimizer(Optimizer):
 
@@ -18,12 +20,12 @@ class RFOptimizer(Optimizer):
         self.trust_radius = trust_radius
         self.min_trust_radius = 0.25*self.trust_radius
         self.trust_radius_max = 5*self.trust_radius
-        self.predicted_energies = list()
+        self.predicted_energy_changes = list()
+        self.actual_energy_changes = list()
 
         # self.hessians = list()
         self.trust_radii = list()
         self.rfo_steps = list()
-        self.predicted_energies = list()#[self.quadratic_approx(self.geometry.coords), ]
 
     def prepare_opt(self):
         self.H = self.geometry.get_initial_hessian()
@@ -47,6 +49,11 @@ class RFOptimizer(Optimizer):
         g = -self.forces[-1]
         return E0 + np.inner(g, step) + 0.5*step.dot(self.H.dot(step))
 
+    def predicted_change(self, step):
+        E0 = self.energies[-1]
+        g = -self.forces[-1]
+        return np.inner(g, step) + 0.5*step.dot(self.H.dot(step))
+
     def find_step(self, step_guess):
         ineq_fun = lambda step: self.trust_radius - np.linalg.norm(step)
         constr = {
@@ -58,7 +65,7 @@ class RFOptimizer(Optimizer):
                  method="SLSQP",
                  constraints=constr)
         step = res.x
-        self.log(f"LQA minimum: {res.fun:.6f}")
+        self.log(f"LQA minimum: {res.fun:.6f} au")
         self.log(f"Optimized step norm: {np.linalg.norm(step):.4f}")
         if not res.success:
             raise Exception("LQA optimization failed!")
@@ -67,19 +74,22 @@ class RFOptimizer(Optimizer):
     def update_trust_radius(self):
         # [3] Chapter 4, Algorithm 4.1
         actual_reduction = self.energies[-2] - self.energies[-1]
-        predicted_reduction = (self.predicted_energies[-2]
-                               - self.predicted_energies[-1])
-        reduction_ratio = actual_reduction / predicted_reduction
+        predicted = self.predicted_energy_changes[-1]
+        actual = self.actual_energy_changes[-1]
+        reduction_ratio = actual / predicted
+        self.log(f"Predicted energy reduction: {predicted:.4e} au")
+        self.log(f"Actual energy reduction: {actual:.4e} au")
         self.log(f"Reduction ratio: {reduction_ratio:.5f}")
+        last_step_norm = np.linalg.norm(self.steps[-1])
         if reduction_ratio < 0.25:
             self.trust_radius = max(0.25*self.trust_radius,
                                     self.min_trust_radius)
             self.log("Decreasing trust radius.")
-        elif reduction_ratio > 0.5:
+        # Only increase trust radius if last step norm was at least 80% of it
+        # See [4], Appendix, step size and direction control
+        elif reduction_ratio > 0.5 and (last_step_norm >= .8*self.trust_radius):
             self.trust_radius = min(2*self.trust_radius, self.trust_radius_max)
             self.log("Increasing trust radius.")
-        #if reduction_ratio < 0:
-        #    self.log("step rejected")
         self.log(f"Trust radius: {self.trust_radius:.3f}")
 
     def optimize(self):
@@ -87,12 +97,17 @@ class RFOptimizer(Optimizer):
         self.forces.append(-self.geometry.gradient)
         self.energies.append(self.geometry.energy)
 
-        predicted_energy = self.quadratic_approx(self.coords[-1])
-        self.predicted_energies.append(predicted_energy)
-
         if self.cur_cycle > 0:
-            self.bfgs_update()
+            # Predicted changes
+            # predicted_energy_change = self.quadratic_approx(self.steps[-1])
+            predicted_energy_change = self.predicted_change(self.steps[-1])
+            self.predicted_energy_changes.append(predicted_energy_change)
+            # Actual change
+            self.log(f"Last two energies: {self.energies[-2:]}")
+            actual_energy_change = self.energies[-1] - self.energies[-2]
+            self.actual_energy_changes.append(actual_energy_change)
             self.update_trust_radius()
+            self.bfgs_update()
 
         if self.geometry.internal:
             self.H = self.geometry.internal.project_hessian(self.H)
@@ -105,6 +120,8 @@ class RFOptimizer(Optimizer):
                      (gradient[None,:], [[0]]))
         )
         eigvals, eigvecs = np.linalg.eigh(aug_hess)
+        self.log(f"First 5 eigenvalues: {np.array2string(eigvals[:5], precision=2, suppress_small=True)}")
+        self.log(f"Lowest eigenvalue: {eigvals[0]:.6f}")
         # Select eigenvector corresponding to smallest eigenvalue.
         # As the eigenvalues are sorted in ascending order eigvals.argmin()
         # should always give 0...
@@ -113,7 +130,9 @@ class RFOptimizer(Optimizer):
         # aug_step is currently a matrix. Convert it to an array.
         aug_step = np.asarray(aug_step).flatten()
         # Scale aug_step so the last element equals 1
-        aug_step /= aug_step[-1]
+        lambda_ = aug_step[-1]
+        self.log(f"lambda: {lambda_:.6f}")
+        aug_step /= lambda_
         step = aug_step[:-1]
 
         self.keep()