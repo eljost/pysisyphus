579a4b22f145d9237ac0c969742da728f8d99e54
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-04-04 09:51:44 +0200

    some cleanup

--- pysisyphus/interpolate/LST.py
+++ pysisyphus/interpolate/LST.py
@@ -4,17 +4,12 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 from scipy.optimize import minimize
 
-from pysisyphus.calculators.LSTPot import LSTPot
-from pysisyphus.constants import BOHR2ANG, ANG2BOHR
-from pysisyphus.cos.ChainOfStates import ChainOfStates
 from pysisyphus.Geometry import Geometry
-from pysisyphus.helpers import fit_rigid, geom_from_xyz_file
 from pysisyphus.interpolate.Interpolator import Interpolator
 
 
-np.set_printoptions(suppress=True, precision=3)
-
-# https://www.sciencedirect.com/science/article/pii/S0927025603001113
+# [1] https://www.sciencedirect.com/science/article/pii/S0927025603001113
+# [2] https://pubs.acs.org/doi/pdf/10.1021/ct200654u
 
 
 class LST(Interpolator):
@@ -36,33 +31,35 @@ class LST(Interpolator):
 
     def interpolate(self, initial_geom, final_geom):
         coords3d = np.array((initial_geom.coords3d, final_geom.coords3d))
-        # coords3d *= BOHR2ANG
-
         # Calculate the condensed distances matrices
         pdists = [pdist(c3d) for c3d in coords3d]
 
         def rab_(f, pdist_r, pdist_p):
+            """Difference in internuclear distances."""
             return (1-f)*pdist_r + f*pdist_p
         rab = lambda f: rab_(f, pdists[0], pdists[1])
 
         def wab_(f, coords_r, coords_p):
+            """Difference in actual cartesian coordinates."""
             return (1-f)*coords_r + f*coords_p
         wab = lambda f: wab_(f, coords3d[0], coords3d[1])
         G = lambda w_c, f: self.cost_function(w_c, f, rab, wab)
 
         interpolated_geoms = list()
         x0_flat = wab(0).flatten()
+        minimize_kwargs = {
+            "method": "L-BFGS-B",
+            "options": {
+               "gtol": 1e-4,
+            }
+        }
         for f in np.linspace(0, 1, self.between):
             x0_flat = wab(f)
-            res = minimize(self.cost_function, x0=x0_flat, args=(f, rab, wab), method="BFGS",
-                           options={
-                               "gtol": 1e-4,
-                            }
-            )
+            res = minimize(self.cost_function, x0=x0_flat, args=(f, rab, wab),
+                           **minimize_kwargs)
             x0_flat = res.x
             print(f"{f:.04f}, success: {res.success}")
             interpolated_geoms.append(
-                # Geometry(self.atoms, res.x*ANG2BOHR)
                 Geometry(self.atoms, res.x)
             )
         return interpolated_geoms
