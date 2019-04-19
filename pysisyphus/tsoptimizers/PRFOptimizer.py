89d6d2be6ea87436b159189f06927d347e6c81f2
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2019-04-19 21:23:13 +0200

    addedd root following.

--- pysisyphus/tsoptimizers/PRFOptimizer.py
+++ pysisyphus/tsoptimizers/PRFOptimizer.py
@@ -12,16 +12,21 @@ from pysisyphus.optimizers.Optimizer import Optimizer
 
 class PRFOptimizer(Optimizer):
 
-    def __init__(self, geometry, root=-1, max_size=.3, recalc_hess=None,
+    def __init__(self, geometry, root=0, max_size=.3, recalc_hess=None,
                  **kwargs):
         super().__init__(geometry, **kwargs)
 
-        self.root = root
+        self.root = int(root)
         self.max_size = max_size
         self.recalc_hess = recalc_hess
 
+        self.H = None
+        self.ts_mode = None
+
     def prepare_opt(self):
         self.H = self.geometry.hessian
+        eigvals, eigvecs = np.linalg.eigh(self.H)
+        self.ts_mode = eigvecs[:,self.root]
 
     def bofill_update(self, H, dx, dg):
         dgHdx = dg - H.dot(dx)
@@ -60,35 +65,47 @@ class PRFOptimizer(Optimizer):
             self.log("Did Bofill hessian update.")
 
         eigvals, eigvecs = np.linalg.eigh(self.H)
-        neg_eigvals = eigvals < -1e-8
-        assert neg_eigvals.sum() >= 1, \
+        neg_eigval_inds = eigvals < -1e-8
+        neg_num = neg_eigval_inds.sum()
+        assert neg_num >= 1, \
             "Need at least 1 negative eigenvalue for TS optimization."
-        eigval_str = np.array2string(eigvals[neg_eigvals], precision=6)
-        self.log(f"Found {neg_eigvals.sum()} negative eigenvalue(s): {eigval_str}")
+        eigval_str = np.array2string(eigvals[neg_eigval_inds], precision=6)
+        self.log(f"Found {neg_num} negative eigenvalue(s): {eigval_str}")
+        # Select TS mode with biggest overlap to the previous TS mode
+        self.log("Overlaps of previous TS mode with current imaginary mode(s):")
+        ovlps = [imag_mode.dot(self.ts_mode) for imag_mode in eigvecs.T[:neg_num]]
+        for i, ovlp in enumerate(ovlps):
+            self.log(f"{i:02d}: {ovlp:.6f}")
+        max_ovlp_ind = np.argmax(ovlps)
+        max_ovlp = ovlps[max_ovlp_ind]
+        self.log(f"Highest overlap: {max_ovlp:.6f}, mode {max_ovlp_ind}")
+        self.log(f"Continuing with mode {max_ovlp_ind} as TS mode.")
+        self.root = max_ovlp_ind
+        self.ts_mode = eigvecs.T[max_ovlp_ind]
 
         # Transform to eigensystem of hessian
         forces_trans = eigvecs.T.dot(forces)
 
-        # Maximize energy along first root
-        mu = 0
-        max_mat = np.array(((eigvals[mu], -forces_trans[mu]),
-                           (-forces_trans[mu], 0)))
-        # Minimize energy along the other roots
+        # Maximize energy along the chosen TS mode
+        max_mat = np.array(((eigvals[self.root], -forces_trans[self.root]),
+                           (-forces_trans[self.root], 0)))
+        # Minimize energy along all modes, except the TS mode
+        min_indices = [i for i in range(forces.size) if i != self.root]
         min_mat = np.bmat((
-            (np.diag(eigvals[1:]), -forces_trans[1:,None]),
-            (-forces_trans[None,1:], [[0]])
+            (np.diag(eigvals[min_indices]), -forces_trans[min_indices,None]),
+            (-forces_trans[None,min_indices], [[0]])
         ))
 
         # Scale eigenvectors of the largest (smallest) eigenvector
         # of max_mat (min_mat) so the last item is 1.
-        max_evals, max_evecs = np.linalg.eigh(max_mat)
+        max_eigvals, max_evecs = np.linalg.eigh(max_mat)
         # Eigenvalues and -values are sorted, so we just use the last
         # eigenvector corresponding to the biggest eigenvalue.
         max_step = max_evecs.T[-1]
         lambda_max = max_step[-1]
         max_step = max_step[:-1] / lambda_max
 
-        min_evals, min_evecs = np.linalg.eigh(min_mat)
+        min_eigvals, min_evecs = np.linalg.eigh(min_mat)
         # Again, as everything is sorted we use the (smalelst) first eigenvalue.
         min_step = np.asarray(min_evecs.T[0]).flatten()
         lambda_min = min_step[-1]
@@ -96,8 +113,8 @@ class PRFOptimizer(Optimizer):
 
         # Create the full PRFO step
         prfo_step = np.zeros_like(forces)
-        prfo_step[0] = max_step[0]
-        prfo_step[1:] = min_step
+        prfo_step[self.root] = max_step[0]
+        prfo_step[min_indices] = min_step
         # Right now the step is still given in the Hessians eigensystem. We
         # transform it back now.
         step = eigvecs.dot(prfo_step)
