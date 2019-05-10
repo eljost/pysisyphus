d421fcd028dff17165b7226010c2429ec3e890e5
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-05-10 10:04:04 +0200

    added dampedBFGS. multi_step_hessian_update needs much work, as the calculation of the dx values is currently faulty.

--- pysisyphus/optimizers/hessian_updates.py
+++ pysisyphus/optimizers/hessian_updates.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-#     [1] https://link.springer.com/article/10.1007/s00214-016-1847-3
-#         Birkholz, 2016
+# [1] https://link.springer.com/article/10.1007/s00214-016-1847-3
+#     Birkholz, 2016
+# [2] Geometry optimization in Cartesian coordinates: Constrained optimization
+#     Baker, 1992
+# [3] https://epubs.siam.org/doi/pdf/10.1137/S1052623496306450
+#     BFGS WITH UPDATE SKIPPING AND VARYING MEMORY
+#     Kolda, 1998
 
 import numpy as np
 
@@ -12,6 +17,21 @@ def bfgs_update(H, dx, dg):
                   / dx.dot(H).dot(dx)
     )
     return first_term - second_term, "BFGS"
+
+
+def damped_bfgs_update(H, dx, dg):
+    dxdg = dx.dot(dg)
+    dxHdx = dx.dot(H).dot(dx)
+    theta = 1
+    if dxdg < 0.2*dxHdx:
+        theta = 0.8*dxHdx / (dxHdx - dxdg)
+    r = theta*dg + (1-theta)*H.dot(dx)
+
+    first_term = np.outer(r, r) / r.dot(dx)
+    second_term = (H.dot(np.outer(dx, dx)).dot(H)
+                  / dxHdx
+    )
+    return first_term - second_term, "damped BFGS"
 
 
 def sr1_update(z, dx):
@@ -68,3 +88,96 @@ def bofill_update(H, dx, dg):
     bofill_update = (mix * sr1) + (1 - mix)*(powell)
 
     return bofill_update
+
+
+"""
+def multi_step_update(H, coords, gradients, energies, last_cycles=3,
+                      key="flowchart"):
+    coords = np.array(coords)
+    gradients = np.array(gradients)
+    energies = np.array(energies)
+
+    # At the beginning there may not be enough cycles present.
+    # Substract 1 because N coords are produced from N-1 steps.
+    use_cycles = min(len(coords)-1, last_cycles)
+    funcs = {
+        "bfgs": bfgs_update,
+        "flowchart": flowchart_update,
+        "bofill": bofill_update, 
+    }
+    update_func = funcs[key]
+    steps = np.diff(coords, axis=0)[-last_cycles:]
+    grad_diffs = np.diff(gradients, axis=0)[-last_cycles:]
+    energy_diffs = np.diff(energies)[-last_cycles:]
+    # Exclude steps that yielded and increased energy
+    energy_lowered = energy_diffs < 0
+    # See [2] p. 247, step 9, regarding the hessian update.
+    # Exclude cycles that would lead to positive but small divisors
+    # and exclude cycles where the dot product is negative (gradient
+    # and step vector point in opposite directions, 'curvature conditon').
+    # The einsum call calcualtes the row-wise dot product
+    dgdx_big_enough = np.einsum("ij,ij->i", grad_diffs, steps) > 1e-8
+    use_cycles = energy_lowered & dgdx_big_enough
+    last_coord = coords[0]
+    last_grad = gradients[0]
+    for coord, grad in zip(coords, gradients):
+
+    # Recalculate dx and dg values because we maybe left out some cycles
+    dxs = list()
+    dgs = list()
+    for i, use in enumerate(use_cycles):
+        if use:
+            dxs.append(steps[-last_cycles+i])
+            dgs.append(grad_diffs[-last_cycles+i])
+    import pdb; pdb.set_trace()
+    grads_ = gradients[-last_cycles:][use_cycles]
+    coords_ = coords[-last_cycles:][use_cycles]
+    grad_diffs_ = np.diff(grads_, axis=0)
+    steps_ = np.diff(coords_, axis=0)
+    for dx, dg in zip(steps_, grad_diffs_):
+        dH = update_func(H, dx, dg)
+        H += dH
+    return H
+"""
+
+
+# def multi_step_update(H, coords, gradients, energies, last_cycles=3,
+                      # key="bfgs", ref=None, steps_ref=None):
+def multi_step_update(H, steps, gradients, energies, last_cycles=1, key="bfgs"):
+    last_cycles = min(len(steps), last_cycles)
+    funcs = {
+        "bfgs": bfgs_update,
+        "flowchart": flowchart_update,
+        "bofill": bofill_update, 
+    }
+    update_func = funcs[key]
+    # TODO: calculation of steps is wrong/not so easy ;)
+    steps_from_current = np.cumsum(steps, axis=0)
+    grad_diffs = gradients[-1] - np.array(gradients)[-last_cycles-1:-1]
+    energy_diffs = energies[-1] - np.array(energies)[-last_cycles-1:-1]
+    # Exclude steps that yielded and increased energy
+    energy_lowered = energy_diffs < 0
+    # See [2] p. 247, step 9, regarding the hessian update.
+    # Exclude cycles that would lead to positive but small divisors
+    # and exclude cycles where the dot product is negative (gradient
+    # and step vector point in opposite directions, 'curvature conditon').
+    # The einsum call calcualtes the row-wise dot product
+    # dgdx_big_enough = np.einsum("ij,ij->i", grad_diffs, steps) > 1e-8
+    # use_cycles = energy_lowered & dgdx_big_enough
+    # use_cycles = energy_lowered
+    dgdxs = np.einsum("ij,ij->i", grad_diffs, steps_from_current)
+    # dgdx_big_enough = np.einsum("ij,ij->i", grad_diffs, steps) > 1e-8
+    dgdx_big_enough = np.abs(dgdxs) > 1e-8
+    use_cycles = dgdx_big_enough
+    # print("Use cycles:", use_cycles)
+    H_updated = H.copy()
+    if len(gradients) == 3:
+        import pdb; pdb.set_trace()
+    for i, to_use in enumerate(use_cycles[::-1], 1):
+        if not to_use:
+            continue
+        dx = steps_from_current[-i]
+        dg = grad_diffs[-i]
+        dH, _ = update_func(H, dx, dg)
+        H_updated += dH
+    return H_updated
