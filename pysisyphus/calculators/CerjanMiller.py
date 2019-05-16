eb8b6ace39907453a02a36be4ae3527dd05f86b3
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-05-16 16:27:17 +0200

    started working on SphereOptimizer

---
+++ pysisyphus/calculators/CerjanMiller.py
@@ -0,0 +1,20 @@
+from pysisyphus.calculators.AnaPotBase import AnaPotBase
+
+
+class CerjanMiller(AnaPotBase):
+
+    def __init__(self, a=1, b=1, c=1): 
+        V_str = f"({a}-{b}*y**2)*x**2*exp(-x**2)+{c}/2*y**2"
+        xlim = (-1.3, 1.3)
+        ylim = (-0.7, 1.9)
+        super().__init__(V_str=V_str, xlim=xlim, ylim=ylim)
+
+    def __str__(self):
+        return "CerjanMiller calculator"
+
+
+if __name__ == "__main__":
+    cj = CerjanMiller()
+    cj.plot()
+    import matplotlib.pyplot as plt
+    plt.show()
