551c09d49dae38049399c21e398445b200ba4d07
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2019-05-16 21:49:29 +0200

    still no clue whats going on :)

--- pysisyphus/optimizers/SphereOptimizer.py
+++ pysisyphus/optimizers/SphereOptimizer.py
@@ -37,28 +37,30 @@ def run():
     import matplotlib.pyplot as plt
     from matplotlib.patches import Circle
     from pysisyphus.calculators.CerjanMiller import CerjanMiller
-    geom = CerjanMiller.get_geom((0.05, 0, 0))
-    import pdb; pdb.set_trace()
+    from pysisyphus.calculators.AnaPot import AnaPot
+    # geom = CerjanMiller.get_geom((0.00, 0, 0))
+    geom = CerjanMiller.get_geom((-0.15, 0, 0))
+    # geom = AnaPot.get_geom((-1, 1, 0))
 
     ref_ind = 1
-    ref_coords = np.array((0.05, 0, 0 ))
+    # ref_coords = geom.coords.copy()
+    ref_coords = np.array((-0.15, 0, 0))
     free_inds = np.arange(geom.coords.size) != ref_ind
     free_ref_coords = ref_coords[free_inds]
     ref_coord = ref_coords[ref_ind]
 
     def get_constr_coord(coords, radius):
         free_coords = coords[free_inds]
-        _ = np.sqrt(radius**2 - radius*np.sum((free_coords - free_ref_coords)**2))
-        return float(ref_coord + _)
+        _ = max(0, radius**2 - radius*np.sum((free_coords - free_ref_coords)**2))
+        sqrt = np.sqrt(_)
+        return ref_coord + sqrt
 
-    # for radius in (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7):
     c = list()
-    rads = np.array((0.1, 0.2))
-    rs = list()
-    dR = 0.02
-    alpha = 0.01
+    dR = 0.1
+    alpha = 0.1
     R = dR
-    for i in range(15):
+    rs = [R, ]
+    for i in range(6):
         c.append(geom.coords.copy())
         print(f"Cycle {i:02d}, R={R:.04f}")
         constr_coord = get_constr_coord(geom.coords, R)
@@ -66,13 +68,14 @@ def run():
         new_coords = geom.coords.copy()
         new_coords[ref_ind] = constr_coord
         geom.coords = new_coords
-        print("\t", geom.coords)
+        diff = R - np.linalg.norm(geom.coords - ref_coords)
+        print("\t coords", geom.coords)
         geom.clear()
 
         # Optimize remaining coordinates
         forces = geom.forces
+        print("\t forces", forces)
         norm_forces = np.linalg.norm(forces)
-        diff = R - np.linalg.norm(geom.coords - ref_coords)
         free_forces = forces[free_inds]
         norm_free_forces = np.linalg.norm(free_forces)
         # print(f"{i:02d}: {geom.coords} norm(f)={norm_forces:.6f} "
@@ -82,14 +85,15 @@ def run():
         if norm_free_forces < alpha:
             # Increase radius
             R += dR
+            rs.append(R)
             continue
 
         free_coords = geom.coords[free_inds]
         ref_force = forces[ref_ind]
         _ = free_forces - ref_force*(free_coords-free_ref_coords)/(constr_coord-ref_coord)
-        # dir_ = _ / np.linalg.norm(_)
-        # step = 0.01 * dir_
-        step = _
+        dir_ = _ / np.linalg.norm(_)
+        step = 0.01 * dir_
+        # step = _
         new_coords = geom.coords.copy()
         new_coords[free_inds] += step
         geom.coords = new_coords
@@ -105,7 +109,14 @@ def run():
         pot.ax.add_artist(circle)
     for i, xy in enumerate(c[:,:2]):
         pot.ax.annotate(i, xy)
+    # pot.ax.set_xlim(-0.2, 0.2)
+    # pot.ax.set_ylim(-0.2, 0.2)
     plt.show()
+
+    # from pysisyphus.calculators.AnaPot import AnaPot
+    # g = AnaPot.get_geom((-1, 1, 0))
+    # g.calculator.plot()
+    # plt.show()
 
 
 if __name__ == "__main__":
