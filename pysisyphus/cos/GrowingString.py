d613ddd4e2884ff02069d7840b858e66fc824f64
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-05-20 17:01:19 +0200

    refactored GrowingStrign so it can be used with pysisyphus optimizers

--- pysisyphus/cos/GrowingString.py
+++ pysisyphus/cos/GrowingString.py
@@ -2,34 +2,59 @@
 
 # See [1] 10.1063/1.1691018
 
-from copy import copy
-
 import numpy as np
 from scipy.interpolate import splprep, splev
 
+from pysisyphus.cos.ChainOfStates import ChainOfStates
 from pysisyphus.cos.GrowingChainOfStates import GrowingChainOfStates
 
 
 class GrowingString(GrowingChainOfStates):
 
-    def __init__(self, images, calc_getter, max_cycles=25, **kwargs):
+    def __init__(self, images, calc_getter, perp_thresh=0.05, **kwargs):
         assert len(images) >= 2, "Need at least 2 images for GrowingString."
-        if len(images) >= 2:
+        if len(images) > 2:
             images = [images[0], images[-1]]
             print("More than 2 images were supplied! Will only use the "
                   "first and last images to start the GrowingString."
             )
         super().__init__(images, calc_getter, **kwargs)
+        self.perp_thresh = perp_thresh
 
-        self.max_cycles = max_cycles
         left_img, right_img = self.images
 
         self.left_string = [left_img, ]
         self.right_string = [right_img, ]
 
+        self._tangents = None
         self.tangent_list = list()
-        self.perp_force_list = list()
+        self.perp_forces_list = list()
         self.coords_list = list()
+
+        # Adding nodes requires a direction/tangent. As we start
+        # from two images we can't do a cubic spline yet, so there are also
+        # no splined tangents.
+        # For the first two new nodes we use a "classic" tangent, the
+        # unit vector pointing from the left to the right image.
+        # As we this class calculates its own tangent as derivative of the
+        # spline we get the original tangent from the parent ChainOfStates class.
+        init_tangent = super().get_tangent(0)
+        Sk, _ = self.arc_dims
+        S = Sk / (self.max_nodes+1)
+        # Create first two mobile nodes
+        left_img, right_img = self.images
+        new_left_coords = left_img.coords + S*init_tangent
+        new_right_coords = right_img.coords - S*init_tangent
+        left_frontier = self.get_new_image(new_left_coords, 1)
+        self.left_string.append(left_frontier)
+        right_frontier = self.get_new_image(new_right_coords, 2)
+        self.right_string.append(right_frontier)
+
+        # Set initial tangents
+        self.set_tangents()
+        # The desired spacing of the nodes in the final string on the
+        # normalized arclength.
+        self.sk = 1 / (self.max_nodes+1)
 
     @property
     def left_size(self):
@@ -62,6 +87,7 @@ class GrowingString(GrowingChainOfStates):
         reshaped = self.coords.reshape(-1, self.coords_length)
         # To use splprep we have to transpose the coords.
         transp_coords = reshaped.transpose()
+        # Spline in batches as scipy can't handle > 11 rows at once
         tcks, us = zip(*[splprep(transp_coords[i:i+9], s=0, k=3)
                          for i in range(0, len(transp_coords), 9)]
         )
@@ -74,82 +100,77 @@ class GrowingString(GrowingChainOfStates):
         new_points = new_points.reshape(-1, len(self.images))
         self.coords = new_points.transpose().flatten()
 
-    def run(self):
-        # To add nodes we need a direction/tangent. As we start
-        # from two images we can't do a cubic spline yet, so we
-        # can't use splined tangents.
-        # To start off we use a "classic" tangent instead, that
-        # is the unit vector pointing from the left image, to
-        # the right image.
-        init_tangent = super().get_tangent(0)
-        Sk, _ = self.arc_dims
-        S = Sk / (self.max_nodes+1)
-        # Create first two mobile nodes
-        left_img, right_img = self.images
-        new_left_coords = left_img.coords + S*init_tangent
-        new_right_coords = right_img.coords - S*init_tangent
-        left_frontier = self.get_new_image(new_left_coords, 1)
-        self.left_string.append(left_frontier)
-        right_frontier = self.get_new_image(new_right_coords, 2)
-        self.right_string.append(right_frontier)
+    def set_tangents(self):
+        """This method may be considerd hacky as calculates all
+        tangents at once, and not one-by-one as the parent class
+        implementation. Right now one must not forget to call this method at
+        after the coordinates were modified somehow, e.g. after
+        reparametrization. Otherwise some wrong old tangets are used.
+        """
 
-        def ind_func(perp_force, tol=0.5):
-            return int(np.linalg.norm(perp_force) <= tol)
-
-        # Step length on the normalized arclength
-        sk = 1 / (self.max_nodes+1)
+        self.log("Setting tangents using hacky method :)")
         tcks, us = self.spline()
-        for self.cur_cycle in range(self.max_cycles):
-            Sk, cur_mesh = self.arc_dims
-            print(f"Cycle {self.cur_cycle:03d}, total arclength Sk={Sk:.4f}")
-            forces = self.forces
-            self.cur_forces = forces.reshape(-1, 3)
-            tangents = np.vstack([splev(cur_mesh, tck, der=1) for tck in tcks]).T
-            norms = np.linalg.norm(tangents, axis=1)
-            tangents = tangents / norms[:,None]
-            # Tangents of the right string shall point towards the center.
-            tangents[self.rf_ind:] *= -1
-            self.tangents = tangents
-            self.tangent_list.append(self.tangents)
+        Sk, cur_mesh = self.arc_dims
+        self.log(f"Total arclength Sk={Sk:.4f}")
+        tangents = np.vstack([splev(cur_mesh, tck, der=1) for tck in tcks]).T
+        norms = np.linalg.norm(tangents, axis=1)
+        tangents = tangents / norms[:,None]
+        # Tangents of the right string shall point towards the center, so
+        # we reverse their orientation.
+        tangents[self.rf_ind:] *= -1
+        self._tangents = tangents
 
-            # Perpendicular force component
-            # Dot product between rows in one line
-            # np.einsum("ij,ij->i", tangents, forces.reshape(-1, 3))
-            force_per_img = forces.reshape(-1, 3)
-            self.perp_forces = np.array(
-                [force - force.dot(tangent)*tangent
-                 for force, tangent in zip(force_per_img, tangents)]
-            )
-            self.perp_force_list.append(self.perp_forces)
-            perp_norms = np.linalg.norm(self.perp_forces, axis=1)
+    def get_tangent(self, i):
+        return self._tangents[i]
 
-            # Take step
-            step = 0.05 * self.perp_forces.flatten()
-            step_norms = np.linalg.norm(step.reshape(-1, 3), axis=1)
-            self.coords += step
-            self.coords_list.append(self.coords)
+    @ChainOfStates.forces.getter
+    def forces(self):
+        if self._forces is None:
+            self.calculate_forces()
+
+        indices = range(len(self.images))
+        perp_forces = [self.get_perpendicular_forces(i) for i in indices]
+        self._forces = np.array(perp_forces).flatten()
+        self.perp_forces_list.append(self._forces.copy())
+        # TODO: Add climbing modification
+        # total_forces = self.set_climbing_forces(total_forces)
+
+        return self._forces
+
+    def reparametrize(self):
             # Spline displaced coordinates
             tcks, us = self.spline()
 
+            # Calculate the norm of the perpendicular force for every
+            # node/image on the string.
+            perp_forces  = self.perp_forces_list[-1].reshape(-1, len(self.images))
+            perp_norms = np.linalg.norm(perp_forces, axis=1)
+            # We can add a new node if the norm of the perpendicular force
+            # on the frontier node(s) is below a threshold.
+            def converged(i):
+                return perp_norms[i] <= self.perp_thresh
+            
             # Check if we can add new nodes
-            if self.images_left and ind_func(self.perp_forces[self.lf_ind]):
+            if self.images_left and converged(self.lf_ind):
                 # Insert at the end of the left string, just before the 
                 # right frontier node.
                 new_left_frontier = self.get_new_image(self.dummy_coords, self.rf_ind)
                 self.left_string.append(new_left_frontier)
-                print("Added new left frontier node.")
-            if self.images_left and ind_func(self.perp_forces[self.rf_ind]):
+                self.log("Added new left frontier node.")
+            if self.images_left and converged(self.rf_ind):
                 # Insert at the end of the right string, just before the 
                 # current right frontier node.
                 new_right_frontier = self.get_new_image(self.dummy_coords, self.rf_ind)
                 self.right_string.append(new_right_frontier)
-                print("Added new right frontier node.")
+                self.log("Added new right frontier node.")
 
-            print("Current string size:", self.left_size, "+", self.right_size)
+            self.log(f"Current string size: {self.left_size}+{self.right_size}")
             # Reparametrize nodes
             left_inds = np.arange(self.left_size)
             right_inds = np.arange(self.max_nodes+2)[-self.right_size:]
             param_inds = np.concatenate((left_inds, right_inds))
-            param_density = sk*param_inds
-            print("New param density", np.array2string(param_density, precision=2))
+            param_density = self.sk*param_inds
+            self.log(f"New param density: " + np.array2string(param_density, precision=2))
             self.reparam(tcks, param_density)
+
+            self.set_tangents()
