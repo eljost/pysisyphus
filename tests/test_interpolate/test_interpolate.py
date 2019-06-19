e5ad72733292123161d4850986202eddfe4d41ce
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-06-19 17:52:31 +0200

    further work on IC interpolation

--- tests/test_interpolate/test_interpolate.py
+++ tests/test_interpolate/test_interpolate.py
@@ -48,8 +48,19 @@ def test_redund():
     # initial = geom_from_xyz_file("min115.xyz", coord_type="redund")
     # final = geom_from_xyz_file("plu115.xyz", coord_type="redund")
 
-    initial = geom_from_xyz_file("hcn.xyz", coord_type="redund")
-    final = geom_from_xyz_file("nhc.xyz", coord_type="redund")
+    # initial = geom_from_xyz_file("hcn.xyz", coord_type="redund")
+    # final = geom_from_xyz_file("nhc.xyz", coord_type="redund")
+
+    # initial = geom_from_xyz_file("h2_sih2_start.xyz", coord_type="redund")
+    # final = geom_from_xyz_file("h2_sih2_end.xyz", coord_type="redund")
+
+    # initial = geom_from_library("dipeptide_init.xyz", coord_type="redund")
+    # final = geom_from_library("dipeptide_fin.xyz", coord_type="redund")
+
+    initial = geom_from_xyz_file("butadiene_ethene.xyz", coord_type="redund")
+    final = geom_from_xyz_file("cyclohexene.xyz", coord_type="redund")
+    print("initial primitives", initial.coords.size)
+    print("final primitives", final.coords.size)
 
     def to_set(iterable):
         return set([tuple(_) for _ in iterable])
@@ -82,6 +93,7 @@ def test_redund():
     valid_bends = bends1 & bends2
     valid_dihedrals = dihedrals1 & dihedrals2
     prim_indices = (valid_bonds, valid_bends, valid_dihedrals)
+    print("union of primitives", len(valid_bonds) + len(valid_bends) + len(valid_dihedrals))
 
     geom1 = Geometry(initial.atoms, initial.cart_coords,
                      coord_type="redund", prim_indices=prim_indices
@@ -109,6 +121,16 @@ def test_redund():
     coords2_ = update_internals(geom1.coords, geom2.coords, bonds_bends, d)
     d_ = coords2_ - geom1.coords
     num = 20
+    bl = len(valid_bonds)
+    ba = len(valid_bends)
+    bd = len(valid_dihedrals)
+    bo1 = geom1.coords[:bl]
+    bo2 = coords2_[:bl]
+    be1 = geom1.coords[bl:bl+ba]
+    be2 = coords2_[bl:bl+ba]
+    d1 = geom1.coords[bl+ba:]
+    d2 = coords2_[bl+ba:]
+    import pdb; pdb.set_trace()
     base_step = d_ / (num-1)
     # import pdb; pdb.set_trace()
     geoms = [geom1, ]
@@ -117,12 +139,18 @@ def test_redund():
     for i in range(num):
         print(i)
         # step = base_step
+        # if i == 11:
+            # import pdb; pdb.set_trace()
         new_geom = geoms[-1].copy()
-        new_coords = new_geom.coords + base_step
+        try:
+            new_coords = new_geom.coords + base_step
+        except ValueError:
+            import pdb; pdb.set_trace()
         new_geom.coords = new_coords
         # print(i, new_coords)
         geoms.append(new_geom)
         write_geoms_to_trj(geoms, f"redund_{i:02d}.trj")
+    write_geoms_to_trj(geoms, f"liiic_{i:02d}.trj")
 
 
 if __name__ == "__main__":
