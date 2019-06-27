1c2efed23645a49f6dca08eeb22a6956179f0cf9
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-06-27 12:14:46 +0200

    number of calculated roots is now adaptive for TDDFT/TDA

--- pysisyphus/calculators/Turbomole.py
+++ pysisyphus/calculators/Turbomole.py
@@ -193,12 +193,20 @@ class Turbomole(OverlapCalculator):
             td_vec_fn = self.td_vec_fn.suffix[1:]
             shutil.copy(self.td_vec_fn, path / td_vec_fn)
             self.log(f"Using '{self.td_vec_fn}' as escf guess.")
-        root_log_msg = "with current root information."
+        root_log_msg = "with current root information: {self.root}"
         if self.root and self.td:
             repl = f"$exopt {self.root}"
             self.sub_control("\$exopt\s*(\d+)", f"$exopt {self.root}",
                              root_log_msg)
             self.log(f"Using '{repl}'")
+
+            # Adapt number of roots
+            roots_number_repl = "\$soes\s+a\s+(\d+)"
+            self.sub_control(roots_number_repl,
+                             f"$soes\n a {self.roots_number}",
+                              "with number of roots to be calculated: "
+                             f"{self.roots_number}"
+            )
         if self.root and self.ricc2:
             repl = f"state=(a {self.root})"
             log_msg = " with current root."
