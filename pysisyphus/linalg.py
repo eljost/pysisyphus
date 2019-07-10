d4f6978d178dc9a5beedb7bb94cb594b3633ad83
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-07-10 15:36:16 +0200

    bugfix: linear dependent (near zero) vectors weren't dropped but instead renormalized and added to the set of orthonormalized vectors. now they are dropped

--- pysisyphus/linalg.py
+++ pysisyphus/linalg.py
@@ -3,12 +3,20 @@
 import numpy as np
 
 
-def gram_schmidt(vecs):
+def gram_schmidt(vecs, thresh=1e-8):
     def proj(v1, v2): return v1.dot(v2)/v1.dot(v1)
     ortho = [vecs[0], ]
     for v1 in vecs[1:]:
         tmp = v1.copy()
         for v2 in ortho:
             tmp -= proj(v2, v1)*v2
-        ortho.append(tmp / np.linalg.norm(tmp))
-    return ortho
+        norm = np.linalg.norm(tmp)
+        print("norm:", norm)
+        # Don't append linear dependent vectors, as their norm will be
+        # near zero. Renormalizing them to unity would lead to numerical
+        # garbage and to erronous results later on, when we orthgonalize
+        # against this 'arbitrary' vector.
+        if norm <= thresh:
+            continue
+        ortho.append(tmp / norm)
+    return np.array(ortho)
