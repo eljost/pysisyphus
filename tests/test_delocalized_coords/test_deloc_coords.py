e6f630d68a4f1f71f868e2b2d9f5f21423c7b398
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-07-19 14:24:00 +0200

    updated test after rework of DLC constraints

--- tests/test_delocalized_coords/test_deloc_coords.py
+++ tests/test_delocalized_coords/test_deloc_coords.py
@@ -28,6 +28,31 @@ def test_dlc_constraints():
     constraints_ = int_.constraints
 
 
+def test_h2o2_dlc_constraints():
+    xyz_fn = "h2o2_hf_321g_opt.xyz"
+    geom = geom_from_library(xyz_fn, coord_type="dlc")
+    xyz_fn2 = "h2o2_rot2.xyz"
+    geom2 = geom_from_library(xyz_fn2, coord_type="dlc")
+    tangent = geom2 - geom
+    from pysisyphus.intcoords.helpers import get_tangent
+    prim_tangent = get_tangent(geom2.internal.prim_coords,
+                               geom.internal.prim_coords,
+                               geom2.internal.dihed_start)
+
+    freeze = ((0, 1, 2, 3), )
+    int_ = geom.internal
+    int_.freeze_primitives(freeze)
+    U = int_.U
+
+    constraints = int_.constraints
+    Uc = np.concatenate((int_.constraints, int_.U), axis=1)
+    Uc_inv = np.linalg.pinv(Uc.T)
+    dlc_tangent = Uc.T.dot(prim_tangent/np.linalg.norm(prim_tangent))
+    # int_.reset_constraints()
+    # constraints_ = int_.constraints
+    import pdb; pdb.set_trace()
+
+
 def run():
     xyz_fn = "fluorethylene.xyz"
     geom = geom_from_library(xyz_fn, coord_type="dlc")
@@ -53,5 +78,6 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
-    test_dlc_constraints()
+    # run()
+    # test_dlc_constraints()
+    test_h2o2_dlc_constraints()
