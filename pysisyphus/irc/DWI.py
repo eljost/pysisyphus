a95c44e6961c2956b14c438fe27c509d50221bc2
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2019-09-02 13:56:47 +0200

    added DWI gradient and checked it against finite differences

--- pysisyphus/irc/DWI.py
+++ pysisyphus/irc/DWI.py
@@ -11,14 +11,23 @@ import numpy as np
 
 
 def taylor(energy, gradient, hessian, step):
+    """Taylor series expansion of the energy to second order."""
     return energy + step @ gradient + 0.5 * step @ hessian @ step
+
+
+def taylor_grad(gradient, hessian, step):
+    """Gradient of a Taylor series expansion of the energy to second order."""
+    return gradient + hessian @ step
 
 
 class DWI:
 
     def __init__(self, n=4, maxlen=2):
+        """Distance weighted interpolation."""
+
         self.n = int(n)
         assert self.n > 0
+        assert (self.n % 2) == 0
         self.maxlen = maxlen
         assert self.maxlen == 2, \
             "Right now only maxlen=2 is supported!"
@@ -38,7 +47,7 @@ class DWI:
         assert len(self.coords) == len(self.energies) \
                == len(self.gradients) == len(self.hessians)
 
-    def interpolate(self, at_coords):
+    def interpolate(self, at_coords, gradient=False):
         """See [1] Eq. (25) - (29)"""
         c1, c2 = self.coords
 
@@ -63,4 +72,28 @@ class DWI:
 
         E_dwi = w1*t1 + w2*t2
 
-        return E_dwi
+        if not gradient:
+            return E_dwi
+
+        t1_grad = taylor_grad(g1, h1, dx1)
+        t2_grad = taylor_grad(g2, h2, dx2)
+
+        # The gradient of dx2_norm_n w.r.t the coordinates is formulated with
+        # **2n instead of **n, so the square root can be easily reduced.
+        # sqrt(x)**2n = x**(1/2)**2n = x**n
+        #
+        # Thats why we have to do following calculations with the half the value
+        # of n.
+        n_2 = self.n // 2
+        dx1_norm_n_grad = 2 * n_2 * dx1_norm**(2*n_2-2) * dx1
+        dx2_norm_n_grad = 2 * n_2 * dx2_norm**(2*n_2-2) * dx2
+        grad_denom = denom**2
+        w1_grad = (dx2_norm_n_grad*dx1_norm_n  - dx1_norm_n_grad*dx2_norm_n) / grad_denom
+        w2_grad = -w1_grad
+
+        # E_dwi = w1(x)*T1(x) + w2(x)*T2(x)
+        #
+        # dE_DWI / dx = dw1(x)*T1(x) + w1(x)*dT1(x) + dw2(x)*T2(x) + w2*dT2(x)
+        grad_dwi = w1_grad*t1 + w1*t1_grad + w2_grad*t2 + w2*t2_grad
+
+        return E_dwi, grad_dwi
--- tests/test_dwi/test_dwi.py
+++ tests/test_dwi/test_dwi.py
@@ -33,17 +33,52 @@ def test_dwi():
     points = 10
     step = (coords[1] - coords[0]) / (points - 1)
     true_ens = list()
+    true_grads = list()
     interpol_ens = list()
+    interpol_grads = list()
+    findiff = 1e-4
+    fd_grads = list()
     for i in range(points):
         new_coords = coords[0] + i*step
         geom.coords = new_coords
+        true_grad = geom.gradient
+        true_grads.append(true_grad)
         true_energy = geom.energy
         true_ens.append(true_energy)
-        interpol_energy = dwi.interpolate(new_coords)
+        interpol_energy, interpol_grad = dwi.interpolate(new_coords, gradient=True)
         interpol_ens.append(interpol_energy)
+        interpol_grads.append(interpol_grad)
+
+        fd_grad = list()
+        for i in range(3):
+            fd_step = np.zeros(3)
+            fd_step[i] = findiff
+            plus_val = dwi.interpolate(new_coords+fd_step)
+            minus_val = dwi.interpolate(new_coords-fd_step)
+            fd = (plus_val - minus_val) / (2*findiff)
+            fd_grad.append(fd)
+        fd_grads.append(fd_grad)
+
+    true_ens = np.array(true_ens)
+    interpol_ens = np.array(interpol_ens)
+    true_grads = np.array(true_grads)
+    interpol_grads = np.array(interpol_grads)
+    fd_grads = np.array(fd_grads)
+    np.testing.assert_allclose(interpol_grads, fd_grads)
+
+    quiver_xs = np.arange(interpol_ens.size)
+    quiver_true_ys = true_ens
+    quiver_interpol_ys = interpol_ens
     fig, ax = plt.subplots()
     ax.plot(true_ens, label="True")
     ax.plot(interpol_ens, label="DWI")
+
+    tg_U = true_grads[:,0]
+    tg_V = true_grads[:,1]
+    ti_U = interpol_grads[:,0]
+    ti_V = interpol_grads[:,1]
+    ax.quiver(quiver_xs, quiver_true_ys, tg_U, tg_V, color="b", label="grad(True)")
+    ax.quiver(quiver_xs, quiver_interpol_ys, ti_U, ti_V, color="orange", label="grad(DWI)")
     ax.legend()
 
     plt.show()
