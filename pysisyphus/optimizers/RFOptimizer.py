bugfix: forgot 2nd hessian projection and gradient projection is still missing

pysisyphus/optimizers/RFOptimizer.py
@@ -26,15 +26,90 @@ class RFOptimizer(HessianOptimizer):
         self.forces.append(-self.geometry.gradient)
         self.energies.append(self.geometry.energy)
 
+        org_grad = gradient.copy()
         if self.cur_cycle > 0:
             # TODO: skip hessian update in the beginning when the gradients
             # are big?
             self.update_trust_radius()
             self.update_hessian()
+
+            # interpol_forces = None
             if self.eins:
                 # print("eins")
                 gradient = self.poly_line_search()
                 # print(f"\tnorm(grad)={np.linalg.norm(gradient):.6f}")
+            # elif self.zwei and self.cur_cycle > 0:
+                # # print("zwei")
+                # ie_kwargs = {
+                    # # "geom": self.geometry,
+                    # "coords": self.coords,
+                    # "energies": self.energies,
+                    # "forces": self.forces,
+                    # "steps": self.steps,
+                    # "ref_step": None,
+                    # "err_vecs": self.forces,
+                    # "max_vecs": 10,
+                    # # "gediis_thresh": 1e-2,
+                    # # "gdiis_thresh": 2.5e-3,
+                    # "gediis_thresh": -1,
+                    # "gdiis_thresh": -1,
+                # }
+                # interpol_step, interpol_forces, interpol_en = interpolate_extrapolate(**ie_kwargs)
+            # if interpol_forces is not None:
+            # # if False:
+
+                # fit_coords = self.coords[-2] + interpol_step
+                # self.geometry.coords = fit_coords
+                # self.forces[-1] = interpol_forces
+                # self.energies[-1] = interpol_en
+                # self.coords[-1] = fit_coords.copy()
+                # self.cart_coords[-1] = self.geometry.cart_coords.copy()
+                # self.steps[-1] = interpol_step
+                # gradient = -interpol_forces
+
+
+                # # print("drei")
+                # # fit_coords = self.coords[-2] + interpol_step
+                # # self.geometry.coords = fit_coords
+                # # self.forces[-1] = interpol_forces
+                # # self.energies[-1] = interpol_en
+                # # self.coords[-1] = fit_coords.copy()
+                # # self.cart_coords[-1] = self.geometry.cart_coords.copy()
+                # # self.steps[-1] = interpol_step
+                # # print(f"\tnorm(grad)={np.linalg.norm(interpol_forces):.6f}")
+
+                # # fit_coords = self.coords[-2] + interpol_step
+                # # fit_coords = self.coords[-1] + interpol_step
+
+                # # TODO: update step and other saved entries?!
+                # # self.geometry.coords = fit_coords
+                # # self.forces[-1] = interpol_forces
+                # # self.energies[-1] = interpol_en
+                # # self.coords[-1] = fit_coords.copy()
+                # # # self.cart_coords[-1] = self.geometry.cart_coords.copy()
+                # # self.steps[-1] = interpol_step
+
+                # # H_aug = np.array(np.bmat(
+                                    # # ((H, -interpol_forces[:, None]),
+                                     # # (-interpol_forces[None, :], [[0]]))
+                # # ))
+                # # step, eigval, nu = self.solve_rfo(H_aug, "min")
+                # # step = interpol_step + step
+                # # step = interpol_step + step
+            # # else:
+                # # print(f"\tnorm(grad)={np.linalg.norm(gradient):.6f}")
+
+            # # can_gediis = np.sqrt(np.mean(self.forces[-1]**2)) < 1e-2
+            # # try:
+                # # can_diis = np.sqrt(np.mean(self.steps[-1]**2)) < 2.5e-3
+            # # except IndexError:
+                # # can_diis = False
+            # # # GDIIS check
+            # # can_gediis = False
+            # # if self.hybrid and (self.cur_cycle) > 0 and can_diis:
+                # # # rms_step = np.sqrt(np.mean(self.steps[-1]**2))
+                # # print(f"\t\t\trms(step): {rms_step:.6f}")
+                # # gdiis_kwargs = {
 
         H = self.H
 
@@ -85,7 +160,8 @@ class RFOptimizer(HessianOptimizer):
             }
             interpol_step, interpol_forces, interpol_en = interpolate_extrapolate(**ie_kwargs)
         if interpol_forces is not None:
-            # print("drei")
+        # if False:
+
             fit_coords = self.coords[-2] + interpol_step
             self.geometry.coords = fit_coords
             self.forces[-1] = interpol_forces
@@ -93,6 +169,16 @@ class RFOptimizer(HessianOptimizer):
             self.coords[-1] = fit_coords.copy()
             self.cart_coords[-1] = self.geometry.cart_coords.copy()
             self.steps[-1] = interpol_step
+
+
+            # print("drei")
+            # fit_coords = self.coords[-2] + interpol_step
+            # self.geometry.coords = fit_coords
+            # self.forces[-1] = interpol_forces
+            # self.energies[-1] = interpol_en
+            # self.coords[-1] = fit_coords.copy()
+            # self.cart_coords[-1] = self.geometry.cart_coords.copy()
+            # self.steps[-1] = interpol_step
             # print(f"\tnorm(grad)={np.linalg.norm(interpol_forces):.6f}")
 
             # fit_coords = self.coords[-2] + interpol_step
@@ -106,11 +192,21 @@ class RFOptimizer(HessianOptimizer):
             # # self.cart_coords[-1] = self.geometry.cart_coords.copy()
             # self.steps[-1] = interpol_step
 
+            gradient = -interpol_forces
+            # H_aug = np.array(np.bmat(
+                                # ((H, -interpol_forces[:, None]),
+                                 # (-interpol_forces[None, :], [[0]]))
+            # ))
+            # if self.geometry.internal:
+                # H_proj = self.geometry.internal.project_hessian(self.H)
+                # # Symmetrize hessian, as the projection probably breaks it?!
+                # H = (H_proj + H_proj.T) / 2
             H_aug = np.array(np.bmat(
-                                ((H, -interpol_forces[:, None]),
-                                 (-interpol_forces[None, :], [[0]]))
+                                ((H, gradient[:, None]),
+                                 (gradient[None, :], [[0]]))
             ))
             step, eigval, nu = self.solve_rfo(H_aug, "min")
+            # step = interpol_step + step
             # step = interpol_step + step
             # step = interpol_step + step
         # else:
@@ -121,7 +217,7 @@ class RFOptimizer(HessianOptimizer):
             # can_diis = np.sqrt(np.mean(self.steps[-1]**2)) < 2.5e-3
         # except IndexError:
             # can_diis = False
-        # # GDIIS check
+        # GDIIS check
         # can_gediis = False
         # if self.hybrid and (self.cur_cycle) > 0 and can_diis:
             # # rms_step = np.sqrt(np.mean(self.steps[-1]**2))
@@ -165,7 +261,8 @@ class RFOptimizer(HessianOptimizer):
             step = step / step_norm * self.trust_radius
         self.log(f"norm(step)={np.linalg.norm(step):.6f}")
 
-        quadratic_prediction = step @ gradient + 0.5 * step @ self.H @ step
+        # quadratic_prediction = step @ gradient + 0.5 * step @ self.H @ step
+        quadratic_prediction = step @ org_grad + 0.5 * step @ self.H @ step
         rfo_prediction = quadratic_prediction / (1 + step @ step)
         self.predicted_energy_changes.append(rfo_prediction)
 