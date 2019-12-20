b08e8362348096d2cee06272731980fb3d31b735
eljost <johannes.steinmetzer@uni-jena.de>
2019-12-20 18:11:01 +0100

    fixed test_opt with LEPS after calculator update

--- tests/test_opt/test_opt.py
+++ tests/test_opt/test_opt.py
@@ -24,7 +24,7 @@ from pysisyphus.optimizers.RFOptimizer import RFOptimizer
     (AnaPotCBM, (-0.32, 0.71, 0.), 9, (-1., 0. ,0.)),
     (CerjanMiller, (-0.46, 1.48, 0.), 10, (0., 0., 0.)),
     (FourWellAnaPot, (1.45, 0.04, 0.), 8, (1.1241, -1.4853, 0.)),
-    (LEPSBase, (1.31, 0.82, 0.), 36, (0.742, 11.1987, 0.)),
+    (LEPSBase, (1.31, 0.82, 0.), 41, (0.742, 11.1987, 0.)),
     (MullerBrownPot, (-0.69, 0.55, 0.), 10, (-0.05, 0.4667, 0.)),
     (Rosenbrock, (-1.00, 1.00, 0.), 47, (1., 1., 0.)),
     ]
