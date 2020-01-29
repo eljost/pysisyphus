2d520275890dbd35d703dc8f8fe555946a84a6d7
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-01-29 15:50:53 +0100

    work on GEDIIS

--- pysisyphus/optimizers/gdiis.py
+++ pysisyphus/optimizers/gdiis.py
@@ -8,6 +8,9 @@
 # [3] https://pubs.acs.org/doi/abs/10.1021/ct050275a
 #     GEDIIS/Hybrid method
 #     Li, Frisch, 2006
+# [4] https://aip.scitation.org/doi/10.1063/1.2977735
+#     Sim-GEDIIS using hessian information
+#     Moss, Li, 2008
 
 from collections import namedtuple
 import logging
@@ -134,7 +137,7 @@ def gdiis(err_vecs, coords, forces, ref_step, max_vecs=5):
     return diis_result(valid_coeffs, coords, forces, prefix="G")
 
 
-def gediis(coords, energies, forces, max_vecs=3):
+def gediis(coords, energies, forces, hessian=None, max_vecs=3):
     use = min(len(coords), max_vecs)
 
     R = coords[::-1][:use]
@@ -164,10 +167,22 @@ def gediis(coords, energies, forces, max_vecs=3):
         # cs = x2c(xs)
         # return anp.sum(cs*E) - anp.einsum("i,j,jk,ik", cs, cs, R, f) + anp.einsum("i,ij,ij", cs, R, f)
 
-    def fun(xs):
-        """Usage of precomputed values."""
-        cs = x2c(xs)
-        return anp.sum(cs*E) - anp.sum(anp.outer(cs, cs)*Rjfi) + (cs * Rifi).sum()
+    # Using precomputed values from above in 'fun()'
+    if hessian is None:
+        def fun(xs):
+            """Eq. (6) from [3]."""
+            cs = x2c(xs)
+            return anp.sum(cs*E) - anp.sum(anp.outer(cs, cs)*Rjfi) + (cs * Rifi).sum()
+    else:
+        hessian_inv = np.linalg.pinv(hessian)
+        # It doesn't matter if we use forces or gradients, as the signs will cancel.
+        # gHig = 0.5 * np.einsum("ki,ji,ki->k", f, hessian_inv, f)
+        gHig = np.einsum("ki,ji,ki->k", f, hessian_inv, f)
+        def fun(xs):
+            """Eq. (5) from [4]."""
+            cs = x2c(xs)
+            # Consider the hessian in the first term
+            return 0.5 * anp.sum(cs*gHig) - anp.sum(anp.outer(cs, cs)*Rjfi) + (cs * Rifi).sum()
 
     # def fun(xs):
         # cs = x2c(xs)
@@ -180,6 +195,10 @@ def gediis(coords, energies, forces, max_vecs=3):
 
     x0 = np.ones(use) / use
     res = minimize(fun, x0=x0, jac=jac)#, tol=1e-7)
+    # print(res)
+    # print("final x", res.x)
+    # x = res.x
+    # import pdb; pdb.set_trace()
 
     coeffs = None
     if res.success:
@@ -189,7 +208,8 @@ def gediis(coords, energies, forces, max_vecs=3):
     coeff_str = np.array2string(coeffs, precision=4)
     log(f"\tCoefficients: {coeff_str}")
     # en_ = (E * coeffs).sum()
-    if en_ >= E[0]:
+    # import pdb; pdb.set_trace()
+    if (hessian is None) and (en_ >= E[0]):
         print(f"GEDIIS converged, but proposed energy is above current energy! Returning None")
         return None
     return diis_result(coeffs, coords, forces, energy=en_, prefix="GE")
