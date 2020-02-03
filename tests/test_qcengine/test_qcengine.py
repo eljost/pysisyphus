57e13d032fc74ca8c3a78b42ea4d04ca2d9965f6
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-02-03 10:05:03 +0100

    fixed test and added QCEngine MOPAC test

--- tests/test_qcengine/test_qcengine.py
+++ tests/test_qcengine/test_qcengine.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pytest
 
-from pysisyphus.calculators.QCEngine import QCEngine
+try:
+    from pysisyphus.calculators.QCEngine import QCEngine
+except ImportError:
+    print("QCEngine import failed. Did you install it?")
 from pysisyphus.helpers import geom_from_library
-from pysisyphus.testing import using_turbomole, using_qcengine
+from pysisyphus.testing import using_turbomole, using_qcengine, using
 
 
 @using_turbomole
 @using_qcengine
-def test_qcengine():
+def test_qcengine_turbomole():
     geom = geom_from_library("h2o_guess.xyz")
 
     qce_kwargs = {
@@ -17,7 +20,30 @@ def test_qcengine():
             "method": "hf",
             "basis": "def2-SVP",
         },
-        "keywords": {},
+    }
+    qce = QCEngine(**qce_kwargs)
+
+    geom.set_calculator(qce)
+
+    forces = geom.forces
+    energy = geom.energy
+    norm = np.linalg.norm(forces)
+
+    assert energy == pytest.approx(-75.95615655854)
+    assert norm == pytest.approx(0.11354367)
+
+
+@using("mopac")
+@using_qcengine
+def test_qcengine_mopac():
+    geom = geom_from_library("h2o_guess.xyz")
+
+    qce_kwargs = {
+        "program": "mopac",
+        "model": {
+            "method": "pm6",
+            "basis": None,
+        },
     }
     qce = QCEngine(**qce_kwargs)
 
@@ -29,5 +55,5 @@ def test_qcengine():
     print(f"forces={forces}")
     norm = np.linalg.norm(forces)
 
-    assert energy == pytest.approx(-75.95615655854)
-    assert norm == pytest.approx(0.11354367)
+    assert energy == pytest.approx(-0.083446211)
+    assert norm == pytest.approx(0.044034367)
