ed352600c726db534ff2c363c7cd230a9c812eaa
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-02-17 14:38:07 +0100

    added dask test. fixes #56

---
+++ tests/test_dask/test_dask.py
@@ -0,0 +1,30 @@
+from distributed import LocalCluster
+import pytest
+
+from pysisyphus.cos.NEB import NEB
+from pysisyphus.helpers import geom_from_library
+from pysisyphus.calculators import XTB
+from pysisyphus.optimizers.SteepestDescent import SteepestDescent
+from pysisyphus.testing import using
+
+
+@using("xtb")
+def test_dask():
+    with LocalCluster(n_workers=2) as cluster:
+        address = cluster.scheduler_address
+
+        geoms = geom_from_library("ala_dipeptide_iso_b3lyp_631gd_10_images.trj")
+
+        for i, geom in enumerate(geoms):
+            calc = XTB(pal=1, calc_number=i)
+            geom.set_calculator(calc)
+
+        neb = NEB(geoms, scheduler=address)
+
+        # from pysisyphus.optimizers.ConjugateGradient import ConjugateGradient
+        # opt = ConjugateGradient(neb, align=True, max_cycles=5)
+        max_cycles = 5
+        opt = SteepestDescent(neb, align=True, max_cycles=max_cycles)
+        opt.run()
+
+    assert opt.cur_cycle == (max_cycles - 1)
