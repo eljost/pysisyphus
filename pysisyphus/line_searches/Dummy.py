382b94709af70d5e2a43415f5eccbecda6792cc9
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-02-18 16:34:37 +0100

    removed Dummy

--- pysisyphus/line_searches/Dummy.py
+++
@@ -1,6 +0,0 @@
-from pysisyphus.line_searches.LineSearch import LineSearch
-
-class Dummy(LineSearch):
-
-    def run_line_search(self):
-        return 1.
--- pysisyphus/line_searches/__init__.py
+++ pysisyphus/line_searches/__init__.py
@@ -2,10 +2,8 @@ __all__ = [
     "Backtracking",
     "HagerZhang",
     "StrongWolfe",
-    "Dummy",
 ]
 
 from pysisyphus.line_searches.Backtracking import Backtracking
-from pysisyphus.line_searches.Dummy import Dummy
 from pysisyphus.line_searches.HagerZhang import HagerZhang
 from pysisyphus.line_searches.StrongWolfe import StrongWolfe
