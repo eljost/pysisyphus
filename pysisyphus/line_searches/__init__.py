f96273e3fd083933e18de5a0c69fa66cf372810f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-02-18 15:55:59 +0100

    added scale

--- pysisyphus/line_searches/__init__.py
+++ pysisyphus/line_searches/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     "Backtracking",
     "HagerZhang",
+    "Scale",
     "StrongWolfe",
     "Dummy",
 ]
@@ -8,4 +9,5 @@ __all__ = [
 from pysisyphus.line_searches.Backtracking import Backtracking
 from pysisyphus.line_searches.Dummy import Dummy
 from pysisyphus.line_searches.HagerZhang import HagerZhang
+from pysisyphus.line_searches.Scale import Scale
 from pysisyphus.line_searches.StrongWolfe import StrongWolfe
