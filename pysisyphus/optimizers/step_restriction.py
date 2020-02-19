b9ffd76a6f34d69581fcc90c96d495f9ea16c224
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-02-19 09:50:46 +0100

    deleted

--- pysisyphus/optimizers/step_restriction.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def scale_by_max_step(steps, max_step):
-    steps_max = np.abs(steps).max()
-    if steps_max > max_step:
-        steps *= max_step / steps_max
-    return steps
