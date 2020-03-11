30c116d37d8e4bec98f58e1693a4c59f1d0072f6
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-03-11 09:45:22 +0100

    added augment_coordinates test for biaryl_bare_pm6

--- tests/test_tsopt/test_augment_coordinates.py
+++ tests/test_tsopt/test_augment_coordinates.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from pysisyphus.calculators import Gaussian16
 from pysisyphus.calculators.PySCF import PySCF
 from pysisyphus.helpers import geom_from_library
 from pysisyphus.intcoords.augment_bonds import augment_bonds
@@ -44,6 +45,37 @@ def test_augment_coordinates_silyl(augment, ref_cycle):
     opt = RSIRFOptimizer(geom, **opt_kwargs)
     opt.run()
 
+    assert opt.is_converged
+    assert opt.cur_cycle == ref_cycle
+
     ref_en = -367.20778
     assert geom.energy == pytest.approx(ref_en)
+
+
+
+@using("gaussian16")
+@pytest.mark.parametrize(
+    "augment, ref_cycle",
+    [
+        (True, 28),
+        (False, 40),
+    ]
+)
+def test_augment_biaryl_bare(augment, ref_cycle):
+    geom = geom_from_library("biaryl_bare_pm6_splined_hei.xyz",
+                             coord_type="redund")
+
+    calc = Gaussian16("PM6", pal=4)
+    geom.set_calculator(calc)
+
+    if augment:
+        geom = augment_bonds(geom)
+
+    opt_kwargs = {
+        "thresh": "gau_tight",
+    }
+    opt = RSIRFOptimizer(geom, **opt_kwargs)
+    opt.run()
+
+    assert opt.is_converged
     assert opt.cur_cycle == ref_cycle
