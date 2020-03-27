581e091683bbb63314dd23e29fded060ccca5182
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-03-27 07:37:14 +0100

    improved logging

--- pysisyphus/InternalCoordinates.py
+++ pysisyphus/InternalCoordinates.py
@@ -264,7 +264,8 @@ class RedundantCoords:
                 angle = self.calc_bend(coords3d, (x_ind, h_ind, y_ind))
                 if (cov_rad_sum < distance < vdw) and (angle > np.pi/2):
                     self.hydrogen_bond_indices.append((h_ind, y_ind))
-                    self.log(f"Added hydrogen bond between {h_ind} and {y_ind}")
+                    self.log(f"Added hydrogen bond between atoms {h_ind} "
+                             f"({self.atoms[h_ind]}) and {y_ind} ({self.atoms[y_ind]})")
         self.hydrogen_bond_indices = np.array(self.hydrogen_bond_indices)
 
     def set_bond_indices(self, define_bonds=None, factor=None):
