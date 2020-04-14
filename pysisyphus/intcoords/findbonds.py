f07683b053d28fa6cb6909d84c87eb926617b6c6
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-04-14 09:12:58 +0200

    get_bond_sets can now also return the bond matrix

--- pysisyphus/intcoords/findbonds.py
+++ pysisyphus/intcoords/findbonds.py
@@ -12,23 +12,27 @@ def get_pair_covalent_radii(atoms):
     return pair_cov_radii
 
 
-def get_bond_sets(atoms, coords3d, bond_factor=1.3, return_cdm=False):
+def get_bond_mat(geom=None, bond_factor=1.3):
+    cdm = pdist(geom.coords3d)
+    pair_cov_radii = get_pair_covalent_radii(geom.atoms)
+    bond_mat = squareform(cdm <= (pair_cov_radii * bond_factor))
+    return bond_mat
+
+
+def get_bond_sets(atoms, coords3d, bond_factor=1.3, return_cdm=False,
+                  return_cbm=False):
     cdm = pdist(coords3d)
     # Generate indices corresponding to the atom pairs in the
     # condensed distance matrix cdm.
     atom_indices = list(it.combinations(range(len(coords3d)),2))
     atom_indices = np.array(atom_indices, dtype=int)
-    cov_rad_sums = get_pair_covalent_radii(atoms)
-    cov_rad_sums *= bond_factor
-    bond_flags = cdm <= cov_rad_sums
-    bond_indices = atom_indices[bond_flags]
-    if return_cdm:
-        return bond_indices, cdm
-    return bond_indices
-
-
-def get_bond_mat(geom, bond_factor=1.3):
-    cdm = pdist(geom.coords3d)
-    pair_cov_radii = get_pair_covalent_radii(geom.atoms)
-    bond_mat = squareform(cdm <= (pair_cov_radii * bond_factor))
-    return bond_mat
+    scaled_cr_sums = bond_factor * get_pair_covalent_radii(atoms)
+    # condensed bond matrix
+    cbm = (cdm <= scaled_cr_sums)
+    bond_indices = atom_indices[cbm]
+    if not return_cbm and not return_cdm:
+        return bond_indices
+    add_returns = tuple([
+        mat for flag, mat in ((return_cdm, cdm), (return_cbm, cbm)) if flag
+    ])
+    return (bond_indices, ) + add_returns
