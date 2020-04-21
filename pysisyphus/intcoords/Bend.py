9f2db77ae04c5ca6084e94f58046dbc930cf6b64
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-04-21 07:51:24 +0200

    full implementation including linear and complement angles

--- pysisyphus/intcoords/Bend.py
+++ pysisyphus/intcoords/Bend.py
@@ -6,14 +6,15 @@ from pysisyphus.intcoords.Primitive import Primitive
 class Bend(Primitive):
 
     def __init__(self, *args, linear=False, complement=False, **kwargs):
-        kwargs["calc_kwargs"] = ("complement", )
+        kwargs["calc_kwargs"] = ("complement", "linear")
         super().__init__(*args, **kwargs)
 
         self.linear = linear
         self.complement = complement
 
     @staticmethod
-    def _calculate(coords3d, indices, gradient=False, complement=False):
+    def _calculate(coords3d, indices, gradient=False, complement=False,
+                   linear=False):
         m, o, n = indices
         u_dash = coords3d[m] - coords3d[o]
         v_dash = coords3d[n] - coords3d[o]
@@ -22,15 +23,19 @@ class Bend(Primitive):
         u = u_dash / u_norm
         v = v_dash / v_norm
 
+        cross_vec1 = ( 1, -1, 1)
+        cross_vec2 = (-1,  1, 1)
+
         # Determine second vector for the cross product, to get an
         # orthogonal direction. Eq. (24) in [1]
         uv_parallel = Bend.parallel(u, v)
+        # if (not linear) and (not uv_parallel):
         if not uv_parallel:
             cross_vec = v
-        elif not Bend.parallel(u, (1, -1, 1)):
-            cross_vec = (1, -1, 1)
+        elif not Bend.parallel(u, cross_vec1):
+            cross_vec = cross_vec1
         else:
-            cross_vec = (-1, 1, 1)
+            cross_vec = cross_vec2
 
         w_dash = np.cross(u, cross_vec)
         w = w_dash / np.linalg.norm(w_dash)
@@ -39,6 +44,8 @@ class Bend(Primitive):
             angle_rad = np.arccos(u.dot(w)) + np.arccos(w.dot(v))
         else:
             angle_rad = np.arccos(u.dot(v))
+        # print(f"deg={np.rad2deg(angle_rad):.2f}°")
+        # print("\tcross_vec", cross_vec)
 
         if gradient:
             # Use a second direction, orthogonal to the original
