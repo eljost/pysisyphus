0ec9732f2bbc763ac8303d0378ce1a15a8c56bb6
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-04-21 08:08:55 +0200

    cleand up Bend definition and removed linear/complement referecnes

--- pysisyphus/intcoords/Bend.py
+++ pysisyphus/intcoords/Bend.py
@@ -5,16 +5,8 @@ from pysisyphus.intcoords.Primitive import Primitive
 
 class Bend(Primitive):
 
-    def __init__(self, *args, linear=False, complement=False, **kwargs):
-        kwargs["calc_kwargs"] = ("complement", "linear")
-        super().__init__(*args, **kwargs)
-
-        self.linear = linear
-        self.complement = complement
-
     @staticmethod
-    def _calculate(coords3d, indices, gradient=False, complement=False,
-                   linear=False):
+    def _calculate(coords3d, indices, gradient=False):
         m, o, n = indices
         u_dash = coords3d[m] - coords3d[o]
         v_dash = coords3d[n] - coords3d[o]
@@ -29,7 +21,6 @@ class Bend(Primitive):
         # Determine second vector for the cross product, to get an
         # orthogonal direction. Eq. (24) in [1]
         uv_parallel = Bend.parallel(u, v)
-        # if (not linear) and (not uv_parallel):
         if not uv_parallel:
             cross_vec = v
         elif not Bend.parallel(u, cross_vec1):
@@ -40,22 +31,9 @@ class Bend(Primitive):
         w_dash = np.cross(u, cross_vec)
         w = w_dash / np.linalg.norm(w_dash)
 
-        if complement:
-            angle_rad = np.arccos(u.dot(w)) + np.arccos(w.dot(v))
-        else:
-            angle_rad = np.arccos(u.dot(v))
-        # print(f"deg={np.rad2deg(angle_rad):.2f}°")
-        # print("\tcross_vec", cross_vec)
+        angle_rad = np.arccos(u.dot(v))
 
         if gradient:
-            # Use a second direction, orthogonal to the original
-            # orthogonal direction w.
-            if complement and (not uv_parallel):
-                w = -(u + v) / np.linalg.norm(u + v)
-            elif complement:
-                w = np.cross(u, w)
-
-
             uxw = np.cross(u, w)
             wxv = np.cross(w, v)
 
@@ -74,5 +52,4 @@ class Bend(Primitive):
         return angle_rad
 
     def __str__(self):
-        return f"Bend({tuple(self.indices)}, linear={self.linear}, " \
-               f"complement={self.complement})"
+        return f"Bend({tuple(self.indices)}")
