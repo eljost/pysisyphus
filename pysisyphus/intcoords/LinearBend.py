739ea13b95f2e3255aeb4b89e8db63a876a69ab5
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-04-21 07:42:30 +0200

    added linearbend-implementation that uses the sympy generated code

--- pysisyphus/intcoords/LinearBend.py
+++ pysisyphus/intcoords/LinearBend.py
@@ -1,60 +1,57 @@
 import numpy as np
 
+from pysisyphus.intcoords.derivatives import dq_lb
 from pysisyphus.intcoords.Primitive import Primitive
 
 
 class LinearBend(Primitive):
 
-    def __init__(self, *args, linear=False, complement=False, **kwargs):
+    def __init__(self, *args, complement=False, **kwargs):
         kwargs["calc_kwargs"] = ("complement", )
         super().__init__(*args, **kwargs)
 
-        self.linear = linear
         self.complement = complement
 
     @staticmethod
     def _calculate(coords3d, indices, gradient=False, complement=False):
-        m, o, n, p = indices
+        m, o, n = indices
         u_dash = coords3d[m] - coords3d[o]
         v_dash = coords3d[n] - coords3d[o]
         u_norm = np.linalg.norm(u_dash)
         v_norm = np.linalg.norm(v_dash)
         u = u_dash / u_norm
-        v = v_dash / v_norm
+        # v = v_dash / v_norm  # Never used ...
 
-        if not LinearBend.parallel(u, v):
-            cross_vec = v
-        else:
-            cross_vec = coords3d[p] - coords3d[o]
+        # Select initial vector for cross product, similar to
+        # geomeTRIC. It must NOT be parallel to u and/or v.
+        x_dash = coords3d[n] - coords3d[m]
+        x_norm = np.linalg.norm(x_dash)
+        x = x_dash / x_norm
+        cross_vecs = np.eye(3)
+        min_ind = np.argmin([np.dot(cv, x)**2 for cv in cross_vecs])
+        cross_vec = cross_vecs[min_ind]
+        # Generate first orthogonal direction
         w_dash = np.cross(u, cross_vec)
         w = w_dash / np.linalg.norm(w_dash)
 
+        # Generate second orthogonal direction
         if complement:
-            angle_rad = np.arccos(u.dot(w)) + np.arccos(w.dot(v))
-        else:
-            angle_rad = np.arccos(u.dot(v))
+            w = np.cross(u, w)
+
+        lb_rad = w.dot(np.cross(u_dash, v_dash)) / (u_norm * v_norm)
 
         if gradient:
-            if complement:
-                w = np.cross(u, w)
-
-            uxw = np.cross(u, w)
-            wxv = np.cross(w, v)
+            # Foruth argument is the orthogonal direction
+            grad = dq_lb(*coords3d[m], *coords3d[o], *coords3d[n], *w)
+            grad = grad.reshape(-1, 3)
 
             row = np.zeros_like(coords3d)
-            #                  |  m  |  n  |  o  |
-            # -----------------------------------
-            # sign_factor(amo) |  1  |  0  | -1  | first_term
-            # sign_factor(ano) |  0  |  1  | -1  | second_term
-            first_term = uxw / u_norm
-            second_term = wxv / v_norm
-            row[m,:] = first_term
-            row[o,:] = -first_term - second_term
-            row[n,:] = second_term
+            row[m, :] = grad[0]
+            row[o, :] = grad[1]
+            row[n, :] = grad[2]
             row = row.flatten()
-            return angle_rad, row
-        return angle_rad
+            return lb_rad, row
+        return lb_rad
 
     def __str__(self):
-        return f"Bend({tuple(self.indices)}, linear={self.linear}, " \
-               f"complement={self.complement})"
+        return f"LinearBend({tuple(self.indices)}, complement={self.complement})"
