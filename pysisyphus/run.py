dba8c40a98eac9cc3fa452510f0ea0c15660d446
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-07 15:23:41 +0200

    deleted superfluous line

--- pysisyphus/run.py
+++ pysisyphus/run.py
@@ -206,7 +206,7 @@ def run_cos(cos, calc_getter, opt_getter):
     if not opt.stopped:
         hei_coords, hei_energy, hei_tangent = cos.get_splined_hei()
         hei_geom = Geometry(cos.images[0].atoms, hei_coords)
-        hei_geom.coords = hei_coords
+        hei_geom.energy = hei_energy
         hei_fn = "splined_hei.xyz"
         with open(hei_fn, "w") as handle:
             handle.write(hei_geom.as_xyz())
