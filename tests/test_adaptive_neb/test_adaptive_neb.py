2668c06198a132fc8646d473dbfca040e9ced5ea
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-07 12:13:32 +0200

    further test cleanup after moving many functions to AnaPotBase class

--- tests/test_adaptive_neb/test_adaptive_neb.py
+++ tests/test_adaptive_neb/test_adaptive_neb.py
@@ -6,50 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
-from pysisyphus.plotters.AnimPlot import AnimPlot
 from pysisyphus.calculators.AnaPot import AnaPot
 from pysisyphus.cos.AdaptiveNEB import AdaptiveNEB
 from pysisyphus.Geometry import Geometry
 from pysisyphus.optimizers.SteepestDescent import SteepestDescent
 
 
-KWARGS = {
-    "images": 3,
-    "max_cycles": 50,
-    "dump": False,
-}
+@pytest.mark.sd
+def test_anapot_aneb():
+    image_num = 5
+    calc = AnaPot()
+    all_geoms = calc.get_path(image_num)
+    aneb_kwargs = {
+        # "keep_hei": True,
+    }
+    aneb = AdaptiveNEB(all_geoms, **aneb_kwargs)
 
-
-def run_cos_opt(cos, Opt, images, **kwargs):
-    opt = Opt(cos, **kwargs)
-    for img in cos.images:
-        img.set_calculator(AnaPot())
+    opt = SteepestDescent(aneb)
     opt.run()
 
-    return opt
-
-
-def animate_bare(opt):
-    xlim = (-2, 2.5)
-    ylim = (0, 5)
-    levels = (-3, 4, 80)
-    ap = AnimPlot(AnaPot(), opt, xlim=xlim, ylim=ylim, levels=levels,
-                  energy_profile=False, colorbar=False, figsize=(8, 6))
-    ap.animate()
-    return ap
-
-
-@pytest.mark.sd
-def test_steepest_descent_neb():
-    kwargs = copy.copy(KWARGS)
-    all_geoms = AnaPot().get_path(5)
-    aneb = AdaptiveNEB(all_geoms)#, keep_hei=False)
-    opt = run_cos_opt(aneb, SteepestDescent, **kwargs)
-
-    return opt
-
-
-if __name__ == "__main__":
-    opt = test_steepest_descent_neb()
-    ap = animate_bare(opt)
+    ap = calc.anim_opt(opt)
     plt.show()
+
+    return opt
