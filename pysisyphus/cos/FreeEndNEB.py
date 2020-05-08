01bc8812ca6f1cd3645d43e0337d9e3c5fb0ba55
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-08 15:49:59 +0200

    fixed FE-NEB

--- pysisyphus/cos/FreeEndNEB.py
+++ pysisyphus/cos/FreeEndNEB.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 from pysisyphus.cos.NEB import NEB
 
-# [1] http://dx.doi.org/10.1063/1.4962019
+# [1] https://www.pnas.org/content/pnas/104/9/3031.full.pdf
+#     Zhu, 2006
+#     Original method
+# [2] http://dx.doi.org/10.1063/1.4962019
+#     Zhang, 2016
+#     FreeEnd Adaptive NEB
 
 
 class FreeEndNEB(NEB):
-    def __init__(self, *args, mod=False, **kwargs):
-        super().__init__(*args, **kwargs)
+    kinds = ("original", "improved", "improved_v2")
 
-        self.mod = mod
+    def __init__(self, *args, kind="improved", fix_first=False, fix_last=False,
+                 **kwargs):
+        super().__init__(*args, fix_first=fix_first, fix_last=fix_last, **kwargs)
 
-    def mod_end_forces(self, i):
-        """Equation (7) in [1]."""
-        assert (i == 0) or (i == self.last_index)
-        true_forces = self.images[i].forces
-        tangent = self.get_tangent(i)
-        return true_forces - true_forces.dot(tangent) * tangent
+        assert self.fix_ends == False
+        assert kind in self.kinds
+
+        self.kind = kind
 
     @NEB.forces.getter
     def forces(self):
@@ -24,16 +30,33 @@ class FreeEndNEB(NEB):
         i_last = self.last_index
         forces_last = self.images[-1].forces
         forces_size = forces_last.size
-        if self.mod:
-            start_forces = self.mod_end_forces(0)
-            end_forces = self.mod_end_forces(self.last_index)
-            forces[:forces_size] = start_forces
-            forces[-forces_size:] = end_forces
+
+        tangent = self.get_tangent(i_last)
+        coord_diff = self.images[i_last].coords - self.images[i_last-1].coords
+        distance = np.linalg.norm(coord_diff)
+        spring_constant = self.k[i_last-1]
+        par_forces = -spring_constant * distance * tangent
+
+        # Original FE-NEB forces on last image, Eq. (1) in [1]
+        if self.kind == "original":
+            end_forces = (par_forces
+                          - par_forces.dot(forces_last) / np.linalg.norm(forces_last)
+                          * forces_last
+            )
+        # Improved FE-NEB forces proposed by Zhang in [2], Eq. (7)
+        elif self.kind == "improved":
+            end_forces = forces_last - forces_last.dot(tangent) * tangent
+            # a = self.get_perpendicular_forces(i_last)
+            # import pdb; pdb.set_trace()
+            # np.testing.assert_allclose(a, end_forces)
+        # Another proposed FE-NEB force by Zhang in [2], Appendix B
+        elif self.kind == "improved_v2":
+            lambda_ = np.linalg.norm(forces_last)/-forces_last.dot(par_forces)
+            self.log(f"λ={lambda_:.6f}")
+            end_forces = forces_last + lambda_ * par_forces
         else:
-            par_forces_last = self.get_parallel_forces(i_last)
-            lambda_ = -forces_last.dot(forces_last)/par_forces_last.dot(forces_last)
-            fe_forces = lambda_ * par_forces_last + forces_last
-            forces[-forces_size:] = fe_forces
+            raise Exception(f"Invalid kind={self.kind}! Valid kinds are: {self.kinds}.")
+        forces[-forces_size:] = end_forces
         self._forces = forces
 
         return self._forces
