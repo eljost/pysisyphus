65e89084f2b1795236c62ff6dd99c1a95929baf0
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-08 16:45:45 +0200

    added assert that checks if moving end images are present

--- pysisyphus/cos/FreeEndNEB.py
+++ pysisyphus/cos/FreeEndNEB.py
@@ -12,8 +12,14 @@ class FreeEndNEB(AdaptiveNEB):
     def __init__(self, *args, fix_first=False, fix_last=False, **kwargs):
         """Simple Free-End-NEB method.
 
+        Derived from AdaptiveNEB with disabled adaptation.
         Only implements Eq. (7) from [2]. For other implementations
         please see the commit 01bc8812ca6f1cd3645d43e0337d9e3c5fb0ba55.
+        There the other variants are present but I think Eq. (7) in [2] is
+        the simplest & best bet.
         """
         kwargs["adapt"] = False
         super().__init__(*args, fix_first=fix_first, fix_last=fix_last, **kwargs)
+
+        assert (not self.fix_first) or (not self.fix_last), \
+            "FreeEndNEB without moving end-image(s) is useless!"
