bdec7d886c146f33cf33453871659375070f26ab
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-08 16:05:07 +0200

    cleanup & test for FreeEndNEB. works now.

--- pysisyphus/cos/FreeEndNEB.py
+++ pysisyphus/cos/FreeEndNEB.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-
-import numpy as np
-
 from pysisyphus.cos.NEB import NEB
 
 # [1] https://www.pnas.org/content/pnas/104/9/3031.full.pdf
@@ -13,50 +9,29 @@ from pysisyphus.cos.NEB import NEB
 
 
 class FreeEndNEB(NEB):
-    kinds = ("original", "improved", "improved_v2")
-
-    def __init__(self, *args, kind="improved", fix_first=False, fix_last=False,
+    def __init__(self, *args, fix_first=False, fix_last=False,
                  **kwargs):
+        """Simple Free-End-NEB method.
+
+        Only implements Eq. (7) from [2]. For other implementations
+        please see the commit 01bc8812ca6f1cd3645d43e0337d9e3c5fb0ba55.
+        """
         super().__init__(*args, fix_first=fix_first, fix_last=fix_last, **kwargs)
 
         assert self.fix_ends == False
-        assert kind in self.kinds
-
-        self.kind = kind
 
     @NEB.forces.getter
     def forces(self):
         forces = super().forces
-        i_last = self.last_index
-        forces_last = self.images[-1].forces
-        forces_size = forces_last.size
+        forces_size = self.images[-1].forces.size
 
-        tangent = self.get_tangent(i_last)
-        coord_diff = self.images[i_last].coords - self.images[i_last-1].coords
-        distance = np.linalg.norm(coord_diff)
-        spring_constant = self.k[i_last-1]
-        par_forces = -spring_constant * distance * tangent
+        if not self.fix_first:
+            mod_forces = self.get_perpendicular_forces(0)
+            forces[:forces_size] = mod_forces
 
-        # Original FE-NEB forces on last image, Eq. (1) in [1]
-        if self.kind == "original":
-            end_forces = (par_forces
-                          - par_forces.dot(forces_last) / np.linalg.norm(forces_last)
-                          * forces_last
-            )
-        # Improved FE-NEB forces proposed by Zhang in [2], Eq. (7)
-        elif self.kind == "improved":
-            end_forces = forces_last - forces_last.dot(tangent) * tangent
-            # a = self.get_perpendicular_forces(i_last)
-            # import pdb; pdb.set_trace()
-            # np.testing.assert_allclose(a, end_forces)
-        # Another proposed FE-NEB force by Zhang in [2], Appendix B
-        elif self.kind == "improved_v2":
-            lambda_ = np.linalg.norm(forces_last)/-forces_last.dot(par_forces)
-            self.log(f"λ={lambda_:.6f}")
-            end_forces = forces_last + lambda_ * par_forces
-        else:
-            raise Exception(f"Invalid kind={self.kind}! Valid kinds are: {self.kinds}.")
-        forces[-forces_size:] = end_forces
+        if not self.fix_last:
+            mod_forces = self.get_perpendicular_forces(self.last_index)
+            forces[-forces_size:] = mod_forces
+
         self._forces = forces
-
         return self._forces
--- tests/test_freeend_neb/test_freeend_neb.py
+++ tests/test_freeend_neb/test_freeend_neb.py
@@ -1,101 +1,37 @@
-#!/usr/bin/env python3
-
-import copy
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pytest
-
-from pysisyphus.plotters.AnimPlot import AnimPlot
+from pysisyphus.calculators.AnaPot import AnaPot
 from pysisyphus.calculators.FreeEndNEBPot import FreeEndNEBPot
 from pysisyphus.cos.FreeEndNEB import FreeEndNEB
-from pysisyphus.cos.NEB import NEB
-from pysisyphus.Geometry import Geometry
 from pysisyphus.optimizers.SteepestDescent import SteepestDescent
-from pysisyphus.optimizers.QuickMin import QuickMin
-# from pysisyphus.optimizers.BFGS import BFGS
-
-KWARGS = {
-    "images": 21,
-    "max_cycles": 100,
-    "convergence": {
-        "max_force_thresh": 6e-3,
-        "rms_force_thresh": 5e-3,
-        "max_step_thresh": 6e-4,
-        "rms_step_thresh": 3e-4,
-    },
-    "dump": False,
-}
 
 
-@pytest.mark.skip
-def test_steepest_descent_neb():
-    kwargs = copy.copy(KWARGS)
+def test_freeend_anapot():
+    calc = AnaPot()
+    geoms = calc.get_path(15)
+    geoms = geoms[3:11]
+
+    fe_neb = FreeEndNEB(geoms)
+    opt = SteepestDescent(fe_neb)
+    opt.run()
+
+    # calc.anim_opt(opt, show=True)
+
+    assert opt.is_converged
+    assert opt.cur_cycle == 31
+
+
+def test_freeend_freeendpot():
     calc = FreeEndNEBPot()
-    all_geoms = calc.get_path(50)[:15]
-    # from pysisyphus.cos.NEB import NEB
-    neb_kwargs = {
-        "fix_ends": False,
-        "k_min": 50,
-        "k_max": 53,
-        "mod": True,
+    geoms = calc.get_path(50)[:35]
+
+    kwargs = {
+        "k_min": 5,
+        "k_max": 10,
     }
-    neb = FreeEndNEB(all_geoms, **neb_kwargs)
-    # neb = NEB(all_geoms)#, k_min=50, k_max=53)#, mod=True)#, k_min=50, k_max=50.3)
-    # opt = QuickMin(neb, max_cycles=60)
-    opt = SteepestDescent(neb, max_cycles=3)
+    fe_neb = FreeEndNEB(geoms, **kwargs)
+    opt = SteepestDescent(fe_neb, max_cycles=100)
     opt.run()
-    calc.anim_opt(opt, show=True)
 
-    return opt
+    # calc.anim_opt(opt, show=True)
 
-
-@pytest.mark.skip
-def yolo():
-    from pysisyphus.calculators.LEPSBase import LEPSBase
-    lb = LEPSBase(pot_type="harmonic")
-    initial = np.array((0.7423, 1.18342, 0))
-    final = np.array((2.7234, -1.08509, 0))
-    coords = (initial, final)
-    atoms = ("H")
-    geoms = [Geometry(atoms, c) for c in coords]
-    cos = NEB(geoms)
-    cos.interpolate(21)
-    opt = SteepestDescent(cos, max_cycles=10)
-    for img in cos.images:
-        img.set_calculator(LEPSBase("harmonic"))
-    opt.run()
-    levels = (-20, 20, 250)
-    ap = AnimPlot(lb, opt, xlim=lb.xlim, ylim=lb.ylim, levels=levels, tight_layout=False)
-    ap.animate()
-    # plt.show()
-
-
-@pytest.mark.skip
-def test_mb():
-    from pysisyphus.calculators.MullerBrownPot import MullerBrownPot
-    initial = np.array((-0.5588, 1.437, 0))
-    final = np.array((-0.22404, 1.0055, 0))
-    calc = MullerBrownPot()
-    geoms = [Geometry(("H", ), coords) for coords in (initial, final) ]
-    from pysisyphus.interpolate import interpolate
-    geoms = interpolate(*geoms, between=5)
-    for geom in geoms:
-        geom.set_calculator(calc)
-    cos = FreeEndNEB(geoms, k_min=10, k_max=20)
-    # cos = NEB(geoms)
-    opt = QuickMin(cos, max_cycles=100)
-    opt.run()
-    levels=(-150, -15, 40)
-    xlim = (-1, 0.2)
-    ylim = (0.4, 1.8)
-    mb = MullerBrownPot()
-    ap = AnimPlot(mb, opt, xlim=xlim, ylim=ylim, levels=levels)
-    ap.animate()
-    # plt.show()
-
-
-if __name__ == "__main__":
-    # opt = test_steepest_descent_neb()
-    test_mb()
-    # yolo()
+    assert opt.is_converged
+    assert opt.cur_cycle == 93
