a2d6cf01211e99393593281ea962c65b59872185
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-19 16:29:10 +0200

    added helper function for easy acces to red/green

--- pysisyphus/color.py
+++ pysisyphus/color.py
@@ -4,3 +4,16 @@ def red(str_):
 
 def green(str_):
     return f"\033[92m{str_}\033[0m"
+
+
+BOOL_COLORS = {
+    True: green,
+    False: red,
+}
+
+
+def bool_color(bool_, str_=None):
+    if str_ is None:
+        str_ = str(bool_)
+    color = BOOL_COLORS[bool_]
+    return color(str_)
