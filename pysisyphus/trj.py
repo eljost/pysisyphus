12f849bf9029937a91bfb04cf7204327eecb2374
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-25 14:39:35 +0200

    added fragsort to pysistrj

--- pysisyphus/trj.py
+++ pysisyphus/trj.py
@@ -17,6 +17,7 @@ import yaml
 from pysisyphus.constants import BOHR2ANG
 from pysisyphus.cos import *
 from pysisyphus.Geometry import Geometry
+from pysisyphus.intcoords.fragments import get_fragments
 from pysisyphus.helpers import (geom_from_xyz_file, geoms_from_trj, procrustes,
                                 get_coords_diffs, shake_coords)
 from pysisyphus.interpolate import *
@@ -37,69 +38,72 @@ def parse_args(args):
     parser = argparse.ArgumentParser("Utility to transform .xyz and .trj files.")
 
     parser.add_argument("fns", nargs="+",
-            help="Filenames of .xyz and/or .trj files (xyz and trj can be mixed)."
+        help="Filenames of .xyz and/or .trj files (xyz and trj can be mixed)."
     )
 
     action_group = parser.add_mutually_exclusive_group(required=True)
     action_group.add_argument("--between", type=int, default=0,
-                    help="Interpolate additional images."
+        help="Interpolate additional images."
     )
     action_group.add_argument("--align", action="store_true",
-                    help="Align geometries onto the first geometry."
+        help="Align geometries onto the first geometry."
     )
     action_group.add_argument("--split", action="store_true",
-                    help="Split a supplied geometries in multiple .xyz files."
+        help="Split a supplied geometries in multiple .xyz files."
     )
     action_group.add_argument("--reverse", action="store_true",
-                    help="Reverse a .trj file."
+        help="Reverse a .trj file."
     )
     action_group.add_argument("--cleantrj", action="store_true",
-                    help="Keep only the first four columns of xyz/trj files."
+        help="Keep only the first four columns of xyz/trj files."
     )
     action_group.add_argument("--spline", action="store_true",
-                    help="Evenly redistribute geometries along a splined path."
+        help="Evenly redistribute geometries along a splined path."
     )
     action_group.add_argument("--first", type=int,
-                    help="Copy the first N geometries to a new .trj file."
+        help="Copy the first N geometries to a new .trj file."
     )
     action_group.add_argument("--every", type=int,
-                    help="Create new .trj with every N-th geometry. "
-                         "Always includes the first and last point."
+        help="Create new .trj with every N-th geometry. "
+             "Always includes the first and last point."
     )
     action_group.add_argument("--center", action="store_true",
-                    help="Move the molecules centroid into the origin."
+        help="Move the molecules centroid into the origin."
     )
     action_group.add_argument("--centerm", action="store_true",
-                    help="Move the molecules center of mass into the origin."
+        help="Move the molecules center of mass into the origin."
     )
     action_group.add_argument("--translate", nargs=3, type=float,
-                    help="Translate the molecule by the given vector given " \
-                         "in Ångström."
+        help="Translate the molecule by the given vector given " \
+             "in Ångström."
     )
     action_group.add_argument("--append", action="store_true",
-                    help="Combine the given .xyz files into one .xyz file."
+        help="Combine the given .xyz files into one .xyz file."
     )
     action_group.add_argument("--join", action="store_true",
-                    help="Combine the given .xyz/.trj files into one .trj file."
+        help="Combine the given .xyz/.trj files into one .trj file."
     )
     action_group.add_argument("--match", action="store_true",
-            help="Resort the second .xyz file so the atom order matches the "
-                 "first .xyz file. Uses the hungarian method."
+        help="Resort the second .xyz file so the atom order matches the "
+             "first .xyz file. Uses the hungarian method."
     )
     action_group.add_argument("--std", action="store_true",
-                    help="Move supplied geometry to its standard orientation."
+        help="Move supplied geometry to its standard orientation."
     )
     action_group.add_argument("--shake", action="store_true",
-                    help="Shake (randomly displace) coordiantes."
+        help="Shake (randomly displace) coordiantes."
     )
     action_group.add_argument("--internals", action="store_true",
-                    help="Print automatically generated internals."
+        help="Print automatically generated internals."
     )
     action_group.add_argument("--get", type=int,
-                    help="Get n-th geometry. Expects 0-based index input."
+        help="Get n-th geometry. Expects 0-based index input."
     )
     action_group.add_argument("--origin", action="store_true",
-                    help="Translate geometry, so that min(X/Y/Z) == 0."
+        help="Translate geometry, so that min(X/Y/Z) == 0."
+    )
+    action_group.add_argument("--fragsort", action="store_true",
+        help="Resort atoms by fragments."
     )
 
     shake_group = parser.add_argument_group()
@@ -423,6 +427,22 @@ def origin(geoms):
     return geoms
 
 
+def frag_sort(geoms):
+    sorted_geoms = list()
+    for i, geom in enumerate(geoms):
+        print(f"{i:02d}: {geom}")
+        frags = get_fragments(geom.atoms, geom.coords)
+        print(f"\tFound {len(frags)} fragments.\n"
+               "\tResorting atoms and coordinates.")
+        new_indices = list(it.chain(*frags))
+        new_atoms = [geom.atoms[i] for i in new_indices]
+        new_coords = geom.coords3d[new_indices]
+        sorted_geoms.append(
+                Geometry(new_atoms, new_coords)
+        )
+    return sorted_geoms
+
+
 def run():
     args = parse_args(sys.argv[1:])
 
@@ -500,6 +520,9 @@ def run():
     elif args.internals:
         print_internals(geoms, args.atoms, args.add_prims)
         return
+    elif args.fragsort:
+        to_dump = frag_sort(geoms)
+        fn_base = "frag_sorted"
     elif args.origin:
         origin(geoms)
         fn_base = "origin"
