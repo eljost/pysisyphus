1ad5b04161f10bc06ee7baacd9d607de5c928559
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-05-26 11:39:47 +0200

    added rastrigin function & test in preparation of stocastic search test

---
+++ pysisyphus/calculators/Rastrigin.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from pysisyphus.calculators.AnaPotBase import AnaPotBase
+
+
+class Rastrigin(AnaPotBase):
+    """
+        http://www.sfu.ca/~ssurjano/rastr.html
+    """
+
+    def __init__(self): 
+        V_str = "20 + x**2 - 10*cos(2*pi*x) + y**2 - 10*cos(2*pi*y)"
+        xlim = (-5.12, 5.12)
+        ylim = xlim
+        levels = np.linspace(0, 50, 50)
+        minima = ((0., 0., 0.), )
+        super().__init__(V_str=V_str, xlim=xlim, ylim=ylim, levels=levels, minima=minima)
+
+    def __str__(self):
+        return "Rastrigin calculator"
