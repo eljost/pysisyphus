e7132a8c5046ed9c196b8ce3371454aa56964101
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-10 09:18:21 +0200

    added test for the new imag_modes_from_geom helper function

---
+++ tests/test_imag_modes_from_geom/test_imag_modes_from_geom.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pytest
+
+from pysisyphus.helpers import geom_loader, imag_modes_from_geom
+
+
+def test_imag_modes_from_geom(this_dir):
+    H = np.loadtxt(this_dir / "calculated_final_cart_hessian")
+    geom = geom_loader("lib:hcn_iso_ts_opt_xtb.xyz")
+    geom._hessian = H
+
+    imag_modes = imag_modes_from_geom(geom)
+    assert len(imag_modes) == 1
+
+    imag_mode = imag_modes[0]
+    assert imag_mode.nu == pytest.approx(-1425.22063)
+
+    with open("imag_mode.trj", "w") as handle:
+        handle.write(imag_mode.trj_str)
