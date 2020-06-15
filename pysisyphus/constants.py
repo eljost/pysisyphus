d29edcd5b97f5f19509972c88b5e3b35dccc8418
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-15 15:08:32 +0200

    added some conversion factors

--- pysisyphus/constants.py
+++ pysisyphus/constants.py
@@ -9,12 +9,16 @@ EVANG2AUBOHR = 1/AU2EV/ANG2BOHR
 # fs -> Bohr * sqrt(amu/Hartree)
 FS2AU = 0.9682885864793366
 # Boltzman constant
-KB = 1.38064852E-23  # m² kg s⁻² K⁻¹
+KB = 1.38064852E-23  # (m² kg s⁻² K⁻¹) or just (J / K)
 # Atomic mass unit to kg
 AMU2KG = 1.660_539_066_60e-27
 SEC2AU = 2.4188843265857e-17
 # Velocity from m/s to au
 AU2MPERSEC = BOHR2M / SEC2AU
 MPERSEC2AU = 1 / AU2MPERSEC
+# Velocity from m/s to Bohr/fs
+MPERSEC2AUPERFS = 1/BOHR2M * 1e-15
 # Hartree to kcal mol⁻¹
 AU2KCALMOL = 627.509474
+# Force to acceleration from Hartree/(Bohr*amu) -> Bohr / fs²
+FORCE2ACC = AU2J / AMU2KG * 1 / BOHR2M**2 / 1e30
