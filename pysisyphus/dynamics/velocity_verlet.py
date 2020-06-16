43a941114fc558cd569bcc64e0194915d82c7f1f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-16 12:33:29 +0200

    added verbose term_funcs & argument. deleted old implementation. fixed erronous t in MDResult.

--- pysisyphus/dynamics/velocity_verlet.py
+++ pysisyphus/dynamics/velocity_verlet.py
@@ -3,7 +3,7 @@ import logging
 
 import numpy as np
 
-from pysisyphus.constants import BOHR2M, AMU2KG, AU2J, AU2MPERSEC, FORCE2ACC
+from pysisyphus.constants import FORCE2ACC
 from pysisyphus.dynamics.helpers import kinetic_energy_from_velocities, \
                                         temperature_for_kinetic_energy
 
@@ -13,76 +13,7 @@ MDResult = namedtuple("MDResult",
 )
 
 
-def md(geom, v0, t, dt, term_funcs=None, rm_vcom=False):
-    """TODO: dump coords, velocities; check energy conservation.
-    Align geometry to avoid drift."""
-    """Velocity verlet integrator.
-
-    Parameters
-    ----------
-    geom : Geometry
-        The system to be integrated.
-    v0 : np.array, floats
-        Initial velocities.
-    t : float
-        Total simulation time i n fs.
-    dt : float
-        Timestep in fs.
-    term_funcs : optional
-        Add description.
-    rm_vcom : bool, optional, default=False
-        Remove center of mass velocity to avoid drift.
-    """
-
-    assert geom.coord_type == "cart"
-
-    steps = int(t/dt)
-    print(f"Doing {steps} steps of {dt:.1f} fs for a total of {steps*dt:.1f} fs.")
-
-    # Convert to seconds
-    t = t  * 1e-15
-    dt = dt * 1e-15
-
-    if term_funcs is None:
-        term_funcs = list()
-
-    m = geom.masses_rep * AMU2KG
-    M = geom.masses.sum() * AMU2KG
-    # x = geom.cart_coords * BOHR2M
-    x = geom.coords * BOHR2M
-    v = v0 * AU2MPERSEC
-    a_prev = 0
-    xs = list()
-
-    t_cur = 0
-    while t_cur < t:
-        print(t_cur)
-        t_cur += dt
-        xs.append(x.copy())
-        try:
-            f = geom.forces * AU2J / BOHR2M
-        except:
-            logging.exception("Force calculation in MD failed.")
-            break
-        a = f / m
-        v += .5 * (a + a_prev) * dt
-        # Remove center of mass velocity
-        if rm_vcom:
-            v -= np.sum(m*v / M)
-        x += v*dt + .5*a*dt**2
-        geom.coords = x / BOHR2M
-        a_prev = a
-        if any([tf(x) for tf in term_funcs]):
-            break
-    md_result = MDResult(
-                    coords=np.array(xs)/BOHR2M,
-                    t=t,
-    )
-
-    return md_result
-
-
-def md(geom, v0, steps, dt):
+def md(geom, v0, steps, dt, term_funcs=None, verbose=True):
     """Velocity verlet integrator.
 
     Parameters
@@ -95,13 +26,20 @@ def md(geom, v0, steps, dt):
         Number of simulation steps.
     dt : float
         Timestep in fs.
+    term_funcs : iterable, optional
+        Iterable of functions that are called with the atomic
+        coordinates in every MD cycle and result in termination
+        of the MD integration when they evaluate to true.
     """
 
     assert geom.coord_type == "cart"
 
-    # Total simulation time
-    t_ps = steps * dt * 1e-3
-    print(f"Doing {steps} steps of {dt:.1f} fs for a total of {t_ps:.2f} ps.")
+    if term_funcs is None:
+        term_funcs = list()
+
+    if verbose:
+        t_ps = steps * dt * 1e-3  # Total simulation time
+        print(f"Doing {steps} steps of {dt:.4f} fs for a total of {t_ps:.2f} ps.")
 
     # In amu
     masses = geom.masses
@@ -124,7 +62,7 @@ def md(geom, v0, steps, dt):
             logging.exception("Force calculation in MD failed.")
             break
 
-        if (i % 25) == 0:
+        if verbose and (i % 25) == 0:
             E_kin = kinetic_energy_from_velocities(masses, v.reshape(-1, 3))
             T = temperature_for_kinetic_energy(len(masses), E_kin)
             E_tot = E_pot + E_kin
@@ -141,9 +79,12 @@ def md(geom, v0, steps, dt):
         geom.coords = x
         a_prev = a
 
+        if any([tf(x) for tf in term_funcs]):
+            break
+
     md_result = MDResult(
                     coords=np.array(xs),
-                    t=t_ps,
+                    t=t_cur*1e-3,
     )
 
     return md_result
