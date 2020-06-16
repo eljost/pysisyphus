5c57cbf5616f2d7e4dbbbd1c97e8289f5964906d
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-16 11:19:23 +0200

    added some constants needed for MD

--- pysisyphus/constants.py
+++ pysisyphus/constants.py
@@ -10,6 +10,7 @@ EVANG2AUBOHR = 1/AU2EV/ANG2BOHR
 FS2AU = 0.9682885864793366
 # Boltzman constant
 KB = 1.38064852E-23  # (m² kg s⁻² K⁻¹) or just (J / K)
+KBAU = KB / AU2J
 # Atomic mass unit to kg
 AMU2KG = 1.660_539_066_60e-27
 SEC2AU = 2.4188843265857e-17
@@ -20,5 +21,13 @@ MPERSEC2AU = 1 / AU2MPERSEC
 MPERSEC2AUPERFS = 1/BOHR2M * 1e-15
 # Hartree to kcal mol⁻¹
 AU2KCALMOL = 627.509474
-# Force to acceleration from Hartree/(Bohr*amu) -> Bohr / fs²
-FORCE2ACC = AU2J / AMU2KG * 1 / BOHR2M**2 / 1e30
+
+# MD related
+
+# Force/amu to acceleration
+#   Hartree/(Bohr*amu) -> Bohr / fs²
+FORCE2ACC = AU2J / (AMU2KG * BOHR2M**2 * 1e30)
+# Velocity*amu -> Energy
+#   Bohr²/fs²*amu -> Hartree
+# VELO2E = AMU2KG * BOHR2M**2 / (1e-30 * AU2J)
+VELO2E = 1 / FORCE2ACC
