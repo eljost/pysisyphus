3d704e5cab1d6efff312a8af3a7c0542d1f53c5f
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-18 15:58:26 +0200

    added thermoanalysis test

--- tests/test_hessian/test_hessian.py
+++ tests/test_hessian/test_hessian.py
@@ -1,5 +1,6 @@
 import h5py
 import numpy as np
+import pytest
 
 from pysisyphus.calculators.PySCF import PySCF
 from pysisyphus.helpers import geom_loader
@@ -8,18 +9,21 @@ from pysisyphus.optimizers.RFOptimizer import RFOptimizer
 from pysisyphus.testing import using
 
 
-@using("pyscf")
-def test_save_hessian():
-    # fn, hess_fn = "lib:hcfclbr_xtbopt.xyz", "chfclbr.h5"
-    fn, hess_fn = "lib:h2o.xyz", "h2o_hessian.h5"
+@pytest.fixture
+def geom():
+    fn = "lib:h2o.xyz"
     geom = geom_loader(fn, coord_type="redund")
-
     calc = PySCF(basis="def2svp", xc="bp86", pal=2)
     geom.set_calculator(calc)
+    return geom
 
+
+@using("pyscf")
+def test_save_hessian(geom):
     opt = RFOptimizer(geom, thresh="gau_tight")
     opt.run()
 
+    hess_fn = "_h2o_hessian.h5"
     save_hessian(hess_fn, geom)
 
     with h5py.File(hess_fn, "r") as handle:
