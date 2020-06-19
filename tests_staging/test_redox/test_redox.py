f65078a17b30a71424785473405eceaf36f3e91b
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-19 15:35:57 +0200

    further work

--- tests_staging/test_redox/test_redox.py
+++ tests_staging/test_redox/test_redox.py
@@ -1,28 +1,76 @@
 from collections import namedtuple
 
 import h5py
+import numpy as np
 from scipy.constants import N_A as NA
 from thermoanalysis.QCData import QCData
 from thermoanalysis.thermo import thermochemistry
 
 from pysisyphus.calculators import Gaussian16, ORCA
 from pysisyphus.constants import AU2J
-from pysisyphus.helpers import geom_loader, highlight_text
+from pysisyphus.helpers import geom_loader, highlight_text, eigval_to_wavenumber
 from pysisyphus.io.hessian import save_hessian
 from pysisyphus.optimizers.RFOptimizer import RFOptimizer
+from pysisyphus.testing import using
 
 """
 See
     https://pubs.acs.org/doi/pdf/10.1021/jp025853n
 """
 
+# Farady constant
+F = 96_485.3329  # As mol⁻¹
 
 RedoxResult = namedtuple("RedoxResult",
-                         "energy_gas hessian_gas energy_solv geom_gas"
+                         "energy_gas hessian_gas energy_solv dG_solv geom_gas charge mult"
 )
 
 
-def run(geom, charge, mult, calc_getter_gas, calc_getter_solv, opt=False):
+
+def redox_result_to_qcdata(redox_result):
+    geom = redox_result.geom_gas
+    H = geom.eckart_projection(geom.mass_weigh_hessian(redox_result.hessian_gas))
+    w, v = np.linalg.eigh(H)
+    nus = eigval_to_wavenumber(w)
+
+    thermo_dict = {
+        "masses": geom.masses,
+        "vibfreqs": nus,
+        "coords3d": geom.coords3d,
+        "energy": redox_result.energy_gas,
+        "mult": redox_result.mult,
+    }
+    qcd = QCData(thermo_dict)
+    return qcd
+
+
+def calculate_potential(result_1, result_2, T=298.15):
+    qcd_1 = redox_result_to_qcdata(result_1)
+    qcd_2 = redox_result_to_qcdata(result_2)
+
+    thermo_1 = thermochemistry(qcd_1, temperature=T)
+    thermo_2 = thermochemistry(qcd_2, temperature=T)
+
+    DG_solv_neut = result_1.dG_solv
+    DG_solv_oxd = result_2.dG_solv
+    print(f"ΔG_solv_neut = {DG_solv_neut: >8.6f} E_h")
+    print(f"ΔG_solv_oxd  = {DG_solv_oxd: >8.6f} E_h")
+
+    DG_ox_g = thermo_2.G - thermo_1.G
+    print(f"ΔG_oxd_gas  = {DG_ox_g: >8.6f} E_h")
+    DG_ox_solv = DG_ox_g + DG_solv_oxd - DG_solv_neut
+    print(f"ΔG_oxd_solv  = {DG_ox_solv: >8.6f} E_h")
+
+    n = abs(result_1.charge - result_2.charge)
+    print(f"Δelectron(s) = {n}")
+    # To Joule mol⁻¹
+    redox_pot = DG_ox_solv * AU2J * NA / (n * F)
+    print(f"E^0/+ = {redox_pot:.2f} V")
+
+    return redox_pot
+
+
+def run_calculations(geom, charge, mult, calc_getter_gas, calc_getter_solv, opt=False):
     def get_name(base): return f"{base}_{charge}_{mult}"
 
     print(highlight_text(f"Charge={charge}, Mult={mult}"))
@@ -67,11 +115,16 @@ def run(geom, charge, mult, calc_getter_gas, calc_getter_solv, opt=False):
         handle.write(str(energys))
     print(highlight_text("Solvated energy finished!", level=1))
 
+    dG_solv = energys - energyg
+
     res = RedoxResult(
             energy_gas=energyg,
             hessian_gas=Hg,
             energy_solv=energys,
+            dG_solv=dG_solv,
             geom_gas=geom,
+            charge=charge,
+            mult=mult,
     )
     return res
 
@@ -102,59 +155,44 @@ def test_redox(inp_1, inp_2, calc_cls, gas_kwargs, solv_kwargs):
         solv_calc_number += 1
         return calc
 
-    ref_res = run(geom_1, charge_1, mult_1, calc_getter_gas, calc_getter_solv, opt=opt_1)
+    result_1 = run_calculations(geom_1, charge_1, mult_1,
+                                calc_getter_gas, calc_getter_solv, opt=opt_1)
     print()
-    oxd_res = run(geom_2, charge_2, mult_2, calc_getter_gas, calc_getter_solv, opt=opt_2)
+    result_2 = run_calculations(geom_2, charge_2, mult_2,
+                                calc_getter_gas, calc_getter_solv, opt=opt_2)
+
+    calculate_potential(result_1, result_2, T=298.15)
 
 
-def calculate_potential(n=1, T=298.15):
-    # Neutral
-    gas01 = "gas_0_1.h5"
-    solv01 = "solv_0_1.energy"
-    # Oxidized
-    gas12 = "gas_1_2.h5"
-    solv12 = "solv_1_2.energy"
+@using("orca")
+def test_nitrobenzene():
+    inp_1 = {
+        "xyz": "lib:redox/nitrobenzene.xyz",
+        "charge": 0,
+        "mult": 1,
+        "opt": False,
+    }
+    inp_2 = {
+        "xyz": "lib:redox/nitrobenzene_anion.xyz",
+        "charge": -1,
+        "mult": 2,
+        "opt": False,
+    }
 
-    def process(h5):
-        qc = QCData(h5)
-        thermo = thermochemistry(qc, temperature=T)
-        return qc, thermo
-
-    # Neutral
-    qcg01, thermog01 = process(gas01)
-    with open(solv01) as handle:
-        energy_solv01 = float(handle.read())
-
-    # Oxidized
-    qcg12, thermog12 = process(gas12)
-    with open(solv12) as handle:
-        energy_solv12 = float(handle.read())
-
-    # Farady constant
-    F = 96_485.3329  # As mol⁻¹
-
-    def DG_solv(solv_energy, h5_gas):
-        with h5py.File(h5_gas, "r") as handle:
-            gas_energy = handle.attrs["energy"]
-        DG_solv = solv_energy - gas_energy
-        return DG_solv
-
-    DG_solv_neut = DG_solv(energy_solv01, gas01)
-    DG_solv_oxd = DG_solv(energy_solv12, gas12)
-    print(f"ΔG_solv_neut = {DG_solv_neut: >8.6f} E_h")
-    print(f"ΔG_solv_oxd  = {DG_solv_oxd: >8.6f} E_h")
-
-    DG_ox_g = thermog12.G - thermog01.G
-    print(f"ΔG_oxd_gas  = {DG_ox_g: >8.6f} E_h")
-    DG_ox_solv = DG_ox_g + DG_solv_oxd - DG_solv_neut
-    print(f"ΔG_oxd_solv  = {DG_ox_solv: >8.6f} E_h")
-
-    print(f"Δelectron(s) = {n}")
-    # To Joule mol⁻¹
-    redox_pot = DG_ox_solv * AU2J * NA / (n * F)
-    print(f"E^0/+ = {redox_pot:.2f} V")
-
-    return redox_pot
+    orca_gas = {
+        "keywords": "uks b3lyp 6-31G** rijcosx autoaux",
+        "pal": 4,
+    }
+    orca_solv = orca_gas.copy()
+    orca_solv["keywords"] += " cpcm(acetonitrile)"
+    orca_kwargs = {
+        "calc_cls": ORCA,
+        "gas_kwargs": orca_gas,
+        "solv_kwargs": orca_solv,
+    }
+    test_redox(inp_1, inp_2, **orca_kwargs)
+    import pdb; pdb.set_trace()
+    pass
 
 
 if __name__ == "__main__":
@@ -201,7 +239,6 @@ if __name__ == "__main__":
         "mult": 1,
         "opt": False,
     }
-    inp_2 = inp_1.copy()
     inp_2 = {
         "xyz": fn,
         "charge": 1,
@@ -211,4 +248,3 @@ if __name__ == "__main__":
 
     test_redox(inp_1, inp_2, **g16_kwargs)
     # test_redox(fn, **orca_kwargs)
-    calculate_potential()
