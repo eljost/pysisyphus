073efdfd4877cb51f4b484333c92d47dd8fd9d1e
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-24 11:41:41 +0200

    added xtb init hessians

--- tests/test_guess_hessians/test_guess_hessians.py
+++ tests/test_guess_hessians/test_guess_hessians.py
@@ -7,24 +7,29 @@ from pysisyphus.helpers import geom_from_library, do_final_hessian
 from pysisyphus.optimizers.guess_hessians import ts_hessian
 from pysisyphus.optimizers.RFOptimizer import RFOptimizer
 from pysisyphus.tsoptimizers.RSPRFOptimizer import RSPRFOptimizer
-from pysisyphus.testing import using_pyscf
+from pysisyphus.testing import using
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-@using_pyscf
+@using("pyscf")
 @pytest.mark.parametrize(
     "hessian_init, ref_cycle", [
-        ("lindh", 18),
+        # ("calc", 27),  # Very slow
+        # Converges to wrong minimum
+        # pytest.param("unit", 17, marks=[using("pyscf"), pytest.mark.xfail]),
         ("fischer", 17),
+        ("lindh", 18),
         ("simple", 27),
         ("swart", 20),
+        pytest.param("xtb", 22, marks=[using("pyscf"), using("xtb")]),
+        pytest.param("xtb1", 20, marks=[using("pyscf"), using("xtb")]),
     ]
 )
 def test_guess_hessians(hessian_init, ref_cycle):
     geom = geom_from_library("birkholz/vitamin_c.xyz", coord_type="redund")
-    geom.set_calculator(PySCF(basis="321g", pal=4))
+    geom.set_calculator(PySCF(basis="321g", pal=2))
 
     print("@\tguess_hessian:", hessian_init)
     opt_kwargs = {
@@ -40,7 +45,7 @@ def test_guess_hessians(hessian_init, ref_cycle):
     assert geom.energy == pytest.approx(-677.21287468)
 
 
-@using_pyscf
+@using("pyscf")
 def test_ts_hessian():
     H = np.diag((1, 0.5, 0.25))
     tsh = ts_hessian(H, (0, 1))
@@ -48,7 +53,7 @@ def test_ts_hessian():
     np.testing.assert_allclose(w, (-0.445194, 0.070194, 0.25), atol=1e-7)
 
 
-@using_pyscf
+@using("pyscf")
 def test_ts_hessian_opt():
     geom = geom_from_library("baker_ts/01_hcn.xyz", coord_type="redund",)
     geom.set_calculator(PySCF(basis="321g"))
