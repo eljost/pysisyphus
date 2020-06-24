86f1d005b869f2e6356b54694714e2be0446f8e9
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-24 14:04:42 +0200

    switched modekill test to pyscf so it can run in the CI

--- tests/test_modekill/test_modekill.py
+++ tests/test_modekill/test_modekill.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from pysisyphus.calculators import Gaussian16, XTB
+from pysisyphus.calculators import XTB
+from pysisyphus.calculators.PySCF import PySCF
 from pysisyphus.irc import ModeKill
 from pysisyphus.helpers import geom_loader, eigval_to_wavenumber
 from pysisyphus.testing import using
@@ -27,21 +28,21 @@ def test_modekill_xtb(this_dir):
     assert nus[0] == pytest.approx(-96.31027310)
 
 
-@using("gaussian16")
-def test_modekill_g16(this_dir):
+@using("pyscf")
+def test_modekill_pyscf(this_dir):
     fn = this_dir / "ethane_shaked.xyz"
     geom = geom_loader(fn)
-    calc = Gaussian16(route="bp86 sto-3g")
+    calc = PySCF(basis="sto3g", xc="bp86", pal=2)
     geom.set_calculator(calc)
 
     w, v = np.linalg.eigh(geom.eckart_projection(geom.mw_hessian))
     nus = eigval_to_wavenumber(w)
-    assert nus[0] == pytest.approx(-265.4967) 
+    assert nus[0] == pytest.approx(-266.11360383)
 
-    modekill = ModeKill(geom, kill_inds=[0,])
+    modekill = ModeKill(geom, kill_inds=[0, ])
     modekill.run()
     assert modekill.converged
 
     w, v = np.linalg.eigh(geom.eckart_projection(geom.mw_hessian))
     nus = eigval_to_wavenumber(w)
-    assert nus[0] == pytest.approx(-6.475704754831899e-05)
+    assert nus[0] == pytest.approx(-4.5086469e-05, abs=1e-4)
