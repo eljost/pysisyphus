45d553e4bf107c78371d08d99a90f6852ba96d2e
Johannes Steinmetzer <jsteinmetzer1108@aol.com>
2020-06-25 11:20:39 +0200

    updated test

--- tests/test_irc/test_run_irc.py
+++ tests/test_irc/test_run_irc.py
@@ -2,36 +2,38 @@ import pytest
 
 from pysisyphus.calculators import XTB
 from pysisyphus.helpers import geom_from_library
-from pysisyphus.run import run_irc
+from pysisyphus.run import run_irc, run_endopt
 from pysisyphus.testing import using
 
 
 @using("xtb")
 @pytest.mark.parametrize(
-    "opt_ends, opt_geom_num", [
-        (False, 0),
-        (True, 2),
-        ("fragments", 3),
+    "fragments, opt_geom_num", [
+        (False, 2),
+        (True, 3),
     ]
 )
-def test_run_irc_opt_ends(opt_ends, opt_geom_num):
+def test_run_irc_opt_ends(fragments, opt_geom_num):
     geom = geom_from_library("hfabstraction_ts_opt_xtb.xyz")
     calc = XTB(pal=2)
     geom.set_calculator(calc)
 
-    calc_ind = 0
-    def calc_getter(index):
-        nonlocal calc_ind
-        calc = XTB(calc_number=calc_ind, pal=2)
-        calc_ind += 1
+    def calc_getter(calc_number):
+        calc = XTB(calc_number=calc_number, pal=2)
         return calc
 
     irc_kwargs = {
         "type": "eulerpc",
-        "opt_ends": opt_ends,
         "max_cycles": 25,
 
     }
     irc_key = irc_kwargs.pop("type")
-    opt_geoms, irc = run_irc(geom, irc_key, irc_kwargs, calc_getter)
+    irc = run_irc(geom, irc_key, irc_kwargs, calc_getter)
+
+    endopt_key = "rfo"
+    endopt_kwargs = {
+        "thresh": "gau_tight",
+        "fragments": fragments,
+    }
+    opt_geoms = run_endopt(geom, irc, endopt_key, endopt_kwargs, calc_getter)
     assert len(opt_geoms) == opt_geom_num
