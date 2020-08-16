89d164d1d45c879bd3555ca189c7e9cac51b83dd
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-08-16 13:10:48 +0200

    removed debugging statements

--- tests/test_overlap_calculator/test_overlap_calculator.py
+++ tests/test_overlap_calculator/test_overlap_calculator.py
@@ -15,12 +15,9 @@ from pysisyphus.calculators.OverlapCalculator import OverlapCalculator
 def test_cdds_fallback(cdds, fallback, no_mwfn, no_jmol, monkeypatch):
     # Disable Mwfn/Jmol as requested
     def mock_available(calculator):
-        print("in mock!")
         if (calculator == "mwfn") and no_mwfn:
-            print("mock nowfn")
             return False
         if (calculator == "jmol") and no_jmol:
-            print("mock jmol")
             return False
         return True
     monkeypatch.setattr(pysisyphus.calculators.OverlapCalculator,
