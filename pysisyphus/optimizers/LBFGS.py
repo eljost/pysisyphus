3641e64b5a3aa28b846f36585285e91fd89f77e1
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-08-26 14:25:47 +0200

    added reset() and double damping. made gamma_mult an argument

--- pysisyphus/optimizers/LBFGS.py
+++ pysisyphus/optimizers/LBFGS.py
@@ -3,23 +3,32 @@
 import numpy as np
 
 from pysisyphus.helpers import fit_rigid
-from pysisyphus.optimizers.Optimizer import Optimizer
 from pysisyphus.optimizers.closures import bfgs_multiply
+from pysisyphus.optimizers.hessian_updates import double_damp
+from pysisyphus.optimizers.Optimizer import Optimizer
 from pysisyphus.optimizers.restrict_step import scale_by_max_step
 
 
+# See pysisyphus.optimizers.hessian_updates for double damping ref.
+
+
 class LBFGS(Optimizer):
-    def __init__(self, geometry, alpha=1.0, keep_last=7, beta=1, max_step=0.2,
-                  **kwargs):
+    def __init__(self, geometry, keep_last=7, beta=1, max_step=0.2,
+                 double_damp=False, gamma_mult=True, **kwargs):
         """[1] Nocedal, Wright - Numerical Optimization, 2006"""
-        self.alpha = alpha
         self.beta = beta
         self.keep_last = int(keep_last)
+        self.double_damp = double_damp
+        self.gamma_mult = gamma_mult
 
         self.coord_diffs = list()
         self.grad_diffs = list()
 
         super().__init__(geometry, max_step=max_step, **kwargs)
+
+    def reset(self):
+        self.coord_diffs = list()
+        self.grad_diffs = list()
 
     def _get_opt_restart_info(self):
         opt_restart_info = {
@@ -50,15 +59,20 @@ class LBFGS(Optimizer):
         self.energies.append(energy)
 
         if self.cur_cycle > 0:
-            prev_forces = self.forces[-2]
-            grad_diff = -forces - -prev_forces
-            self.grad_diffs.append(grad_diff)
+            y = self.forces[-2] - forces
+            s = self.steps[-1]
+            if self.double_damp:
+                s, y = double_damp(s, y, s_list=self.coord_diffs, y_list=self.grad_diffs)
+            self.grad_diffs.append(y)
+            self.coord_diffs.append(s)
+
+            # Drop superfluous oldest vectors
+            self.coord_diffs = self.coord_diffs[-self.keep_last:]
             self.grad_diffs = self.grad_diffs[-self.keep_last:]
 
-        step = bfgs_multiply(self.coord_diffs, self.grad_diffs, forces, beta=self.beta)
+        step = bfgs_multiply(self.coord_diffs, self.grad_diffs, forces, beta=self.beta,
+                             gamma_mult=self.gamma_mult)
         step = scale_by_max_step(step, self.max_step)
 
-        self.coord_diffs.append(step)
-        self.coord_diffs = self.coord_diffs[-self.keep_last:]
 
         return step
