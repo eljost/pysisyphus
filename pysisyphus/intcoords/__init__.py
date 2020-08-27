2197e6e3b5778ea4d3d38c9154acf0adaece858c
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-08-27 14:35:18 +0200

    cleaned logging format

--- pysisyphus/intcoords/__init__.py
+++ pysisyphus/intcoords/__init__.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 # delay = True prevents creation of empty logfiles
 handler = logging.FileHandler("internal_coords.log", mode="w", delay=True)
 # fmt_str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-fmt_str = "%(asctime)s - %(message)s"
+fmt_str = "%(message)s"
 formatter = logging.Formatter(fmt_str)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
