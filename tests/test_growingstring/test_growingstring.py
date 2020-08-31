550a9039b51fc48ec5e090919322e3965f8a7b94
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-08-31 08:00:25 +0200

    added mullerbrown GSM test

--- tests/test_growingstring/test_growingstring.py
+++ tests/test_growingstring/test_growingstring.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pysisyphus.calculators.AnaPot import AnaPot
+from pysisyphus.calculators.MullerBrownSympyPot import MullerBrownPot
 from pysisyphus.cos.GrowingString import GrowingString
 from pysisyphus.optimizers.StringOptimizer import StringOptimizer
 
@@ -38,3 +39,53 @@ def test_anapot_growing_string(keep_last, ref_cycle):
 
     # calc = AnaPot()
     # calc.anim_opt(opt, show=True)
+
+
+@pytest.mark.skip
+def test_anapot_growing_string():
+    initial = MullerBrownPot.get_geom((-0.5592, 1.443, 0))
+    final = MullerBrownPot.get_geom((0.605, 0.036, 0))
+    calc = MullerBrownPot()
+    geoms = (initial, final)
+    from pysisyphus.optimizers.RFOptimizer import RFOptimizer
+    for geom in geoms:
+        opt = RFOptimizer(geom, thresh="gau_tight", hessian_recalc=1,)
+        opt.run()
+
+    from pysisyphus.cos.NEB import NEB
+    from pysisyphus.interpolate import interpolate
+    geoms = interpolate(*geoms, 18)
+    for geom in geoms:
+        geom.set_calculator(calc)
+    cos = NEB(geoms, k_max=5.1, k_min=5.)
+    from pysisyphus.optimizers.QuickMin import QuickMin
+    from pysisyphus.optimizers.LBFGS import LBFGS
+    opt = QuickMin(cos, max_cycles=10)
+    opt.run()
+    opt = LBFGS(cos, max_step=0.02, gamma_mult=True)
+    opt.run()
+    calc = MullerBrownPot()
+    calc.anim_opt(opt, show=True)
+    import sys; sys.exit()
+
+    gs_kwargs = {
+        "perp_thresh": 25.0,
+        "reparam_check": "rms",
+        "max_nodes": 16,
+        # "reparam_every": 10,
+    }
+    gs = GrowingString(geoms, lambda: MullerBrownPot(), **gs_kwargs)
+
+    opt_kwargs = {
+        # "gamma": 20,
+        "gamma": 100,
+        "lbfgs_when_full": False,
+        "max_step": 0.03,
+        "max_cycles": 200,
+    }
+    opt = StringOptimizer(gs, **opt_kwargs)
+    opt.run()
+
+    # import pdb; pdb.set_trace()
+    calc = MullerBrownPot()
+    calc.anim_opt(opt, show=True)
