2ce7f5c5ff91a58df87d6bb4a6ca648c9d557a28
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-09-21 22:50:05 +0200

    fixed test

--- tests/test_intcoords/test_dihedral.py
+++ tests/test_intcoords/test_dihedral.py
@@ -41,9 +41,10 @@ def test_dihedral_definition():
         (1, 0, 3),
         # 4 Dihedrals
         (1, 0, 3, 2),
-        (1, 2, 3, 0),
-        (3, 0, 1, 2),
+        (0, 3, 2, 1),
+        (2, 1, 0, 3),
         (0, 1, 2, 3),
     }
 
+    import pdb; pdb.set_trace()
     assert (prim_inds - ref) == set()
