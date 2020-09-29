03e1004d11ceea0b431cb6e1e89f213bf8235493
Phillip Seeber <phillip.seeber@uni-jena.de>
2020-09-29 14:11:49 +0200

    Using nixos-20.09 channel and working Psi4

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -2,9 +2,9 @@ let
   # nixos-20.03 at 21.07.2020
   nixpkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs-channels/";
-    name = "nixos-20.03-9ea61f7";
-    rev = "9ea61f7bc4454734ffbff73c9b6173420fe3147b";
-    ref = "refs/heads/nixos-20.03";
+    name = "nixos-20.09";
+    rev = "a9226f2b3a52fcbbc5587d2fa030729e714f40fe";
+    ref = "refs/heads/nixos-20.09";
   }) { overlays = [NixWithChemistry]; };
 
   NixWithChemistry = import ./nixwithchemistry/default.nix;
