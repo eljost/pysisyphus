8b221a0a0b22c0cd41e375834111d5baac346aad
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-10-02 15:02:24 +0200

    updated afir test

--- tests/test_afir/test_afir.py
+++ tests/test_afir/test_afir.py
@@ -14,7 +14,20 @@ init_logging()
 
 
 @using("pyscf")
-def test_afir():
+@pytest.mark.parametrize(
+    "calc, ref_cycle, ccl_dist, oc_dist",
+    [
+        pytest.param(
+            PySCF(basis="6-31g*", xc="b3lyp", pal=6, charge=-1),
+            29,
+            4.79584,
+            2.67745,
+            marks=using("pyscf"),
+        ),
+        pytest.param(XTB(charge=-1), 27, 5.26346, 2.62731, marks=using("xtb")),
+    ],
+)
+def test_ohch3f_anion(calc, ref_cycle, ccl_dist, oc_dist):
     """Example (R1) from
         https://aip.scitation.org/doi/pdf/10.1063/1.3457903?class=pdf
 
@@ -23,55 +36,36 @@ def test_afir():
 
     geom = geom_loader("lib:ohch3f_anion_cs.xyz")
     # OH group is a fragment
-    fragment_indices = [(5, 6), ]
-    calc = PySCF(basis="6-31g*", xc="b3lyp", pal=2, charge=-1)
+    fragment_indices = [
+        (5, 6),
+    ]
     gamma = 100
-    afir = AFIR(calc, fragment_indices, gamma)
+    afir = AFIR(calc, fragment_indices, gamma, ignore_hydrogen=True)
     geom.set_calculator(afir)
 
-    opt = RFOptimizer(geom, dump=True, trust_max=.3)
+    opt = RFOptimizer(geom, dump=True, trust_max=0.3)
     opt.run()
 
     assert opt.is_converged
-    assert opt.cur_cycle == 47
+    assert opt.cur_cycle == ref_cycle
 
     # Broken C-Cl bond
     c3d = geom.coords3d
-    assert np.linalg.norm(c3d[0]-c3d[4]) == pytest.approx(4.805665, abs=1e-4)
+    assert np.linalg.norm(c3d[0] - c3d[4]) == pytest.approx(ccl_dist, abs=1e-4)
     # Formed O-C bond
-    assert np.linalg.norm(c3d[0]-c3d[5]) == pytest.approx(2.674330, abs=1e-4)
-
-
-@using("xtb")
-def test_xtb_afir():
-    geom = geom_loader("lib:ohch3f_anion_cs.xyz")
-    # OH group is a fragment
-    fragment_indices = [(5, 6), ]
-    calc = XTB()
-    gamma = 400
-    afir = AFIR(calc, fragment_indices, gamma)
-    geom.set_calculator(afir)
-
-    opt = RFOptimizer(geom, dump=True, trust_max=.3)
-    opt.run()
-
-    assert opt.is_converged
-    assert opt.cur_cycle == 33
-
-    # Broken C-Cl bond
-    c3d = geom.coords3d
-    assert np.linalg.norm(c3d[0]-c3d[4]) == pytest.approx(4.96192495, abs=1e-4)
-    # Formed O-C bond
-    assert np.linalg.norm(c3d[0]-c3d[5]) == pytest.approx(2.43431276, abs=1e-4)
+    assert np.linalg.norm(c3d[0] - c3d[5]) == pytest.approx(oc_dist, abs=1e-4)
 
 
 @using("xtb")
 def test_three_frag_afir():
     geom = geom_loader("lib:afir3test.xyz", coord_type="redund")
-    fragment_indices = [(0, 1, 2), (3, 4, 5, 6), ]
+    fragment_indices = [
+        (0, 1, 2),
+        (3, 4, 5, 6),
+    ]
     calc = XTB()
     gamma = 150
-    afir = AFIR(calc, fragment_indices, gamma)
+    afir = AFIR(calc, fragment_indices, gamma, ignore_hydrogen=False)
     geom.set_calculator(afir)
 
     opt = RFOptimizer(geom, dump=True, overachieve_factor=2)
@@ -82,5 +76,5 @@ def test_three_frag_afir():
     assert geom.energy == pytest.approx(-22.57975056)
 
     c3d = geom.coords3d
-    assert np.linalg.norm(c3d[3]-c3d[9]) == pytest.approx(2.6099288505)
-    assert np.linalg.norm(c3d[2]-c3d[0]) == pytest.approx(3.96382029)
+    assert np.linalg.norm(c3d[3] - c3d[9]) == pytest.approx(2.6099288505)
+    assert np.linalg.norm(c3d[2] - c3d[0]) == pytest.approx(3.96382029)
