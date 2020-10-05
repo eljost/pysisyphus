7adf62f6039076187e2db0fc71567f7c43afb151
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-10-05 07:07:09 +0200

    added support for ts line search in rsirfoptimizer

--- pysisyphus/tsoptimizers/RSIRFOptimizer.py
+++ pysisyphus/tsoptimizers/RSIRFOptimizer.py
@@ -8,15 +8,26 @@ from pysisyphus.tsoptimizers.TSHessianOptimizer import TSHessianOptimizer
 
 
 class RSIRFOptimizer(TSHessianOptimizer):
-
     def optimize(self):
         energy, gradient, H, eigvals, eigvecs, resetted = self.housekeeping()
         self.update_ts_mode(eigvals, eigvecs)
 
-        self.log( "Using projection to construct image potential gradient "
-                  f"and hessian for root {self.root}."
+        # Transform gradient to eigensystem of hessian
+        gradient_trans = eigvecs.T.dot(gradient)
+        # Minimize energy along all modes, except the TS-mode
+        min_indices = [i for i in range(gradient_trans.size) if i != self.root]
+        # Get line search steps, if requested.
+        ip_step_trans, ip_gradient_trans = self.step_and_grad_from_line_search(
+            energy, gradient_trans, eigvecs, min_indices
         )
-        trans_vec = eigvecs[:,self.root]
+        ip_gradient = eigvecs.dot(ip_gradient_trans)
+        ip_step = eigvecs.dot(ip_step_trans)
+
+        self.log(
+            "Using projection to construct image potential gradient "
+            f"and hessian for root {self.root}."
+        )
+        trans_vec = eigvecs[:, self.root]
         # Projection matrix to construct g* and H*
         P = np.eye(self.geometry.coords.size) - 2 * np.outer(trans_vec, trans_vec)
         H_star = P.dot(H)
@@ -24,8 +35,10 @@ class RSIRFOptimizer(TSHessianOptimizer):
         # Neglect small eigenvalues
         eigvals_, eigvecs_ = self.filter_small_eigvals(eigvals_, eigvecs_)
 
-        grad_star = P.dot(gradient)
+        # grad_star = P.dot(gradient)
+        grad_star = P.dot(ip_gradient)
         step = self.get_rs_step(eigvals_, eigvecs_, grad_star, name="RS-I-RFO")
+        step += ip_step
 
         quadratic_prediction = step @ gradient + 0.5 * step @ self.H @ step
         rfo_prediction = quadratic_prediction / (1 + step @ step)
