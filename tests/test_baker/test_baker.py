0f0ab953c4e78300772add2fedc139848ec8dac6
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-10-06 15:25:48 +0200

    added adapt step function to RFOptimizer, disabled by default

--- tests/test_baker/test_baker.py
+++ tests/test_baker/test_baker.py
@@ -19,6 +19,7 @@ from pysisyphus.testing import using_pyscf
 def test_baker_gs_opt(name, geom, ref_energy, results_bag):
     opt_kwargs = {
         "thresh": "baker",
+        "adapt_step_func": False,
     }
     print(f"@Running {name}")
     pal = min(os.cpu_count(), 4)
