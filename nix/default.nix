fb5b133463b14713ca9a9224f7bb48d4a1e6056d
Phillip Seeber <phillip.seeber@uni-jena.de>
2020-10-08 14:55:28 +0200

    ASE, QCEngine are added, also NWChem and GAMESS for QC-Engine

--- nix/default.nix
+++ nix/default.nix
@@ -4,5 +4,6 @@ in  with nixpkgs; python3Packages.callPackage ./pysisyphus.nix {
       orca = if config.orca then orca else null;
       turbomole = if config.turbomole then turbomole else null;
       gaussian = if config.gaussian then gaussian else null;
+      gamess = if config.gamess then gamess else null;
       mopac = null;
     }
