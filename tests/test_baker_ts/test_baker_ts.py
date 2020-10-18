18022952938957339abc87da484f5b9eaea959b5
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-10-18 15:37:58 +0200

    full baker_ts set

--- tests/test_baker_ts/test_baker_ts.py
+++ tests/test_baker_ts/test_baker_ts.py
@@ -29,11 +29,11 @@ def get_geoms():
         "17_claisen.xyz",
         "18_silyene_insertion.xyz",
         "19_hnccs.xyz",
-        # "20_hconh3_cation.xyz",
+        "20_hconh3_cation.xyz",
         "21_acrolein_rot.xyz",
-        # "22_hconhoh.xyz",
+        "22_hconhoh.xyz",
         "23_hcn_h2.xyz",
-        # "24_h2cnh.xyz",
+        "24_h2cnh.xyz",
         "25_hcnh2.xyz",
     )
     alpha_negative = ()
@@ -42,7 +42,9 @@ def get_geoms():
         "11_trans_butadiene.xyz",
     )
     only = (
-        # "18_silyene_insertion.xyz",
+        "20_hconh3_cation.xyz",
+        "22_hconhoh.xyz",
+        "24_h2cnh.xyz",
     )
     use = (
         # fails,
