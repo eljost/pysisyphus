46eb17bcb63463cc5e9c5e38ceaf1daf592561ca
Phillip Seeber <phillip.seeber@uni-jena.de>
2020-10-19 11:43:44 +0200

    Back to the official nixos-20.09 channel which now includes fixes for fsspec

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,22 +1,23 @@
 let
-  # nixos-20.09 at 07.10.2020
-  /*
+  # nixos-20.09 at 19.10.2020
   nixpkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs";
     name = "nixos-20.09";
-    rev = "7badbf18c45b7490d893452beb8950d966327831";
+    rev = "51aaa3fa1b69559456f9bd4968bd5b179a784f67";
     ref = "refs/heads/nixos-20.09";
   }) { overlays = [NixWithChemistry]; };
-  */
+
   /*
   Containts the fixes for fsspec python package already.
   */
+  /*
   nixpkgs = import (builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs";
     name = "release-20.09";
     rev = "4188bf40dd247f7af3d6105f1c7097a4478ad376";
     ref = "refs/heads/release-20.09";
   }) { overlays = [NixWithChemistry]; };
+  */
 
   NixWithChemistry = import ./nixwithchemistry/default.nix;
 
