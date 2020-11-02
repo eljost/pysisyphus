6794ee283a449c80bcfc31e2f293ddc6e875c4d5
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-11-02 13:15:13 +0100

    updated geom_from_library to geom_loader

--- tests/test_precon/test_precon.py
+++ tests/test_precon/test_precon.py
@@ -1,10 +1,10 @@
 import pytest
 
-from pysisyphus.helpers import geom_from_library
+from pysisyphus.helpers import geom_loader
 from pysisyphus.optimizers.PreconSteepestDescent import PreconSteepestDescent
 from pysisyphus.optimizers.PreconLBFGS import PreconLBFGS
 from pysisyphus.calculators.PySCF import PySCF
-from pysisyphus.calculators import Gaussian16
+from pysisyphus.calculators import Gaussian16, XTB
 from pysisyphus.testing import using
 
 
@@ -19,7 +19,7 @@ from pysisyphus.testing import using
     ]
 )
 def test_water_hf_precon_opt(opt_cls, precon, ref_cycles):
-    geom = geom_from_library("h2o_shaken.xyz")
+    geom = geom_loader("h2o_shaken.xyz")
     calc = PySCF(basis="sto3g")
     geom.set_calculator(calc)
 
@@ -42,7 +42,7 @@ def test_water_hf_precon_opt(opt_cls, precon, ref_cycles):
     ]
 )
 def test_menthone(opt_cls, precon, ref_cycles):
-    geom = geom_from_library("baker/menthone.xyz")
+    geom = geom_loader("lib:baker/menthone.xyz")
     calc = Gaussian16("PM6", pal=2)
     geom.set_calculator(calc)
 
@@ -57,3 +57,22 @@ def test_menthone(opt_cls, precon, ref_cycles):
 
     assert opt.is_converged
     assert opt.cur_cycle == ref_cycles
+
+
+@using("xtb")
+def test_biaryl_precon():
+    geom = geom_loader("lib:split.image_021.xyz")
+    calc = XTB(pal=2)
+    geom.set_calculator(calc)
+
+    opt_kwargs = {
+        "max_cycles": 100,
+        "precon": True,
+        "overachieve_factor": 3.,
+    }
+
+    opt = PreconLBFGS(geom, **opt_kwargs)
+    opt.run()
+
+    # assert opt.is_converged
+    # assert opt.cur_cycle == ref_cycles
