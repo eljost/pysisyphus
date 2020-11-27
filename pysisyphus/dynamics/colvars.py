30ca71ab46a70fb19eec60cfd2a585e304578a0a
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2020-11-27 09:01:52 +0100

    added bend colvar and test

--- pysisyphus/dynamics/colvars.py
+++ pysisyphus/dynamics/colvars.py
@@ -5,20 +5,24 @@ import numpy as np
 import autograd
 import autograd.numpy as anp
 
-from pysisyphus.intcoords.derivatives import dq_b
+from pysisyphus.intcoords.derivatives import dq_b, dq_a
 
 
 class Colvar(metaclass=abc.ABCMeta):
     def __init__(self, force_agrad=False):
         try:
-            _grad = getattr(self, "_gradient")
+            getattr(self, "_gradient")
         except AttributeError:
             force_agrad = True
 
+        # Set autograd gradient method, if no _gradient forces, or if it
+        # was not implemented.
         if force_agrad:
             grad_func = autograd.grad(self.value)
+
             def wrapped(self, c3d):
                 return grad_func(c3d)
+
             self._gradient = types.MethodType(wrapped, self)
         # Store a flag to indicate use of autograd
         self.agrad = force_agrad
@@ -33,6 +37,12 @@ class Colvar(metaclass=abc.ABCMeta):
     def eval(self, c3d):
         return self.value(c3d), self.gradient(c3d)
 
+    def _wilson_gradient(self, func, c3d):
+        """Gradient of primitive internal w.r.t. Cartesians."""
+        grad = np.zeros_like(c3d)
+        grad[self.indices] = func(*c3d[self.indices].flatten()).reshape(-1, 3)
+        return grad
+
 
 class CVDistance(Colvar):
     def __init__(self, indices, **kwargs):
@@ -44,6 +54,24 @@ class CVDistance(Colvar):
         return anp.linalg.norm(c3d[self.i] - c3d[self.j])
 
     def _gradient(self, c3d):
-        grad = np.zeros_like(c3d)
-        grad[self.indices] = dq_b(*c3d[self.indices].flatten()).reshape(-1, 3)
-        return grad
+        return self._wilson_gradient(dq_b, c3d)
+
+
+class CVBend(Colvar):
+    def __init__(self, indices, **kwargs):
+        self.indices = list(indices)
+        self.i, self.j, self.k = self.indices
+        super().__init__(**kwargs)
+
+    def value(self, c3d):
+        u_dash = c3d[self.i] - c3d[self.j]
+        v_dash = c3d[self.k] - c3d[self.j]
+        u_norm = anp.linalg.norm(u_dash)
+        v_norm = anp.linalg.norm(v_dash)
+        u = u_dash / u_norm
+        v = v_dash / v_norm
+        rad = anp.arccos(anp.dot(u, v))
+        return rad
+
+    def _gradient(self, c3d):
+        return self._wilson_gradient(dq_a, c3d)
--- tests_staging/test_colvars/test_colvars.py
+++ tests_staging/test_colvars/test_colvars.py
@@ -1,4 +1,4 @@
-from pysisyphus.dynamics.colvars import CVDistance
+from pysisyphus.dynamics.colvars import CVDistance, CVBend
 
 import pytest
 import numpy as np
@@ -6,20 +6,29 @@ import autograd
 import autograd.numpy as anp
 
 
-def test_cvdistance():
+# def test_cvdistance():
+@pytest.mark.parametrize(
+    "cls, indices, ref_val", [
+        (CVDistance, (0, 1), 1.0),
+        (CVBend, (0, 1, 2), np.pi / 2),
+    ]
+)
+def test_prim_internal_cv(cls, indices, ref_val):
     coords = np.array((
-        (1.0, 0.0, 0.0),
-        (0.0, 0.0, 0.0),
+        (0.0,  0.0, 0.0),
+        (1.0,  0.0, 0.0),
+        (1.0, -1.0, 0.0),
+        (2.0, -1.0, 0.0),
     ))
-    indices = (0, 1)
-    cv = CVDistance(indices)
+
+    # Check if autograd gradient is used
+    cv = cls(indices)
     assert cv.agrad == False
-    agrad_cv = CVDistance(indices, force_agrad=True)
+    agrad_cv = cls(indices, force_agrad=True)
     assert agrad_cv.agrad
 
-    ref_val = 1.0
     val, grad = cv.eval(coords)
-    agrad_val, agrad_grad = agrad_cv.eval(coords)
+    agrad_grad = agrad_cv.gradient(coords)
 
     assert val == pytest.approx(ref_val)
-    np.testing.assert_allclose(grad, agrad_grad)#coords)
+    np.testing.assert_allclose(grad, agrad_grad)
