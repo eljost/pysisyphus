61a3f8a126a45f756bdb56fc8849c832e3e35e31
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-01-10 17:04:06 +0100

    added missing file

---
+++ pysisyphus/benchmark_sets.py
@@ -0,0 +1,27 @@
+def get_s22_fns():
+    """https://doi.org/10.1039/B600027D"""
+    fns = (
+        "00_adenine_thymine_wc.xyz",
+        "01_adenine_thymine_stack.xyz",
+        "02_ammonia_dimer.xyz",
+        "03_water_dimer.xyz",
+        "04_methane_dimer.xyz",
+        "05_ethene_dimer.xyz",
+        "06_ethene_ethine.xyz",
+        "07_formic_acid_dimer.xyz",
+        "08_formamide_dimer.xyz",
+        "09_benzene_water.xyz",
+        "10_benzene_ammonia.xyz",
+        "11_benzene_methane.xyz",
+        "12_benzene_dimer.xyz",
+        "13_benzene_dimer.xyz",
+        "14_indole_benzene_t-shape.xyz",
+        "15_indole_benzene_stack.xyz",
+        "16_pyrazine_dimer.xyz",
+        "17_2-pyridoxine_2-aminopyridine.xyz",
+        "18_phenol_dimer.xyz",
+        "19_uracil_dimer_stack.xyz",
+        "20_uracil_dimer_hb.xyz",
+        "21_benzene_hcn.xyz",
+    )
+    return fns
