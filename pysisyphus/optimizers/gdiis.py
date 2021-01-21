ee861fb60d733181bf2fe5a1315b970b83236159
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-01-21 13:45:35 +0100

    black and allow disabling direction test

--- pysisyphus/optimizers/gdiis.py
+++ pysisyphus/optimizers/gdiis.py
@@ -33,12 +33,12 @@ COS_CUTOFFS = {
     8: 0.49,
     9: 0.41,
 }
-DIISResult = namedtuple("DIISResult",
-                         "coeffs coords forces energy N type"
-)
+DIISResult = namedtuple("DIISResult", "coeffs coords forces energy N type")
 
 
 logger = logging.getLogger("optimizer")
+
+
 def log(msg):
     logger.debug(msg)
 
@@ -51,32 +51,32 @@ def valid_diis_direction(diis_step, ref_step, use):
 
 
 def from_coeffs(vec, coeffs):
-    return np.sum(coeffs[:,None] * vec[::-1][:len(coeffs)], axis=0)
+    return np.sum(coeffs[:, None] * vec[::-1][: len(coeffs)], axis=0)
 
 
 def diis_result(coeffs, coords, forces, energy=None, prefix=""):
     diis_coords = from_coeffs(coords, coeffs)
     diis_forces = from_coeffs(forces, coeffs)
     diis_result = DIISResult(
-                        coeffs=coeffs,
-                        coords=diis_coords,
-                        forces=diis_forces,
-                        energy=energy,
-                        N=len(coeffs),
-                        type=f"{prefix}DIIS",
+        coeffs=coeffs,
+        coords=diis_coords,
+        forces=diis_forces,
+        energy=energy,
+        N=len(coeffs),
+        type=f"{prefix}DIIS",
     )
     log(f"\tUsed {len(coeffs)} error vectors for {prefix}DIIS.")
     log("")
     return diis_result
 
 
-def gdiis(err_vecs, coords, forces, ref_step, max_vecs=5):
+def gdiis(err_vecs, coords, forces, ref_step, max_vecs=5, test_direction=True):
     # Scale error vectors so the smallest norm is 1
     norms = np.linalg.norm(err_vecs, axis=1)
     err_vecs = err_vecs / norms.min()
 
     valid_coeffs = None
-    for use in range(2, min(max_vecs, len(err_vecs))+1):
+    for use in range(2, min(max_vecs, len(err_vecs)) + 1):
         log(f"Trying GDIIS with {use} previous cycles.")
         use_vecs = np.array(err_vecs[::-1][:use])
 
@@ -114,13 +114,15 @@ def gdiis(err_vecs, coords, forces, ref_step, max_vecs=5):
         log(f"\tGDIIS step has valid length: {valid_length}")
 
         # Compare directions of GDIIS- and reference step
-        valid_direction = valid_diis_direction(diis_step, ref_step, use)
+        valid_direction = (
+            True
+            (if not test_direction)
+            else test_valid_diis_direction(diis_step, ref_step, use)
+        )
         log(f"\tGDIIS step has valid direction: {valid_direction}")
 
-        gdiis_valid = (valid_sums
-                       and valid_coeffs_norm
-                       and valid_direction
-                       and valid_length
+        gdiis_valid = (
+            valid_sums and valid_coeffs_norm and valid_direction and valid_length
         )
         log(f"\tGDIIS step is valid: {gdiis_valid}")
         if not gdiis_valid:
@@ -133,8 +135,8 @@ def gdiis(err_vecs, coords, forces, ref_step, max_vecs=5):
         return None
 
     # if len(valid_coeffs) is 2:
-        # print("GDIIS with only 2 cycles. Skipping! Return None")
-        # return None
+    # print("GDIIS with only 2 cycles. Skipping! Return None")
+    # return None
 
     return diis_result(valid_coeffs, coords, forces, prefix="G")
 
@@ -152,51 +154,60 @@ def gediis(coords, energies, forces, hessian=None, max_vecs=3):
     Rjfi = np.einsum("jk,ik->ji", R, f)
 
     def x2c(x):
-        return x**2 / (x**2).sum()
+        return x ** 2 / (x ** 2).sum()
 
     # def fun(xs):
-        # """Naive implementation with loops."""
-        # cs = x2c(xs)
-        # first = (cs*E).sum()
-        # sec = 0.
-        # for i, ci in enumerate(cs):
-            # for j, cj in enumerate(cs):
-                # sec += ci * cj * (f[j] - f[i]) @ (R[i] - R[j])
-        # return first - 1/2 * sec
+    # """Naive implementation with loops."""
+    # cs = x2c(xs)
+    # first = (cs*E).sum()
+    # sec = 0.
+    # for i, ci in enumerate(cs):
+    # for j, cj in enumerate(cs):
+    # sec += ci * cj * (f[j] - f[i]) @ (R[i] - R[j])
+    # return first - 1/2 * sec
 
     # def fun(xs):
-        # """Recalculation of all values in every call."""
-        # cs = x2c(xs)
-        # return anp.sum(cs*E) - anp.einsum("i,j,jk,ik", cs, cs, R, f) + anp.einsum("i,ij,ij", cs, R, f)
+    # """Recalculation of all values in every call."""
+    # cs = x2c(xs)
+    # return anp.sum(cs*E) - anp.einsum("i,j,jk,ik", cs, cs, R, f) + anp.einsum("i,ij,ij", cs, R, f)
 
     # Using precomputed values from above in 'fun()'
     if hessian is None:
+
         def fun(xs):
             """Eq. (6) from [3]."""
             cs = x2c(xs)
-            return anp.sum(cs*E) - anp.sum(anp.outer(cs, cs)*Rjfi) + (cs * Rifi).sum()
+            return (
+                anp.sum(cs * E) - anp.sum(anp.outer(cs, cs) * Rjfi) + (cs * Rifi).sum()
+            )
+
     else:
         hessian_inv = np.linalg.pinv(hessian, rcond=1e-6)
         # It doesn't matter if we use forces or gradients, as the signs will cancel.
         # gHig = 0.5 * np.einsum("ki,ji,ki->k", f, hessian_inv, f)
         gHig = np.einsum("ki,ji,ki->k", f, hessian_inv, f)
+
         def fun(xs):
             """Eq. (5) from [4]."""
             cs = x2c(xs)
             # Consider the hessian in the first term
-            return 0.5 * anp.sum(cs*gHig) - anp.sum(anp.outer(cs, cs)*Rjfi) + (cs * Rifi).sum()
+            return (
+                0.5 * anp.sum(cs * gHig)
+                - anp.sum(anp.outer(cs, cs) * Rjfi)
+                + (cs * Rifi).sum()
+            )
 
     # def fun(xs):
-        # cs = x2c(xs)
-        # cRjfi = anp.einsum("j,jk,ik->ji", cs, R, f).sum(axis=0)
-        # return anp.sum(
-            # cs * (E + cRjfi + Rifi)
-        # )
+    # cs = x2c(xs)
+    # cRjfi = anp.einsum("j,jk,ik->ji", cs, R, f).sum(axis=0)
+    # return anp.sum(
+    # cs * (E + cRjfi + Rifi)
+    # )
 
     jac = grad(fun)
 
     x0 = np.ones(use) / use
-    res = minimize(fun, x0=x0, jac=jac)#, tol=1e-7)
+    res = minimize(fun, x0=x0, jac=jac)  # , tol=1e-7)
     # print(res)
     # print("final x", res.x)
     # x = res.x
@@ -212,6 +223,8 @@ def gediis(coords, energies, forces, hessian=None, max_vecs=3):
     # en_ = (E * coeffs).sum()
     # import pdb; pdb.set_trace()
     if (hessian is None) and (en_ >= E[0]):
-        print(f"GEDIIS converged, but proposed energy is above current energy! Returning None")
+        print(
+            f"GEDIIS converged, but proposed energy is above current energy! Returning None"
+        )
         return None
     return diis_result(coeffs, coords, forces, energy=en_, prefix="GE")
