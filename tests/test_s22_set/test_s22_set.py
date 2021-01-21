23c8667117207fa7bd0d6d134fe149741162dd73
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-01-21 14:49:52 +0100

    expanded test

--- tests/test_s22_set/test_s22_set.py
+++ tests/test_s22_set/test_s22_set.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 import shutil
+import sys
 
 import pytest
 
@@ -14,7 +16,7 @@ S22Bm = Benchmark("s22", coord_type="redund")
 
 @using("orca")
 @pytest.mark.parametrize("fn, geom, charge, mult, ref_energy", S22Bm.geom_iter)
-def test_s22_set(fn, geom, charge, mult, ref_energy, results_bag):
+def test_s22_set(fn, geom, charge, mult, ref_energy, this_dir):
     calc = ORCA(
         keywords="RI-MP2 6-31G** def2-SVP/C tightscf",
         pal=6,
@@ -34,11 +36,18 @@ def test_s22_set(fn, geom, charge, mult, ref_energy, results_bag):
     opt.run()
 
     id_ = int(fn[:2])
-    def keep(fn):
-        shutil.copy(fn, Path("backup_gdiis_no_direction_test") / f"{fn}.{id_:02}")
+    backup_dir  = this_dir / "backup_gdiis_no_direction_test"
+    if not backup_dir.exists():
+        os.mkdir(backup_dir)
 
-    keep("optimizer.log")
+    def keep(fn, rm=False):
+        shutil.copy(fn, backup_dir / f"{fn}.{id_:02}")
+        if rm:
+            os.remove(fn)
+
+    keep("optimizer.log", rm=True)
     keep("optimization.trj")
+    keep("final_geometry.xyz")
     keep("optimization.h5")
 
     assert opt.is_converged
