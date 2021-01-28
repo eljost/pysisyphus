260cf69b90a5170361fd52666f55321b5a482d38
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-01-28 14:07:59 +0100

    tweaks

--- tests/test_zimmerman_xtb/test_zimmerman_xtb.py
+++ tests/test_zimmerman_xtb/test_zimmerman_xtb.py
@@ -19,8 +19,6 @@ ZmXTB = Benchmark("zimmerman_xtb")
 def test_zimmerman_xtb_gsm(fn, geoms, ref_energy, results_bag):
     start, ts_ref, end = geoms
     id_ = fn[:2]
-    # if id_ in (45, 71):
-    # raise Exception("Disabled!")
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         tmp_path = Path(tmp_dir)
@@ -42,7 +40,7 @@ def test_zimmerman_xtb_gsm(fn, geoms, ref_energy, results_bag):
                 "fix_ends": True,
                 "max_nodes": 11,
                 "reparam_check": "rms",
-                "perp_thresh": 0.075,
+                # "perp_thresh": 0.075,
                 "climb": True,
                 "climb_rms": 0.0075,
                 "climb_lanczos": True,
@@ -52,8 +50,9 @@ def test_zimmerman_xtb_gsm(fn, geoms, ref_energy, results_bag):
             "opt": {
                 "type": "string",
                 "lbfgs_when_full": True,
+                "max_step": 0.25,
                 "keep_last": 10,
-                "rms_force": 0.005,
+                "rms_force": 0.0035,
                 "rms_force_only": True,
                 "double_damp": True,
             },
@@ -61,7 +60,7 @@ def test_zimmerman_xtb_gsm(fn, geoms, ref_energy, results_bag):
                 "type": "rsirfo",
                 "do_hess": True,
                 "hessian_recalc": 3,
-                "thresh": "gau",
+                # "thresh": "gau",
                 "trust_max": 0.3,
                 "max_cycles": 100,
                 "root": 0,
@@ -82,12 +81,13 @@ def test_zimmerman_xtb_gsm(fn, geoms, ref_energy, results_bag):
         rmsd = ts_ref.rmsd(ts_geom)
         diff = ts_ref_energy - ts_energy
         cmt = "Ref" if diff < 0.0 else " TS"
-        print(f"RMSD: {rmsd:.4f}")
+        rmsd_fmt = " >12.4f"
+        print(f"RMSD: {rmsd:{rmsd_fmt}}")
         print(f" TS energy: {ts_energy:.6f}")
         print(f"Ref energy: {ts_ref_energy:.6f}")
         print(f"      Diff: {diff:.6f}")
         print(
-            f"@@@{id_} COMPARE@@@: rmsd={rmsd:.4f}, ΔE= {diff: .6f} {cmt} is lower, "
+            f"@@@{id_} COMPARE@@@: rmsd={rmsd:{rmsd_fmt}}, ΔE= {diff: .6f} {cmt} is lower, "
             f"Ref: {ts_ref_imag: >8.1f}, TS: {ts_imag: >8.1f} cm⁻¹"
         )
 
