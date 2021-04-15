b89808d4c18a304c01e123572476cc4c029e0b72
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-04-15 08:47:24 +0200

    generalized do_endopt_ts_barries to support multiple geoms and one-sided barriers

--- tests/test_endopt_barriers/test_endopt_barriers.py
+++ tests/test_endopt_barriers/test_endopt_barriers.py
@@ -35,16 +35,24 @@ def test_run_dict_endopt_barriers():
     assert results.ts_geom.energy == pytest.approx(-5.38737424)
 
 
-def do_endopt_ts_barriers(end_geoms, end_fns, ts_geom, do_thermo=False, T=298.15):
-    import numpy as np
+import numpy as np
 
-    from pysisyphus.constants import AU2KJPERMOL
-    from pysisyphus.helpers import highlight_text
-    from pysisyphus.thermo import get_thermoanalysis, print_thermoanalysis
+from pysisyphus.constants import AU2KJPERMOL
+from pysisyphus.helpers import highlight_text
+from pysisyphus.thermo import get_thermoanalysis, print_thermoanalysis
 
+
+def do_endopt_ts_barriers(
+    ts_geom,
+    left_geoms,
+    right_geoms=None,
+    do_thermo=False,
+    T=298.15,
+):
     print(highlight_text("Barrier heights after end optimizations"))
 
-    forward_geoms, backward_geoms = end_geoms
+    if right_geoms is None:
+        right_geoms = []
 
     # Gibbs free energies
     if do_thermo:
@@ -59,56 +67,58 @@ def do_endopt_ts_barriers(end_geoms, end_fns, ts_geom, do_thermo=False, T=298.15
 
         ts_thermo = get_thermo(ts_geom, "TS")
         ts_energy = ts_thermo.G
-        forward_thermos = [
-            get_thermo(geom, f"forward_{i:03d}") for i, geom in enumerate(forward_geoms)
+        left_thermos = [
+            get_thermo(geom, f"forward_{i:03d}") for i, geom in enumerate(left_geoms)
         ]
-        forward_energies = [thermo.G for thermo in forward_thermos]
-        backward_thermos = [
-            get_thermo(geom, f"backward_{i:03d}")
-            for i, geom in enumerate(backward_geoms)
+        left_energies = [thermo.G for thermo in left_thermos]
+        right_thermos = [
+            get_thermo(geom, f"backward_{i:03d}") for i, geom in enumerate(right_geoms)
         ]
-        backward_energies = [thermo.G for thermo in backward_thermos]
+        right_energies = [thermo.G for thermo in right_thermos]
     # Electronic energies only
     else:
         print("Thermochemical corrections are NOT included!")
         en_key = "energy"
 
         ts_energy = ts_geom.energy
-        forward_geom, backward_geom = end_geoms
-        forward_energies = [geom.energy for geom in forward_geoms]
-        backward_energies = [geom.energy for geom in backward_geoms]
+        left_energies = [geom.energy for geom in left_geoms]
+        right_energies = [geom.energy for geom in right_geoms]
 
-    forward_energy = sum(forward_energies)
-    backward_energy = sum(backward_energies)
-    energies = np.array((forward_energy, ts_energy, backward_energy))
+    left_energy = sum(left_energies)
+    right_energy = sum(right_energies)
+    energies = [left_energy, ts_energy]
+    # TS is always only 1 geometry
+    geom_nums = [len(left_geoms), 1]
+    if right_geoms:
+        energies.append(right_energy)
+        geom_nums.append(len(right_geoms))
+        fns = ("Forward", "TS", "Backward")
+    else:
+        fns = ("Downhill", "TS")
+    max_len = max(len(s) for s in fns)
+    energies = np.array(energies)
     energies -= energies.min()
     min_ind = energies.argmin()
     energies *= AU2KJPERMOL
-
-    forward_fn, backward_fn = "Forward", "Backward"
-    fns = (forward_fn, "TS", backward_fn)
-    max_len = max(len(s) for s in fns)
 
     print(highlight_text("Barriers", level=1))
     print()
     print(f"Minimum {en_key} of {energies[min_ind]} kJ mol⁻¹ at '{fns[min_ind]}'.")
     print()
-    for fn, en in zip(fns, energies):
-        print(f"\t{fn:>{max_len}s}: {en:>8.2f} kJ mol⁻¹")
-        "Σ"
+    for fn, en, gnum in zip(fns, energies, geom_nums):
+        geom_str = "geometry" if gnum == 1 else "geometries"
+        is_sum = "" if gnum == 1 else "Σ"
+        print(f"\t{is_sum}{fn:>{max_len}s}: {en:>8.2f} kJ mol⁻¹ ({gnum} {geom_str})")
     print()
 
 
 @using("thermoanalysis")
 @using("xtb")
 @pytest.mark.parametrize(
-    "do_thermo",
-    [
-        True,
-        False,
-    ],
+    "do_thermo, downhill",
+    [(True, True), (True, False), (False, True), (False, False)],
 )
-def test_do_endopt_ts_barriers(this_dir, do_thermo):
+def test_do_endopt_ts_barriers(this_dir, do_thermo, downhill):
     def get_geom(fn):
         geom = geom_loader(this_dir / fn)
         geom.set_calculator(XTB(pal=2))
@@ -119,8 +129,14 @@ def test_do_endopt_ts_barriers(this_dir, do_thermo):
     bw_fn = "hcn_backward_end_opt.xyz"
     forward_geom = get_geom(fw_fn)
     backward_geom = get_geom(bw_fn)
-    end_geoms = [forward_geom], [backward_geom]
     end_fns = (fw_fn, bw_fn)
-    do_endopt_ts_barriers(end_geoms, end_fns, ts_geom, do_thermo=do_thermo)
+    left_geoms = [forward_geom]
+    if downhill:
+        right_geoms = None
+    else:
+        right_geoms = [backward_geom]
+    # do_endopt_ts_barriers(end_geoms, end_fns, ts_geom, do_thermo=do_thermo)
+    print("@right", right_geoms)
+    do_endopt_ts_barriers(ts_geom, left_geoms, right_geoms, do_thermo=do_thermo)
     # modify run_endopt in run.py in a way that it returns forwad_endopt
     # and backward_endopt separately
