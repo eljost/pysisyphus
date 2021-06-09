55cfee0414ad68793f6a57ce98a5586bc2864ab4
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-06-09 09:54:30 +0200

    allow it.permutations in HardSphereCalc and bugfix (H vs. of H_)

--- pysisyphus/calculators/HardSphereCalculator.py
+++ pysisyphus/calculators/HardSphereCalculator.py
@@ -9,7 +9,7 @@ from pysisyphus.helpers_pure import get_molecular_radius
 
 
 class HardSphereCalculator:
-    def __init__(self, geom, frag_lists, kappa=1.0):
+    def __init__(self, geom, frag_lists, kappa=1.0, permutations=False):
         """Intra-Image Inter-Molecular Hard-Sphere force.
 
         See A.2. S_2 in [1], Eqs. (A1).
@@ -19,7 +19,9 @@ class HardSphereCalculator:
 
         self.frag_num = len(self.frag_lists)
         self.frag_sizes = np.array([len(frag) for frag in self.frag_lists])
-        self.pair_inds = list(it.combinations(range(self.frag_num), 2))
+        self.pair_inds = np.array(list(it.combinations(range(self.frag_num), 2)))
+        it_func = it.permutations if permutations else it.combinations
+        self.pair_inds = np.array(list(it_func(range(self.frag_num), 2)))
         self.frag_inds = np.array([m for m, n in self.pair_inds])
 
         c3d = geom.coords3d
@@ -29,31 +31,28 @@ class HardSphereCalculator:
             [self.frag_radii[i] + self.frag_radii[j] for i, j in self.pair_inds]
         )
 
-    def get_energy(self, atoms, coords, kappa=None):
-        return {"energy": 1}
-
     def get_forces(self, atoms, coords, kappa=None):
         if kappa is None:
             kappa = self.kappa
         c3d = coords.reshape(-1, 3)
         centroids = np.array([c3d[frag].mean(axis=0) for frag in self.frag_lists])
-        mm, nn = np.triu_indices(self.frag_num, k=1)
+        mm, nn = np.array(self.pair_inds).T
         gdiffs = centroids[mm] - centroids[nn]
         gnorms = np.linalg.norm(gdiffs, axis=1)
         H = (gnorms < self.radii_sums).astype(int)
         N = np.zeros_like(H)
         for frag_ind, H_ in zip(self.frag_inds, H):
-            N[frag_ind] += H
+            N[frag_ind] += H_
         nonzero = H > 0
         N *= 3 * self.frag_sizes[self.frag_inds]
         phi = np.where(N > 0, kappa / N * (gnorms - self.radii_sums), 0)
-        frag_forces = phi * H * gdiffs / gnorms
+        frag_forces = (phi * H / gnorms)[:, None] * gdiffs
         forces = np.zeros_like(c3d)
         # Distribute forces onto fragments
         for frag_ind, ff in zip(self.frag_inds, frag_forces):
             forces[self.frag_lists[frag_ind]] += ff
         # As far as I can tell so far this is actually more like a gradient.
-        # as if we step along the "forces" direction the force increases
-        # and ^
+        # Moving into direction "forces" just increases it, so we multiply with
+        # -1 to get actual forces.
         forces = -forces.flatten()
         return {"energy": 1, "forces": forces}
