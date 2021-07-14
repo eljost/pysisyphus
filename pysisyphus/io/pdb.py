b03b0dfaea7e75fc45a35d149918c7e435ae2baa
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-07-14 08:46:49 +0200

    expanded PDB writer; added TER, CONECT and better atom parsing

--- pysisyphus/io/pdb.py
+++ pysisyphus/io/pdb.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import re
 import struct
 import textwrap
@@ -8,23 +9,29 @@ import numpy as np
 from pysisyphus.elem_data import KNOWN_ATOMS
 from pysisyphus.constants import ANG2BOHR
 from pysisyphus.Geometry import Geometry
+from pysisyphus.helpers_pure import chunks
+from pysisyphus.intcoords.setup_fast import find_bonds
 
 
-AMINOS = "ALA CYS ASP GLU PHE GLY HIS ILE LYS LEU" \
-         "MET ASN PRO GLN ARG SER THR VAL TRP TYR".split()
+AMINOS = (
+    "ALA CYS ASP GLU PHE GLY HIS ILE LYS LEU"
+    "MET ASN PRO GLN ARG SER THR VAL TRP TYR".split()
+)
 
 
 def get_parser(widths):
-    fmt = " ".join('{}{}'.format(width, "s") for width in widths)
+    fmt = " ".join("{}{}".format(width, "s") for width in widths)
     fieldstruct = struct.Struct(fmt)
     parse = lambda line: tuple(s.decode() for s in fieldstruct.unpack(line.encode()))
     return parse
 
-STRIP_RE = re.compile("[\d\s]*")
+
+STRIP_RE = re.compile(r"[\d\s]*")
 NAME_MAP = {
     "hh": "H",
     "he": "H",
 }
+
 
 def parse_atom_name(name):
     org_name = name
@@ -82,7 +89,7 @@ def parse_pdb(fn):
         7  Num. of SITE
         8  numXform
         9  Num. of atomic coordinates (HETATM + ATOMS)
-        10 Num. of CONECT 
+        10 Num. of CONECT
         11 Num. of SEQRES
     """
     master_widths = (6, 9, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
@@ -132,58 +139,102 @@ def geom_from_pdb(fn, **kwargs):
     return geom
 
 
-def atoms_coords_to_pdb_str(atoms, coords, fragments=None):
-    coords3d = coords.reshape(-1, 3)
+def get_conect_lines(atoms, coords):
+    bonds = find_bonds(atoms, coords)
+    # PDB indexing is 1-based
+    bonds += 1
+    # Bring in a suitable order for CONECT entries
+    bonds_org = bonds.copy()
+    bonds.sort(axis=1)
+    conect = OrderedDict()
+    for from_, to_ in bonds:
+        conect.setdefault(from_, list()).append(to_)
 
-    coord_fmt = "{: >8.3f}"*3
+    conect_lines = list()
+    for from_, to_ in conect.items():
+        to_iter = chunks(sorted(to_), 4)
+        for to_chunk in to_iter:
+            conect_lines.append([from_] + to_chunk)
+    return conect_lines
+
+
+def atoms_coords_to_pdb_str(atoms, coords, fragments=None, resname="", conect=True):
+    coords3d = coords.reshape(-1, 3)
+    coords3d_ang = coords3d / ANG2BOHR
+
+    coord_fmt = "{: >8.3f}" * 3
     #            serial  name  altLoc      chainID     iCode
-    #                                resName     resSeq           
-    fmt = "HETATM{: >5d} {: >4}{: >1}{: >3}{: >1} {: >4d}{: >1}   "
+    #                                resName     resSeq
+    hetatm_fmt = "HETATM{: >5d} {: >4}{: >1}{: >3}{: >1} {: >4d}{: >1}   "
     #      xyz
-    fmt += coord_fmt
+    hetatm_fmt += coord_fmt
     #      occupancy tempFactor            atom
-    fmt += "{: >6.2f}{: >6.2f}" + 10*" " + "{: >2s}"
+    hetatm_fmt += "{: >6.2f}{: >6.2f}" + 10 * " " + "{: >2s}"
+    ter_fmt = "TER   {: >5d}      {: >3s} {:1s}{: >4d}"
 
     if fragments is None:
-        fragments = [range(len(atoms)), ]
+        fragments = [
+            range(len(atoms)),
+        ]
 
     # Fixed for now
     altLoc = ""
-    resName = ""
+    resName = resname
     chainID = ""
     iCode = ""
     occupancy = 1.0
     tempFactor = 0.0
 
+    assert len(fragments) == 1
     lines = list()
     serial = 1
-    for resSeq, fragment in enumerate(fragments):
+    for resSeq, fragment in enumerate(fragments, 1):
         for id_ in fragment:
             name = atoms[id_]
-            xyz = coords3d[id_]
-            line = fmt.format(serial, name, altLoc, resName, chainID, resSeq,
-                              iCode, *xyz, occupancy, tempFactor, name)
+            xyz = coords3d_ang[id_]
+            line = hetatm_fmt.format(
+                serial,
+                name,
+                altLoc,
+                resName,
+                chainID,
+                resSeq,
+                iCode,
+                *xyz,
+                occupancy,
+                tempFactor,
+                name,
+            )
             lines.append(line)
             serial += 1
+        lines.append(ter_fmt.format(serial, resName, chainID, resSeq))
 
-    pdb_tpl = Template(textwrap.dedent(
-    """\
+    def fmt_conect_line(conect_line):
+        return "CONECT" + ("{: >5d}" * len(conect_line)).format(*conect_line)
+
+    if conect:
+        conect_lines = get_conect_lines(atoms, coords)
+    else:
+        conect_lines = []
+    conect_str = "\n".join([fmt_conect_line(cl) for cl in conect_lines])
+
+    pdb_tpl = Template(
+        textwrap.dedent(
+            """\
     REMARK 1 Created by pysisyphus
     {%+ for line in lines -%}
     {{ line }}
-    {%+ endfor -%} 
-    END"""))
+    {%+ endfor -%}
+    {{- conect_str }}
+    END"""
+        )
+    )
 
-    pdb_str = pdb_tpl.render(lines=lines)
+    pdb_str = pdb_tpl.render(lines=lines, conect_str=conect_str)
     return pdb_str
 
 
-def geom_to_pdb_str(geom, detect_fragments=False):
-    # Convert to Å
-    coords = geom.cart_coords / ANG2BOHR
-    if len(geom.fragments) > 0:
-        raise Exception("Not yet implemented!")
-
+def geom_to_pdb_str(geom, detect_fragments=False, **kwargs):
     fragments = None
     if detect_fragments:
         try:
@@ -191,5 +242,7 @@ def geom_to_pdb_str(geom, detect_fragments=False):
         except AttributeError:
             geom_ = geom.copy(coord_type="redund")
             fragments = geom_.internal.fragments
-    pdb_str = atoms_coords_to_pdb_str(geom.atoms, coords, fragments=fragments)
+    pdb_str = atoms_coords_to_pdb_str(
+        geom.atoms, geom.coords3d, fragments=fragments, **kwargs
+    )
     return pdb_str
