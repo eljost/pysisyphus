ea07669ea39bb90812a21051bfc272c06a931201
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-07-27 12:51:08 +0200

    base OpenMM on Calculator

--- pysisyphus/calculators/OpenMM.py
+++ pysisyphus/calculators/OpenMM.py
@@ -1,17 +1,20 @@
 import numpy as np
 
 try:
-    from simtk.openmm import app
+    from simtk.openmm import app, NonbondedForce
     from simtk import openmm as mm
     from simtk import unit
 except ModuleNotFoundError:
     print("OpenMM is not installed!")
 
 from pysisyphus.constants import BOHR2ANG
+from pysisyphus.calculators.Calculator import Calculator
 
 
-class OpenMM:
-    def __init__(self, topology, params):
+class OpenMM(Calculator):
+    def __init__(self, topology, params, **kwargs):
+        super().__init__(**kwargs)
+
         self.topology = topology
         self.params = params
         self.system = self.topology.createSystem(self.params, nonbondedMethod=app.NoCutoff)
@@ -22,6 +25,27 @@ class OpenMM:
 
         self.en2au = 1 / unit.hartree / unit.AVOGADRO_CONSTANT_NA
         self.frc2au = 1 / (unit.hartree / unit.bohr) / unit.AVOGADRO_CONSTANT_NA
+
+    def report_charges(self):
+        """Total charge of the system.
+
+        Adapted from modeller.py _addIons() in OpenMM.
+        """
+
+        for i in range(self.system.getNumForces()):
+            if isinstance(self.system.getForce(i), NonbondedForce):
+                nonbonded = self.system.getForce(i)
+                break
+
+        charges = list()
+        for i in range(nonbonded.getNumParticles()):
+            nb_i = nonbonded.getParticleParameters(i)
+            charges.append(nb_i[0].value_in_unit(unit.elementary_charge))
+        charges = np.array(charges)
+        total_charge = charges.sum()
+        charges_str = np.array2string(charges, precision=4)
+        self.log(f"OpenMM charges: {charges_str}")
+        self.log(f"OpenMM system total charge: {total_charge:.6f} e")
 
     def get_energy(self, atoms, coords, **prepare_kwargs):
         results = self.get_forces(atoms, coords, **prepare_kwargs)
