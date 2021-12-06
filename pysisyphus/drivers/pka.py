5181e55bd80c172ad144af786e06e2769401e658
Johannes Steinmetzer <josteinm@posteo.de>
2021-12-06 12:33:23 +0100

    add: scripts/aciddrain.py improvements
    XTB preoptimization
    YAML dump of important quantities
    
    added test for drivers/pka.py:direct_cycle()

--- pysisyphus/drivers/pka.py
+++ pysisyphus/drivers/pka.py
@@ -1,7 +1,18 @@
 from math import log
 
+from pysisyphus.config import p_DEFAULT, T_DEFAULT
 from pysisyphus.constants import AU2KCALPERMOL, KBAU
+from pysisyphus.helpers_pure import standard_state_corr
 from pysisyphus.io import geom_from_hessian
+
+
+def G_aq_from_h5_hessian(h5_hessian, solv_en, T=T_DEFAULT, p=p_DEFAULT):
+    geom = geom_from_hessian(h5_hessian)
+    thermo = geom.get_thermoanalysis(T=T, p=p)
+    dG_solv = solv_en - thermo.U_el
+    G_aq = float(thermo.G + dG_solv)
+    return G_aq
+
 
 def direct_cycle(
     acid_h5,
@@ -11,24 +22,19 @@ def direct_cycle(
     G_aq_H=None,
     G_gas_H=-6.28,
     dG_solv_H=-265.9,
-    T=298.15,
+    T=T_DEFAULT,
+    p=p_DEFAULT,
 ):
-    def G_aq_from_hessian(h5, solv_en):
-        geom = geom_from_hessian(h5)
-        thermo = geom.get_thermoanalysis(T=T)
-        dG_solv = solv_en - thermo.U_el
-        G_aq = thermo.G + dG_solv
-        return G_aq
 
-    G_aq_acid = G_aq_from_hessian(acid_h5, acid_solv_en)
-    G_aq_base = G_aq_from_hessian(base_h5, base_solv_en)
+    G_aq_acid = G_aq_from_h5_hessian(acid_h5, acid_solv_en, T=T, p=p)
+    G_aq_base = G_aq_from_h5_hessian(base_h5, base_solv_en, T=T, p=p)
 
     if G_aq_H is None:
-        G_aq_H = (G_gas_H + dG_solv_H) / AU2KCALPERMOL
+        G_aq_H = (G_gas_H + dG_solv_H)
+    G_aq_H /= AU2KCALPERMOL
 
     dG_aq = G_aq_H + G_aq_base - G_aq_acid
-    corr = KBAU * T * log(24.46)
-    dG_aq_corr = dG_aq + corr
+    dG_aq_corr = dG_aq + standard_state_corr(T=T, p=p)
 
     pKa = dG_aq_corr / (KBAU * T * log(10))
     return pKa
--- scripts/aciddrain.py
+++ scripts/aciddrain.py
@@ -8,12 +8,11 @@ import sys
 import tempfile
 
 import luigi
-from luigi.tools.deps_tree import print_tree
 import psutil
 import yaml
 
 from pysisyphus.calculators import ORCA5, XTB
-from pysisyphus.drivers.pka import direct_cycle
+from pysisyphus.drivers.pka import direct_cycle, G_aq_from_h5_hessian
 from pysisyphus.helpers import geom_loader, do_final_hessian
 from pysisyphus.optimizers.RFOptimizer import RFOptimizer
 
@@ -81,6 +80,34 @@ class InputGeometry(Params, luigi.Task):
             handle.write(geom.as_xyz())
 
 
+class PreMinimization(Params, luigi.Task):
+    def output(self):
+        return luigi.LocalTarget(self.get_path("preopt.xyz"))
+
+    def requires(self):
+        return InputGeometry(self.id_, self.name, self.h_ind, self.is_base)
+
+    def run(self):
+        geom = geom_loader(self.input().path, coord_type="redund")
+        geom.set_calculator(
+            get_xtb_calc(charge=self.calc_charge(), out_dir=self.qm_out_dir)
+        )
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            opt = RFOptimizer(
+                geom,
+                dump=True,
+                overachieve_factor=2.0,
+                thresh="gau_loose",
+                out_dir=tmp_dir,
+                max_cycles=500,
+            )
+            opt.run()
+            assert opt.is_converged
+            with self.output().open("w") as handle:
+                handle.write(geom.as_xyz())
+
+
 class Minimization(Params, luigi.Task):
     def output(self):
         return (
@@ -89,7 +116,14 @@ class Minimization(Params, luigi.Task):
         )
 
     def requires(self):
-        return InputGeometry(self.id_, self.name, self.h_ind, self.is_base)
+        # Derive the initial base geometry from the optimized acid geometry.
+        # Maybe some cycles could be saved when the base is also pre-
+        # optimized, but things could also go wrong.
+        if self.is_base:
+            return InputGeometry(self.id_, self.name, self.h_ind, self.is_base)
+        # Only preoptimize initial acid geometry, not the base.
+        else:
+            return PreMinimization(self.id_, self.name, self.h_ind, self.is_base)
 
     def run(self):
         geom = geom_loader(self.input().path, coord_type="redund")
@@ -105,6 +139,7 @@ class Minimization(Params, luigi.Task):
                 "overachieve_factor": 4.0,
                 "thresh": "gau",
                 "out_dir": tmp_dir,
+                "max_cycles": 250,
             }
             # Iterate until no imaginary frequencies are present
             for i in range(5):
@@ -117,6 +152,7 @@ class Minimization(Params, luigi.Task):
                     **opt_kwargs,
                 )
                 opt.run()
+                assert opt.is_converged
                 #
                 xyz_out, hess_out = self.output()
                 with xyz_out.open("w") as handle:
@@ -128,11 +164,16 @@ class Minimization(Params, luigi.Task):
                 else:
                     suffix = f".{i:02d}"
                     self.backup_from_dir(tmp_dir, final_hess_fn, hess_out.path + suffix)
-                    self.backup_from_dir(tmp_dir, "final_geometry.xyz", xyz_out.path + suffix)
+                    self.backup_from_dir(
+                        tmp_dir, "final_geometry.xyz", xyz_out.path + suffix
+                    )
                     # Delete everything in tmp_dir, besides final_hess_fn, as it will be
                     # reused.
-                    files = [f for f in tmp_dir.glob("./*") if f.is_file()
-                            and f.name != final_hess_fn]
+                    files = [
+                        f
+                        for f in tmp_dir.glob("./*")
+                        if f.is_file() and f.name != final_hess_fn
+                    ]
                     for f in files:
                         os.remove(f)
             else:
@@ -158,7 +199,7 @@ class SolvEnergy(Params, luigi.Task):
 
 class DirectCycle(Params, luigi.Task):
     def output(self):
-        return luigi.LocalTarget(self.get_path("pka"))
+        return luigi.LocalTarget(self.get_path("summary.yaml"))
 
     def requires(self):
         return (
@@ -178,10 +219,25 @@ class DirectCycle(Params, luigi.Task):
             acid_solv_en = float(handle.read())
         with open(base_solv_fn.path) as handle:
             base_solv_en = float(handle.read())
-        pKa = direct_cycle(acid_h5, base_h5, acid_solv_en, base_solv_en)
+        G_aq_H = -6.28 + (-265.9)
+        pKa = direct_cycle(acid_h5, base_h5, acid_solv_en, base_solv_en, G_aq_H=G_aq_H)
         print(f"@@@ pka={pKa:.4f}")
+
+        G_acid_aq = G_aq_from_h5_hessian(acid_h5, acid_solv_en)
+        G_base_aq = G_aq_from_h5_hessian(base_h5, base_solv_en)
+        G_diss_aq = G_base_aq - G_acid_aq
+        print(f"@@@ G_diss_aq={G_diss_aq:.6f} au")
+
+        results = {
+            "name": self.name,
+            "h_ind": self.h_ind,
+            "G_acid_aq": G_acid_aq,
+            "G_base_aq": G_base_aq,
+            "G_diss_aq": G_diss_aq,
+            "pKa": pKa,
+        }
         with self.output().open("w") as handle:
-            handle.write(str(pKa))
+            yaml.dump(results, handle)
 
 
 def parse_args(args):
@@ -251,7 +307,14 @@ def run():
             **solv_kwargs,
         )
 
-    print(print_tree(cycles[0], indent="", last=True))
+    global get_xtb_calc
+
+    def get_xtb_calc(charge, out_dir):
+        return XTB(pal=pal, charge=charge, out_dir=out_dir, base_name="xtb")
+
+    # from luigi.tools.deps_tree import print_tree
+    # for cycle in cycles:
+    # print(print_tree(cycle, indent="", last=True))
 
     luigi.build(
         # (Minimization(id_=0, name="formicacid", h_ind=4, is_base=False),)
