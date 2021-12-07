c6605db17bec79d0068a08089ec1cbc87f2b7e4d
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-12-07 13:14:52 +0100

    started to work on central logging infrastructure

--- pysisyphus/__init__.py
+++ pysisyphus/__init__.py
@@ -1,2 +1,17 @@
+import logging
+import sys
+
 from . import _version
-__version__ = _version.get_versions()['version']
+
+__version__ = _version.get_versions()["version"]
+
+
+logger = logging.getLogger("pysisyphus")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("pysisyphus.log", mode="w", delay=True)
+logger.addHandler(file_handler)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+logger.addHandler(stdout_handler)
