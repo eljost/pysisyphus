e4af2f7710250bb9dcceb756d87286766bc0d2a2
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2021-12-07 13:14:52 +0100

    started to work on central logging infrastructure

--- pysisyphus/drivers/pka.py
+++ pysisyphus/drivers/pka.py
@@ -1,9 +1,14 @@
+import logging
+
 from math import log
 
 from pysisyphus.config import p_DEFAULT, T_DEFAULT
 from pysisyphus.constants import AU2KCALPERMOL, KBAU
 from pysisyphus.helpers_pure import standard_state_corr
 from pysisyphus.io import geom_from_hessian
+
+
+logger = logging.getLogger("pysisyphus")
 
 
 def G_aq_from_h5_hessian(h5_hessian, solv_en, T=T_DEFAULT, p=p_DEFAULT):
@@ -30,11 +35,22 @@ def direct_cycle(
     G_aq_base = G_aq_from_h5_hessian(base_h5, base_solv_en, T=T, p=p)
 
     if G_aq_H is None:
-        G_aq_H = (G_gas_H + dG_solv_H)
+        G_aq_H = G_gas_H + dG_solv_H
     G_aq_H /= AU2KCALPERMOL
 
     dG_aq = G_aq_H + G_aq_base - G_aq_acid
     dG_aq_corr = dG_aq + standard_state_corr(T=T, p=p)
 
     pKa = dG_aq_corr / (KBAU * T * log(10))
+
+    logger.debug(
+        f"pKa calculation using direct cycle:\n"
+        f"\tAcid HDF5: {acid_h5}\n\tBase HDF5: {base_h5}\n"
+        f"\tT={T:.4f}, p={p:.1f} Pa\n"
+        f"\tG_aq_acid={G_aq_acid:.6f} au\n"
+        f"\tG_aq_base={G_aq_base:.6f} au\n"
+        f"\tG_aq_H={G_aq_H:.6f} au\n"
+        f"\tpKa={pKa:.6f}"
+    )
+
     return pKa
