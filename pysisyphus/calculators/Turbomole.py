fix: turbomole hessian parsing was faulty. see #173
the new nprhessian parser relies on dropping everything that is
not a float.

pysisyphus/calculators/Turbomole.py
@@ -1,4 +1,4 @@
-import itertools as it
+from math import sqrt
 import os
 from pathlib import Path
 import re
@@ -475,14 +475,19 @@ class Turbomole(OverlapCalculator):
             fn = path / "nprhessian"
 
         with open(fn) as handle:
-            lines = [l.strip() for l in handle.readlines()]
-        assert lines[0] == "$nprhessian"
-        assert lines[-1] == "$end"
+            text = handle.read()
 
-        hess_lines = [line.split()[2:] for line in lines[1:-1]]
-        atom_num = int(lines[-2].split()[0])
-        hessian = np.array(list(it.chain(*hess_lines)), dtype=float)
-        hessian = hessian.reshape(atom_num, atom_num)
+        split = text.strip().split()
+        assert split[0] == "$nprhessian"
+        assert split[-1] == "$end"
+
+        def is_float(str_):
+            return "." in str_
+
+        hess_items = [item for item in split if is_float(item)]
+        coord_num = int(sqrt(len(hess_items)))
+        assert coord_num ** 2 == len(hess_items)
+        hessian = np.array(hess_items, dtype=float).reshape(-1, coord_num)
 
         energy = self.parse_energy(path)["energy"]
 

tests/test_turbomole/test_turbomole.py
@@ -41,6 +41,20 @@ def test_turbomole_hessian(geom, this_dir):
     np.testing.assert_allclose(nus[-3:], ref_nus, atol=1e-2)
 
 
+def test_turbomole_big_hessian_parsing(this_dir):
+    geom = geom_loader("lib:ch4_12.xyz")
+    control_path = this_dir / "control_path_big_hess"
+    turbo_kwargs = {
+        "control_path": control_path,
+    }
+    calc = Turbomole(**turbo_kwargs)
+    geom.set_calculator(calc)
+    results = calc.parse_hessian(path=control_path)
+
+    hessian = results["hessian"]
+    assert hessian.size == 180 ** 2
+
+
 @using("turbomole")
 @pytest.mark.parametrize(
     "control_path, ref_energy",
@@ -93,6 +107,7 @@ def test_h2o_forces(control_path, ref_energy, ref_force_norm, geom, this_dir):
     assert energy == pytest.approx(ref_energy)
 
 
+@pytest.mark.skip
 @using("turbomole")
 def test_turbomole_cos(this_dir):
     def calc_getter(charge, mult):
@@ -115,9 +130,9 @@ def test_turbomole_cos(this_dir):
     start, _, end = geoms
     images = (start, end)
     cos_kwargs = {
-            "calc_getter": gs_calc_getter,
-            "max_nodes": 9,
-            "climb": True,
+        "calc_getter": gs_calc_getter,
+        "max_nodes": 9,
+        "climb": True,
     }
     cos = GrowingString(images, calc_getter=gs_calc_getter, max_nodes=9, climb=True)
     opt_kwargs = {