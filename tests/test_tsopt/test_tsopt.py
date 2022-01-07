7eac6628cc414c6354ff795de283851183e703aa
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2022-01-07 16:09:41 +0100

    trying to fix old hessian_ref functionality ... still needs some work

--- tests/test_tsopt/test_tsopt.py
+++ tests/test_tsopt/test_tsopt.py
@@ -55,3 +55,17 @@ def test_tshessianoptimizer_kwargs(kwargs):
 
     assert opt.is_converged
     assert geom.energy == pytest.approx(-92.2460426792319)
+
+
+@using("pyscf")
+@pytest.mark.skip
+def test_hessian_ref(this_dir):
+    geom = geom_loader("lib:baker_ts/01_hcn.xyz", coord_type="cart")
+    geom.set_calculator(PySCF(basis="321g"))
+
+    hessian_ref = this_dir / "inp_hessian_ref.h5"
+    opt = RSPRFOptimizer(geom, hessian_ref=hessian_ref)
+    opt.run()
+
+    assert opt.is_converged
+    assert geom.energy == pytest.approx(-92.2460426792319)
