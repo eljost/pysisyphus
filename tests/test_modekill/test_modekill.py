test fixes after update to wavenumber calculation code

tests/test_modekill/test_modekill.py
@@ -17,8 +17,7 @@ def test_modekill_xtb(this_dir):
     geom.set_calculator(calc)
     w, v = np.linalg.eigh(geom.mw_hessian)
     nus = eigval_to_wavenumber(w)
-    assert nus[0] == pytest.approx(-199.532846)
-    assert nus[1] == pytest.approx(-93.9897634)
+    np.testing.assert_allclose(nus[[0, 1]], (-199.6709792, -94.054831))
 
     modekill = ModeKill(geom, kill_inds=[0, ])
     modekill.run()
@@ -26,7 +25,7 @@ def test_modekill_xtb(this_dir):
     w, v = np.linalg.eigh(geom.mw_hessian)
     nus = eigval_to_wavenumber(w)
 
-    assert nus[0] == pytest.approx(-96.1364439)
+    assert nus[0] == pytest.approx(-96.20299777)
 
 
 @using("pyscf")
@@ -38,7 +37,7 @@ def test_modekill_pyscf(this_dir):
 
     w, v = np.linalg.eigh(geom.eckart_projection(geom.mw_hessian))
     nus = eigval_to_wavenumber(w)
-    assert nus[0] == pytest.approx(-266.11360383)
+    assert nus[0] == pytest.approx(-266.2978391005)
 
     modekill = ModeKill(geom, kill_inds=[0, ])
     modekill.run()
@@ -46,4 +45,4 @@ def test_modekill_pyscf(this_dir):
 
     w, v = np.linalg.eigh(geom.eckart_projection(geom.mw_hessian))
     nus = eigval_to_wavenumber(w)
-    assert nus[0] == pytest.approx(324.211369, abs=1e-4)
+    assert nus[0] == pytest.approx(324.4358155, abs=1e-4)