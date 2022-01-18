test fixes after update to wavenumber calculation code

pysisyphus/Geometry.py
@@ -121,6 +121,7 @@ def get_trans_rot_vectors(cart_coords, masses, rot_thresh=1e-6):
     # Drop vectors with vanishing norms
     rot_vecs = rot_vecs[np.linalg.norm(rot_vecs, axis=1) > rot_thresh]
     tr_vecs = np.concatenate((trans_vecs, rot_vecs), axis=0)
+    tr_vecs = np.linalg.qr(tr_vecs.T)[0].T
     return tr_vecs
 
 
@@ -128,7 +129,9 @@ def get_trans_rot_projector(cart_coords, masses, full=False):
     tr_vecs = get_trans_rot_vectors(cart_coords, masses=masses)
     U, s, _ = np.linalg.svd(tr_vecs.T)
     if full:
-        P = U.T
+        P = np.eye(cart_coords.size)
+        for tr_vec in tr_vecs:
+            P -= np.outer(tr_vec, tr_vec)
     else:
         P = U[:, s.size :].T
     return P

tests/test_oniom/test_oniom.py
@@ -385,8 +385,7 @@ def test_oniom3():
     geom = res.opt_geom
     res = do_final_hessian(geom, save_hessian=False)
     nus = res.nus
-    assert nus[-1] == pytest.approx(3747.5594141, abs=1e-2)
-    assert nus[-5] == pytest.approx(3563.8997581, abs=1e-2)
+    np.testing.assert_allclose(nus[[-1, -5]], (3750.1537948, 3566.366994), atol=1e-2)
 
 
 @pytest.mark.skip