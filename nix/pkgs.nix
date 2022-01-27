4ceeff0e335bc65944ce5c929c00e088772b1e55
Phillip Seeber <phillip.seeber@googlemail.com>
2022-01-27 16:39:42 +0100

    nix: add gamess-us interface, update deps, update docs and caching
    disable non-free packages

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,16 +1,19 @@
-{ postOverlays ? [] } :
-
 let
   sources = import ./sources.nix;
   qchemOverlay = import sources.NixOS-QChem;
+  postOverlay = self: super:
+    { # Example to change MPI/BLAS+LAPACK providers. Uncomment to switch to MKL and MVAPICH, i.e.
+      /*
+      blas = super.blas.override { blasProvider = super.mkl; };
+      lapack = super.lapack.override { lapackProvider = super.mkl; };
+      mpi = super.mvapich;
+      */
+    };
   nixpkgs = import sources.nixpkgs {
-    overlays = [ qchemOverlay ] ++ postOverlays;
-    allowUnfree = true;
-
-    # See https://github.com/markuskowa/NixOS-QChem#configuration-via-nixpkgs
+    overlays = [ qchemOverlay postOverlay ];
+    allowUnfree = false; # Change to true if you want to use proprietary software
     qchem-config = {
-      optAVX = false;
-      optArch = null;
+      optAVX = false; # Change to true if your CPU supports at least AVX2 (Haswell upwards)
       useCuda = false;
     };
   };
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,13 +1,7 @@
-{ fullTest ? false
-, postOverlays ? []
-} :
-
 let
-  pkgs = import ./pkgs.nix { inherit postOverlays; };
+  pkgs = import ./pkgs.nix;
   pysis = { pysisyphus = import ./default.nix { inherit fullTest; }; };
   allPkgs = pkgs // pysis;
-in
-  with allPkgs;
-  mkShell {
-    buildInputs = [ pysisyphus ];
-  }
+in with allPkgs; mkShell {
+  buildInputs = [ python3.pkgs.qcengine qchem.gamess-us ];
+}
