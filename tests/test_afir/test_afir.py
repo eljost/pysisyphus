fix: updated atol thresh for fd hessian tests

tests/test_afir/test_afir.py
@@ -104,6 +104,7 @@ def test_afir_hessian():
     def grad_func(coords):
         return -geom.get_energy_and_forces_at(coords)["forces"]
 
-    fd_hessian = finite_difference_hessian(geom.coords, grad_func)
+    fd_hessian = finite_difference_hessian(geom.coords, grad_func, acc=2)
     np.set_printoptions(suppress=True, precision=4)
-    np.testing.assert_allclose(afir_hess, fd_hessian, atol=1e-4)
+    # Locally it works with atol=1e-4, but GH action seem to require 4e-4
+    np.testing.assert_allclose(afir_hess, fd_hessian, atol=4e-4)

tests/test_linalg/test_linalg.py
@@ -14,8 +14,8 @@ def assert_hessians(geom):
     def grad_func(coords):
         return -geom.get_energy_and_forces_at(coords)["forces"]
 
-    fd_hessian = finite_difference_hessian(geom.coords, grad_func, acc=2)
-    np.testing.assert_allclose(hessian, fd_hessian, atol=5e-4)
+    fd_hessian = finite_difference_hessian(geom.coords, grad_func, acc=4)
+    np.testing.assert_allclose(hessian, fd_hessian, atol=1.6e-4)
 
 
 def test_fd_hessian():