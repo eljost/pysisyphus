670c2d3f3c88c6d9de961fa4ec57f7bd30a92132
Johannes Steinmetzer <josteinm@posteo.de>
2022-04-21 21:17:18 +0200

    added comments in AFIR driver and fixed filenames of preopt products

--- pysisyphus/drivers/afir.py
+++ pysisyphus/drivers/afir.py
@@ -1,3 +1,9 @@
+# [1] https://doi.org/10.1002/jcc.23481
+#     Exploring transition state structures for intramolecular pathways
+#     by the artificial force induced reaction method
+#     Maeda, Morokuma et al, 2013
+
+import itertools as it
 from functools import reduce
 from pathlib import Path
 
@@ -10,6 +16,14 @@ from pysisyphus.drivers.opt import run_opt
 from pysisyphus.Geometry import Geometry
 from pysisyphus.helpers import pick_image_inds
 from pysisyphus.intcoords.helpers import get_bond_difference
+
+
+##########################
+#                        #
+#  Multi-component AFIR  #
+#       MC-AFIR          #
+#                        #
+##########################
 
 
 def generate_random_union(geoms, offset=1.0, copy=True):
@@ -156,3 +170,151 @@ def analyze_afir_path(energies):
     else:
         sp_inds = list()
     return sp_inds
+
+
+###########################
+#                         #
+#  Single-component AFIR  #
+#        SC-AFIR          #
+#                         #
+###########################
+
+
+def decrease_distance(coords3d, m, n, frac=0.9):
+    c3d_m = coords3d[m]
+    c3d_n = coords3d[n]
+    dist_vec = c3d_n - c3d_m
+    step = (1 - frac) / 2 * dist_vec
+    c3d_new = coords3d.copy()
+    c3d_new[m] += step
+    c3d_new[n] -= step
+    return c3d_new
+
+
+def lstsqs_with_reference(coords3d, ref_coords3d, freeze_atoms=None):
+    """Least-squares w.r.t. reference coordinates while keeping some
+    atoms frozen."""
+
+    if freeze_atoms is None:
+        freeze_atoms = []
+    else:
+        freeze_atoms = list(freeze_atoms)
+    ref_dists = pdist(ref_coords3d)
+
+    mask = np.ones_like(coords3d[:, 0], dtype=bool)
+    mask[freeze_atoms] = False
+    # All atoms w/o the frozen atoms
+    coords = coords3d[mask].flatten()
+    x0 = coords
+
+    coords_full = coords3d.copy()
+
+    def fun(x):
+        # Consider all distances, including distances to the fixed atoms 'm' and 'n'.
+        coords_full[mask] = x.reshape(-1, 3)
+        dists = pdist(coords_full)
+        return dists - ref_dists
+
+    res = least_squares(fun, x0)
+    opt_coords = res.x
+    coords_full[mask] = opt_coords.reshape(-1, 3)
+    return res, coords_full
+
+
+def weight_function(atoms, coords3d, i, j, p=6):
+    cr_sum = sum([CR[atoms[k].lower()] for k in (i, j)])
+    r_ij = np.linalg.norm(coords3d[i] - coords3d[j])
+    omega = (cr_sum / r_ij) ** 6
+    return omega
+
+
+def find_candidates(center, bond_sets):
+    center_set = {
+        center,
+    }
+    bonded_to_center = list()
+    for bond in bond_sets:
+        if center not in bond:
+            continue
+        bonded_to_center.append(*set(bond) - center_set)
+    return bonded_to_center
+
+
+def automatic_fragmentation(
+    atoms, coords3d, frag1, frag2, cycles=2, p=6, bond_factor=1.25
+):
+    """Automatic fragmentation scheme as described in SC-AFIR paper [1]."""
+
+    frag1 = set(frag1)
+    frag2 = set(frag2)
+
+    def w(m, n):
+        """Shortcut for weight function"""
+        return weight_function(atoms, coords3d, m, n, p=p)
+
+    pairs = list(it.product(frag1, frag2))
+    weights = [w(m, n) for m, n in pairs]
+    max_weight = max(weights)
+
+    bonds = find_bonds(atoms, coords3d, bond_factor=bond_factor)
+    bond_sets = [set(bond) for bond in bonds.tolist()]
+
+    def filter_candidates(candidates, partners, max_weight):
+        to_keep = set()
+        for candidate in candidates:
+            for partner in partners:
+                weight = w(candidate, partner)
+                if weight > max_weight:
+                    break
+            else:
+                to_keep.add(candidate)
+        return to_keep
+
+    def grow_fragment(frag1, frag2):
+        # Find candidates that are bonded to atoms in frag1. Step 2 in [1].
+        candidates = [find_candidates(m, bond_sets) for m in frag1]
+        # Filter out candidates that are already contained in frag1
+        candidates = [c for c in it.chain(*candidates) if c not in frag1]
+        # Filter out candidates with weights that are too big. Step 3/4 in [1].
+        candidates = set(filter_candidates(candidates, frag2, max_weight))
+        return candidates
+
+    for _ in range(cycles):
+        f1_candidates = grow_fragment(frag1, frag2)
+        f2_candidates = grow_fragment(frag2, frag1)
+
+        # Step 5 in [1].
+        f1_candidates = filter_candidates(f1_candidates, f2_candidates, max_weight)
+        f2_candidates = filter_candidates(f2_candidates, f1_candidates, max_weight)
+
+        # Step 6 in [1].
+        frag1.update(f1_candidates)
+        frag2.update(f2_candidates)
+    return frag1, frag2
+
+
+def prepare_single_component_afir(geom, m, n, calc_getter, afir_kwargs):
+    """Create perturbed geometry, determine fragments and set AFIR calculator."""
+    # Move target atoms closer together along distance vector (decrease distance)
+    tmp_coords3d = decrease_distance(geom.coords3d, m, n)
+    # Optimize remaining coordinates using least-squares, while keeping target
+    # atom pair fixed.
+    _, opt_coords3d = lstsqs_with_reference(tmp_coords3d, geom.coords3d, (m, n))
+    # Determine fragments, using the automated fragmentation
+    frag1, frag2 = automatic_fragmentation(geom.atoms, opt_coords3d, [m], [n])
+    fragment_indices = [list(frag) for frag in (frag1, frag2)]
+    # Set lstsq-optimized coordinates and created wrapped calculator
+    geom.coords3d = opt_coords3d
+    calc = calc_getter()
+    afir_defaults = {
+        "calculator": calc,
+        "fragment_indices": fragment_indices,
+        "complete_fragments": False,
+        "ignore_hydrogen": False,
+    }
+    afir_kwargs = afir_kwargs.copy()
+    # Force the use of the determined fragment_indices, by override any
+    # potential user input.
+    afir_kwargs.update(afir_defaults)
+    afir_calc = AFIR(**afir_kwargs)
+    geom.set_calculator(afir_calc)
