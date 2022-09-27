fix: normalization of trans. dens. mats & trans. moments
- renamed wavefunction.es_overlaps to wavefunction.excited_states
- fixed calculation of transition moments
- overhaulted tests/test_wavefunction/test_wavefunction.py
- added RCIS/UCIS test for transtion moments (GS -> ES) and (ES -> ES)
transition moments in pyrrole in
tests/test_wavefunction/test_multipoles.py

pysisyphus/wavefunction/excited_states.py
@@ -1,7 +1,82 @@
-from typing import Iterable, List, Sequence, Tuple
+from typing import Iterable, List, Optional, Sequence, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
+
+
+def norm_ci_coeffs(
+    Xa: NDArray[float],
+    Ya: NDArray[float],
+    Xb: Optional[NDArray[float]] = None,
+    Yb: Optional[NDArray[float]] = None,
+    restricted_norm: float = 0.5,
+    unrestricted_norm: float = 1.0,
+) -> Tuple[NDArray[float], NDArray[float]]:
+    """Normalize transition density matrices.
+
+    target_norm = (N*X)**2 - (N*Y)**2
+                = N**2 * (X**2 - Y**2)
+    N**2 = target_norm / (X**2 - Y**2)
+    N    = sqrt(target_norm / (X**2 - Y**2))
+    """
+    nstates_a, occ_a, virt_a = Xa.shape
+    unrestricted = (Xb is not None) and (Yb is not None)
+    if unrestricted:
+        nstates_b, occ_b, virt_b = Xb.shape
+        assert nstates_a == nstates_b
+        assert (occ_a + virt_a) == (occ_b + virt_b)
+        nstates = nstates_a
+        X = np.concatenate((Xa.reshape(nstates, -1), Xb.reshape(nstates, -1)), axis=1)
+        Y = np.concatenate((Ya.reshape(nstates, -1), Yb.reshape(nstates, -1)), axis=1)
+        target_norm = unrestricted_norm
+    else:
+        nstates = nstates_a
+        X = Xa.reshape(nstates, -1)
+        Y = Ya.reshape(nstates, -1)
+        target_norm = restricted_norm
+    X_norms, Y_norms = [np.linalg.norm(mat, axis=1) for mat in (X, Y)]
+    ci_norms = X_norms ** 2 - Y_norms ** 2
+    N = np.sqrt(target_norm / ci_norms)
+    X *= N[:, None]
+    Y *= N[:, None]
+    entries_a = occ_a * virt_a
+    Xa = X[:, :entries_a].reshape(nstates, occ_a, virt_a)
+    Ya = Y[:, :entries_a].reshape(nstates, occ_a, virt_a)
+    returns = [Xa, Ya]
+    if unrestricted:
+        Xb = X[:, entries_a:].reshape(nstates, occ_b, virt_b)
+        Yb = Y[:, entries_a:].reshape(nstates, occ_b, virt_b)
+        returns.extend([Xb, Yb])
+    return returns
+
+
+def get_state_to_state_transition_density(
+    P_a: NDArray[float], P_b: NDArray[float]
+) -> NDArray[float]:
+    """State-to-state transition density.
+
+    Parameters
+    ----------
+    P_a
+        Transition density matrix for state A of shape (occ, virt).
+    P_b
+        Transition density matrix for state B of shape (occ, virt).
+
+    Returns
+    -------
+    P_trans
+        State-to-state transition density of shape (occ+virt, occ+virt).
+    """
+    assert P_a.shape == P_b.shape
+
+    electron = P_a.T @ P_b
+    hole = P_b @ P_a.T
+    occ, virt = P_a.shape
+    ov = occ + virt
+    P_trans = np.zeros((ov, ov))
+    P_trans[:occ, :occ] = -hole
+    P_trans[occ:, occ:] = electron
+    return P_trans
 
 
 def mo_inds_from_tden(
@@ -210,8 +285,14 @@ def tden_overlaps(
         for j, state2 in enumerate(ci_coeffs2):
             overlaps[i, j] = np.trace(precontr @ state2)
 
-    # overlaps = np.einsum(
-    # "mil,ij,njk,kl->mn", ci_coeffs1, S_MO_occ, ci_coeffs2, S_MO_vir.T,
-    # optimize=['einsum_path', (0, 3), (1, 2), (0, 1)],
-    # )
+    """
+    overlaps = np.einsum(
+        "mil,ij,njk,kl->mn",
+        ci_coeffs1,
+        S_MO_occ,
+        ci_coeffs2,
+        S_MO_vir.T,
+        optimize=["einsum_path", (0, 3), (1, 2), (0, 1)],
+    )
+    """
     return overlaps

pysisyphus/wavefunction/multipole.py
@@ -7,6 +7,8 @@
 #     the random phase approximation. Method and model studies.
 #     Pedersen, Hansen, 1995
 
+from typing import List, Optional
+
 import numpy as np
 from numpy.typing import NDArray
 
@@ -58,15 +60,22 @@ def get_transition_multipole_moment(
     multipole_ints: NDArray[float],
     C_a: NDArray[float],
     C_b: NDArray[float],
-    occ_a: int,
-    occ_b: int,
-    P_a: NDArray[float],
-    P_b: NDArray[float] = None,
-) -> NDArray[float]:
+    T_a: NDArray[float],
+    T_b: NDArray[float] = None,
+    occ_a: Optional[int] = None,
+    occ_b: Optional[int] = None,
+    full: bool = False,
+) -> List[NDArray[float]]:
     """
     Transition multipole moments from transition density matrices.
 
-    See Eq. (33) in [1].
+    This functions relies on the following normalization
+
+        closed shell: 0.5 = norm(X_a)**2 - norm(Y_a)**2
+          open shell: 1.0 = norm(X_a,X_b)**2 - norm(Y_a,Y_b)**2
+
+    with X_a,X_b indicating a vector formed by concatenating the elements
+    of X_a and X_b for every state. See also Eq. (33) in [1].
 
     Parameters
     ----------
@@ -78,16 +87,19 @@ def get_transition_multipole_moment(
         MO-coefficients for α-electrons.
     C_b
         MO-coefficients for β-electrons.
+    T_a
+        Numpy array containing transition density matrices of shape
+        (nstates, occ, virt) or (occ, virt) in the α-electron space.
+        The transition density must be given in the MO-basis!
+    T_b
+        See P_a. Optional. If not provided, P_b is derived from P_a.
     occ_a
         Number of α-electrons.
     occ_b
         Number of β-electrons.
-    P_a
-        Numpy array containing transition density matrices of shape
-        (nstates, occ, virt) or (occ, virt) in the α-electron space.
-        The transition density must be given in the MO-basis!
-    P_b
-        See P_a. Optional. If not provided, P_b is derived from P_a.
+    full
+        Use all MOs to transform multipole integrals. Needed for state-to-state
+        transition denstiy matrices of shape (occ+virt, occ+virt).
 
     Returns
     -------
@@ -103,21 +115,26 @@ def get_transition_multipole_moment(
             tden = tden[None, :]
         return tden
 
-    P_a = atleast_3d(P_a)
-    if P_b is None:
-        P_a = 1 / 2**0.5 * P_a
-        P_b = P_a
-    P_b = atleast_3d(P_b)
-
+    T_a = atleast_3d(T_a)
+    if T_b is not None:
+        T_b = atleast_3d(T_b)
     # Expected shape: (nstates, occ, virt)
-    assert P_a.ndim == P_b.ndim == 3
+    assert T_a.ndim == 3
+    if not full:
+        assert occ_a is not None
 
     def get_trans_moment(
-        C: NDArray[float], occ: NDArray[int], trans_dens: NDArray[float]
+        C: NDArray[float], occ: int, T: NDArray[float]
     ) -> NDArray[float]:
         # Transform AO integrals to MO basis
+        if full:
+            C_occ = C
+            C_virt = C
+        else:
+            C_occ = C[:, :occ]
+            C_virt = C[:, occ:]
         multipole_ints_mo = np.einsum(
-            "jl,...lm,mk->...jk", C[:, :occ].T, multipole_ints, C[:, occ:]
+            "jl,...lm,mk->...jk", C_occ.T, multipole_ints, C_virt
         )
 
         """
@@ -126,11 +143,13 @@ def get_transition_multipole_moment(
         j : occ. MO space
         k : virt. MO space
         """
-        trans_moment = np.einsum("...jk,ijk->i...", multipole_ints_mo, trans_dens)
+        trans_moment = np.einsum("...jk,ijk->i...", multipole_ints_mo, T)
         return trans_moment
 
     # Transitions between α -> α and β -> β
-    trans_moment_a = get_trans_moment(C_a, occ_a, P_a)
-    trans_moment_b = get_trans_moment(C_b, occ_b, P_b)
-    trans_moment = trans_moment_a + trans_moment_b
+    trans_moment = get_trans_moment(C_a, occ_a, T_a)
+    if T_b is None:
+        trans_moment *= 2
+    else:
+        trans_moment += get_trans_moment(C_b, occ_b, T_b)
     return trans_moment

pysisyphus/wavefunction/wavefunction.py
@@ -227,7 +227,7 @@ class Wavefunction:
         """
         Eqs. (2.25) and (2.26) in [1].
         """
-        trans_dens *= 2**0.5 / 2
+        trans_dens *= 2 ** 0.5 / 2
         occ_a, occ_b = self.occ
         assert occ_a == occ_b
         occ = occ_a
@@ -437,37 +437,45 @@ class Wavefunction:
     def quadrupole_moment(self) -> NDArray[float]:
         return self.get_quadrupole_moment()
 
-    def get_transition_dipole_moment(
+    ################################
+    # Transition Multipole Moments #
+    ################################
+
+    def get_transition_multipole_moment(
         self,
-        P_a: NDArray[float],
-        P_b: NDArray[float] = None,
+        order: int,
+        T_a: NDArray[float],
+        T_b: NDArray[float] = None,
         origin: Optional[NDArray[float]] = None,
         kind: Center = "coc",
+        full=False,
     ) -> NDArray[float]:
         if origin is None:
             origin = self.get_origin(kind=kind)
-        dipole_ints = self.dipole_ints(origin)
+        if order == 1:
+            multipole_ints = self.dipole_ints(origin)
+        elif order == 2:
+            multipole_ints = self.quadrupole_ints(origin)
+        else:
+            raise Exception("Multipoles of order {order} are not implemented!")
         C_a, C_b = self.C
         occ_a, occ_b = self.occ
         return get_transition_multipole_moment(
-            dipole_ints, C_a, C_b, occ_a, occ_b, P_a, P_b
+            multipole_ints,
+            C_a,
+            C_b,
+            T_a,
+            T_b,
+            occ_a,
+            occ_b,
+            full=full,
         )
 
-    def get_transition_quadrupole_moment(
-        self,
-        P_a: NDArray[float],
-        P_b: NDArray[float] = None,
-        origin: Optional[NDArray[float]] = None,
-        kind: Center = "coc",
-    ) -> NDArray[float]:
-        if origin is None:
-            origin = self.get_origin(kind=kind)
-        quadrupole_ints = self.quadrupole_ints(origin)
-        C_a, C_b = self.C
-        occ_a, occ_b = self.occ
-        return get_transition_multipole_moment(
-            quadrupole_ints, C_a, C_b, occ_a, occ_b, P_a, P_b
-        )
+    def get_transition_dipole_moment(self, *args, **kwargs):
+        return self.get_transition_multipole_moment(1, *args, **kwargs)
+
+    def get_transition_quadrupole_moment(self, *args, **kwargs):
+        return self.get_transition_multipole_moment(2, *args, **kwargs)
 
     def oscillator_strength(
         self, exc_ens: NDArray[float], trans_moms: NDArray[float]
@@ -476,7 +484,7 @@ class Wavefunction:
         exc_ens = np.atleast_1d(exc_ens)
         if trans_moms.ndim == 1:
             trans_moms = trans_moms[None, :]
-        fosc = 2 / 3 * exc_ens * (trans_moms**2).sum(axis=1)
+        fosc = 2 / 3 * exc_ens * (trans_moms ** 2).sum(axis=1)
         return fosc
 
     def as_geom(self):

tests/test_wavefunction/test_wavefunction.py
@@ -10,6 +10,7 @@ from pysisyphus.wavefunction.pop_analysis import (
     mulliken_charges_from_wf,
     iao_charges_from_wf,
 )
+from pysisyphus.wavefunction.excited_states import norm_ci_coeffs
 
 
 @pytest.mark.parametrize(
@@ -69,44 +70,6 @@ def test_orca_dipole_moments(fn, ref_dip_mom):
     np.testing.assert_allclose(dip_mom, ref_dip_mom, atol=1e-5)
 
 
-def test_transition_dipole_moments(this_dir):
-    def get_fn(fn):
-        return this_dir / "ch4_3states" / fn
-
-    wf = Wavefunction.from_orca_json(get_fn("tda.json"))
-
-    step = 0
-    fn = get_fn("tda_overlap_data.h5")
-    with h5py.File(fn) as handle:
-        all_ens = handle["all_energies"][step]
-        CI = handle["ci_coeffs"][step]
-    exc_ens = all_ens[1:] - all_ens[0]
-
-    tdms = wf.get_transition_dipole_moment(CI)
-    fosc = wf.oscillator_strength(exc_ens, tdms)
-    fmt = " .6f"
-    for i, (f, tdm) in enumerate(zip(fosc, tdms)):
-        x, y, z = tdm
-        tot = (tdm**2).sum()
-        print(f"{i}: {f=:{fmt}} {x:{fmt}} {y:{fmt}} {z:{fmt}}, tot={tot:{fmt}}")
-
-    np.testing.assert_allclose(fosc, (0.506729325, 0.506497773, 0.506627503), atol=1e-6)
-
-
-def test_P_exc(this_dir):
-    def get_fn(fn):
-        return this_dir / "ch4_3states" / fn
-
-    wf = Wavefunction.from_orca_json(get_fn("tda.json"))
-    fn = get_fn("tda_overlap_data.h5")
-    with h5py.File(fn) as handle:
-        CI = handle["ci_coeffs"][0]
-    S1 = CI[0]
-    P_exc = wf.P_exc(S1)
-    exc_dpm = wf.get_dipole_moment(P=P_exc)
-    print(exc_dpm)
-
-
 def test_mulliken_exc(this_dir):
     jfn = this_dir / "h2o2" / "rhf_000.000.orca.json"
     ovfn = this_dir / "h2o2" / "overlap_data.h5"
@@ -134,8 +97,11 @@ def test_mulliken_exc(this_dir):
 def test_orca_unrestricted(this_dir):
     """H2O2, HF/3-21G, charge=-1, mult=2"""
 
-    fn = this_dir / "h2o2_anion" / "05_h2o2_anion.json"
-    wf = Wavefunction.from_orca_json(fn)
+    base_fn = this_dir / "h2o2_anion" / "00_h2o2_anion"
+    json_fn = base_fn.with_suffix(".json")
+    cis_fn = base_fn.with_suffix(".cis")
+
+    wf = Wavefunction.from_orca_json(json_fn)
 
     P_a, P_b = wf.P
     assert P_a.shape == P_b.shape
@@ -145,29 +111,31 @@ def test_orca_unrestricted(this_dir):
     assert occ_b == 9
     # ORCA uses center of mass by default, which differs a bit from the one
     # that pysisyphus calculates.
-    dpm = wf.get_dipole_moment(origin=(-1.487808, 3.034774, 0.292895))
-    ref_dpm = (-0.00017, -0.00085, 0.00235)
+    origin = (-0.864214, -1.994637,  1.223355)
+    dpm = wf.get_dipole_moment(origin=origin)
+    ref_dpm = (0.00047,       0.00016,      -0.00029)
     np.testing.assert_allclose(dpm, ref_dpm, atol=1e-5)
 
-    ref_mulliken = (0.165653, -0.665653, -0.665653, 0.165653)
+    ref_mulliken = (0.077267, 0.077267, -0.577267, -0.577267)
     mulliken = mulliken_charges_from_wf(wf)
     np.testing.assert_allclose(mulliken, ref_mulliken, atol=1e-5)
 
-    # cis = "/home/johannes/tmp/284_marcus/05_h2o2_anion/05_h2o2_anion.cis"
-    cis = this_dir / "h2o2_anion" / "05_h2o2_anion.cis"
-    Xa, Ya, Xb, Yb = parse_orca_cis(cis)
-    assert Xa.shape == Ya.shape == (5, 10, 12)
-    assert Xb.shape == Yb.shape == (5, 9, 13)
+    Xa, Ya, Xb, Yb = parse_orca_cis(cis_fn)
+    # Xa, Ya = norm_ci_coeffs(Xa, Ya)
+    # Xb, Yb = norm_ci_coeffs(Xb, Yb)
+    Xa, Ya, Xb, Yb = norm_ci_coeffs(Xa, Ya, Xb, Yb)
+    assert Xa.shape == Ya.shape == (3, 10, 12)
+    assert Xb.shape == Yb.shape == (3, 9, 13)
     norms_a = np.linalg.norm(Xa, axis=(1, 2))
     norms_b = np.linalg.norm(Xb, axis=(1, 2))
     norms = norms_a**2 + norms_b**2
     ones = np.ones_like(norms)
     np.testing.assert_allclose(norms, ones)
 
-    tdms = wf.get_transition_dipole_moment(Xa, Xb)
-    exc_ens = (0.080127, 0.128713, 0.214026, 0.230042, 0.246588)
+    tdms = wf.get_transition_dipole_moment(Xa, Xb, origin=origin)
+    exc_ens = (0.101429, 0.137824, 0.186742)
     fosc = wf.oscillator_strength(exc_ens, tdms)
-    ref_fosc = (0.030505412, 0.000000181, 0.000000011, 0.000072701, 0.000000226)
+    ref_fosc = (0.061637792, 0.000000021, 0.000435488)
     np.testing.assert_allclose(fosc, ref_fosc, atol=1e-6)
 
 