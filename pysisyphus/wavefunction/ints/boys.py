cfce515ef8cb90c8c9828927e2faf029a9f5ab9a
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2022-10-21 16:57:38 +0200

    mod: speed up boys evaluation
    factorial2 is now pretabulated and must not be calculated over and over.
    also replaced the recursive neville interpolation with an unrolled
    implementation.

--- pysisyphus/wavefunction/ints/boys.py
+++ pysisyphus/wavefunction/ints/boys.py
@@ -14,13 +14,15 @@ import sys
 import numpy as np
 from scipy.special import factorial2
 
-from pysisyphus.config import CONFIG_DIR, L_MAX
+from pysisyphus.config import CONFIG_DIR, L_MAX, L_AUX_MAX
 
 
 _BOYS_TABLE = np.load(
     CONFIG_DIR / "wavefunction/ints/boys_table_N_64_xasym_27.1_step_0.01.npy"
 )
-_BOYS_N_MAX = L_MAX ** 3
+_BOYS_N_MAX = (
+    max(L_MAX, L_AUX_MAX) * 3
+)  # Currently, there are at most 3center integrals
 _BOYS_X_ASYM = (
     27.0  # Beyond this x the Boys function can be calculated via an analytical formula
 )
@@ -30,13 +32,16 @@ _BOYS_FACTOR = 100  # int(1 / _BOYS_FACTOR)
 _BOYS_XS = np.arange(0, _BOYS_X_ASYM + (_BOYS_ADD_POINTS + 1) * _BOYS_STEP, _BOYS_STEP)
 
 PI = np.pi
+FACT2 = np.array(
+    [factorial2(N, exact=True) for N in range(2 * _BOYS_N_MAX)], dtype=np.int64
+)
 
 
 def make_boys_table():
     from scipy.integrate import quad
 
     def boys(t, x, N):
-        return t ** (2 * N) * np.exp(-x * t ** 2)
+        return t ** (2 * N) * np.exp(-x * t**2)
 
     xs = _BOYS_XS
     boys_table = list()
@@ -59,7 +64,7 @@ def make_boys_table():
 
 
 def neville(x, xs_table, table, step, points=5, factor=None):
-    """Recursive implementation of Neville interpolation.
+    """Slow, recursive implementation of Neville interpolation.
 
     We multiply 'x' by int(1/step), so we only have to deal with integer arithmetic, to
     determine the first entry from the table. 'x_closest' is always chosen in a way, that
@@ -86,9 +91,57 @@ def neville(x, xs_table, table, step, points=5, factor=None):
     return p(0, points - 1)
 
 
+def neville_gen5(x, xs_table, table, step, factor=None):
+    """Code-generated Neville interpolation using 5 points."""
+
+    points = 5
+    if factor is None:
+        factor = int(1 / step)
+
+    # Determine closest x
+    x_closest = int(x * factor)
+    indices = np.arange(points) + x_closest
+    xs = xs_table[indices]
+    ys = table[indices]
+
+    x0 = -xs[4]
+    x1 = x - xs[0]
+    x2 = -xs[1]
+    x3 = x + x2
+    x4 = -xs[2]
+    x5 = x + x4
+    x6 = -xs[3]
+    x7 = x + x6
+    x8 = x + x0
+    x9 = (x5 * ys[3] - x7 * ys[2]) / (-x6 - xs[2])
+    x10 = (x3 * ys[2] - x5 * ys[1]) / (-x4 - xs[1])
+    x11 = (-x10 * x7 + x3 * x9) / (-x6 - xs[1])
+
+    return (
+        x1
+        * (
+            -x11 * x8
+            + x3
+            * (x5 * (x7 * ys[4] - x8 * ys[3]) / (-x0 - xs[3]) - x8 * x9)
+            / (-x0 - xs[2])
+        )
+        / (-x0 - xs[1])
+        - x8
+        * (
+            x1 * x11
+            - x7
+            * (x1 * x10 - x5 * (x1 * ys[1] - x3 * ys[0]) / (-x2 - xs[0]))
+            / (-x4 - xs[0])
+        )
+        / (-x6 - xs[0])
+    ) / (-x0 - xs[0])
+
+
 def neville_boys(N, x):
     """Wrapper for Boys function from Neville-interpolation."""
-    return neville(x, _BOYS_XS, _BOYS_TABLE[N], step=_BOYS_STEP, factor=_BOYS_FACTOR)
+    return neville_gen5(
+        x, _BOYS_XS, _BOYS_TABLE[N], step=_BOYS_STEP, factor=_BOYS_FACTOR
+    )
 
 
 def factorial_boys(N, x):
@@ -98,7 +151,7 @@ def factorial_boys(N, x):
     2      ⋅√π⋅╲╱  x         ⋅(2⋅N - 1)!
 
     """
-    return factorial2(2 * N - 1) / 2 ** (N + 1) * np.sqrt(PI / x ** (2 * N + 1))
+    return FACT2[max(0, 2 * N - 1)] / 2 ** (N + 1) * np.sqrt(PI / x ** (2 * N + 1))
 
 
 def boys(N, xs):
