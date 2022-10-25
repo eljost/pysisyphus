fd9eb2d1a6c36bd7e1c81cb51dd21979a3c632e1
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2022-10-25 09:48:08 +0200

    add: mcpdft in openmolcas calculator
    and general improvements of the openmolcas-calculator

--- pysisyphus/calculators/OpenMolcas.py
+++ pysisyphus/calculators/OpenMolcas.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import re
+import warnings
 
 import numpy as np
 
@@ -13,21 +14,42 @@ class OpenMolcas(Calculator):
 
     conf_key = "openmolcas"
 
-    def __init__(self, basis, inporb, roots, mdrlxroot,
-                 supsym=None, track=True, **kwargs):
+    def __init__(
+        self,
+        basis,
+        inporb,
+        rasscf=None,
+        gateway=None,
+        mcpdft=None,
+        track=True,
+        **kwargs,
+    ):
         super(OpenMolcas, self).__init__(**kwargs)
 
         assert self.pal == 1, (
             "RI SA-CASSCF analytical gradients do not work correctly in "
             "parallel (yet). Consider using pal=1 instead of the current "
-           f"pal={self.pal}!"
+            f"pal={self.pal}!"
         )
+        env = os.environ
+        if not ("MOLCAS" in env):
+            warnings.warn(
+                "$MOLCAS environment variable is not set! Couldn't find it in the "
+                "environment!"
+            )
 
         self.basis = basis
-        self.inporb = Path(inporb).absolute()
-        self.roots = roots
-        self.mdrlxroot = mdrlxroot
-        self.supsym = self.build_supsym_str(supsym)
+        inporb = Path(inporb).absolute()
+        self.inporb = inporb
+        if rasscf is None:
+            rasscf = {}
+        self.rasscf = rasscf
+        if gateway is None:
+            gateway = {}
+        self.gateway = gateway
+        if mcpdft is None:
+            mcpdft = {}
+        self.mcpdft = mcpdft
         self.track = track
 
         self.to_keep = ("RasOrb", "out", "in", "JobIph", "rasscf.molden")
@@ -46,8 +68,8 @@ class OpenMolcas(Calculator):
           {basis}
          group
           nosym
-        ricd
- 
+         {gateway_kwargs}
+
         &seward
          doanalytical
 
@@ -58,23 +80,19 @@ class OpenMolcas(Calculator):
           {mult}
          fileorb
           $Project.RasOrb
-         thrs
-          1.0e-6,1.0e-2,1.0e-2
-         ciroot
-          {roots} {roots} 1
-         mdrlxroot
-          {mdrlxroot}
-         {supsym}
+         {rasscf_kwargs}
+
+        {mcpdft}
 
         >> copy $Project.JobIph $CurrDir/$Project.JobIph
 
         {rassi}
 
-        &alaska
-         pnew
+        {alaska}
         """
 
         self.parser_funcs = {
+            "energy": self.parse_energy,
             "grad": self.parse_gradient,
         }
 
@@ -85,13 +103,19 @@ class OpenMolcas(Calculator):
         self.jobiph = self.make_fn("JobIph", last_calc_cycle)
         self.log(f"restarted. using {self.inporb}, {self.jobiph}")
 
-    def build_supsym_str(self, supsym):
-        """Can handle only one subgroup for now."""
-        if not supsym:
-            return ""
+    def build_str_from_dict(self, dct):
+        strs = list()
+        for key, val in dct.items():
+            if val is None:
+                val = ""
+            strs.append(f"{key}\n{val}")
+        return "\n".join(strs)
 
-        num_orbitals = len(supsym.split())
-        return f"supsym\n1\n{num_orbitals} {supsym};"
+    def build_gateway_str(self):
+        return self.build_str_from_dict(self.gateway)
+
+    def build_rasscf_str(self):
+        return self.build_str_from_dict(self.rasscf)
 
     def build_rassi_str(self):
         # In the first iteration self.jobiph isn't set yet.
@@ -107,6 +131,13 @@ class OpenMolcas(Calculator):
              track
             """
 
+    def build_mcpdft_str(self):
+        if not self.mcpdft:
+            return ""
+
+        mcpdft_kwargs = self.build_str_from_dict(self.mcpdft)
+        return f"&mcpdft\n{mcpdft_kwargs}"
+
     def get_pal_env(self):
         env_copy = os.environ.copy()
         env_copy["MOLCAS_NPROCS"] = str(self.pal)
@@ -117,39 +148,42 @@ class OpenMolcas(Calculator):
         coords = coords * BOHR2ANG
         return make_xyz_str(atoms, coords.reshape((-1, 3)))
 
-    def prepare_input(self, atoms, coords):
+    def prepare_input(self, atoms, coords, calc_type):
+        self.log(f"using inporb: {self.inporb}")
         xyz_str = self.prepare_coords(atoms, coords)
+        alaska_str = "&alaska\npnew" if calc_type == "grad" else ""
         inp = self.openmolcas_input.format(
-                                        inporb=self.inporb,
-                                        xyz_str=xyz_str,
-                                        basis=self.basis,
-                                        charge=self.charge,
-                                        mult=self.mult,
-                                        roots=self.roots,
-                                        mdrlxroot=self.mdrlxroot,
-                                        supsym=self.supsym,
-                                        rassi=self.build_rassi_str(),
+            inporb=self.inporb,
+            xyz_str=xyz_str,
+            basis=self.basis,
+            charge=self.charge,
+            mult=self.mult,
+            gateway_kwargs=self.build_gateway_str(),
+            rasscf_kwargs=self.build_rasscf_str(),
+            mcpdft=self.build_mcpdft_str(),
+            rassi=self.build_rassi_str(),
+            alaska=alaska_str,
         )
         return inp
 
-    def get_energy(self, atoms, coords):
-        results = self.get_forces(atoms, coords)
-        del results["forces"]
-        return results
-
-    def get_forces(self, atoms, coords):
-        self.log(f"using inporb: {self.inporb}")
-        inp = self.prepare_input(atoms, coords)
+    def run_calculation(self, atoms, coords, calc_type="energy"):
+        inp = self.prepare_input(atoms, coords, calc_type)
         add_args = ("-clean", "-oe", self.out_fn)
         env_copy = self.get_pal_env()
         env_copy["MOLCAS_PROJECT"] = f"{self.name}_{self.calc_counter}"
         kwargs = {
-            "calc": "grad",
+            "calc": calc_type,
             "add_args": add_args,
             "env": env_copy,
         }
         results = self.run(inp, **kwargs)
         return results
+
+    def get_energy(self, atoms, coords):
+        return self.run_calculation(atoms, coords, "energy")
+
+    def get_forces(self, atoms, coords):
+        return self.run_calculation(atoms, coords, "grad")
 
     def keep(self, path):
         kept_fns = super().keep(path)
@@ -160,17 +194,34 @@ class OpenMolcas(Calculator):
         self.jobiph = kept_fns["jobiph"]
         self.log(f"current JobIph is {self.jobiph}")
 
+    def get_root(self):
+        ras = self.rasscf
+        return int(ras.get("mdrlxroot", ras.get("rlxroot", 1)))
+
     def parse_energies(self, text):
-        # Energy of root for which gradient was computed
-        energy_regex = r"RASSCF state energy =\s*" + self.float_regex
-        energy = float(re.search(energy_regex, text).groups()[0])
+        if self.mcpdft:
+            root_re = r"PDFT Root\s*\d+\s*Total energy:\s*" + self.float_regex
+            matches = re.findall(root_re, text)
+            root_energies = np.array(matches, dtype=float)
+            root = self.get_root()
+            energy = root_energies[root - 1]
+        else:
+            # Energy of root for which gradient was computed
+            energy_regex = r"RASSCF state energy =\s*" + self.float_regex
+            energy = float(re.search(energy_regex, text).groups()[0])
+            # All state average energies
+            root_re = "RASSCF root number.+Total energy.+?" + self.float_regex
+            matches = re.findall(root_re, text)
+            root_energies = np.array(matches, dtype=float)
+        return energy, root_energies
 
-        # All state average energies
-        root_re = "RASSCF root number.+Total energy.+?" + self.float_regex
-        matches = re.findall(root_re, text)
-        sa_energies = np.array(matches, dtype=float)
-
-        return energy, sa_energies
+    def parse_energy(self, path):
+        with open(path / self.out_fn) as handle:
+            text = handle.read()
+        energy, _ = self.parse_energies(text)
+        return {
+            "energy": energy,
+        }
 
     def parse_gradient(self, path):
         results = {}
@@ -197,7 +248,7 @@ class OpenMolcas(Calculator):
         if self.track and self.calc_counter > 0:
             self.parse_rassi_track(path)
 
-        energy, sa_energies = self.parse_energies(text)
+        energy, _ = self.parse_energies(text)
 
         results["energy"] = energy
         # results["sa_energies"] = sa_energies
@@ -209,21 +260,24 @@ class OpenMolcas(Calculator):
         gradient_fn = path / self.out_fn
         with open(gradient_fn) as handle:
             text = handle.read()
-        track_re = r"Initial root:\s*(\d+)\s*Overlaps with current " \
-                   r"states:(.+)New root:\s*(\d+)"
-        #overlap_re = "OVERLAP MATRIX FOR THE ORIGINAL STATES:(.+?)##"
+        track_re = (
+            r"Initial root:\s*(\d+)\s*Overlaps with current "
+            r"states:(.+)New root:\s*(\d+)"
+        )
+        # overlap_re = "OVERLAP MATRIX FOR THE ORIGINAL STATES:(.+?)##"
         mobj = re.search(track_re, text, re.DOTALL)
 
         initial_root, overlaps, new_root = mobj.groups()
         overlaps = np.array(overlaps.strip().split(), dtype=float).reshape(-1, 2)
         # Filters for overlaps > 10% (0.1**2 ~ 0.31622)
         thresh = 0.1
-        inds = np.where(np.abs(overlaps[:,1]) > thresh**0.5)
-        ov_perc_str = ", ".join([f"{nr:.0f}: {ov**2:.2%}"
-                                 for nr, ov in overlaps[inds]])
-        self.log(f"Overlaps between previous root {initial_root} and "
-                 f"new roots bigger {thresh:.0%}:  {ov_perc_str}. Will "
-                 f"use root {new_root} for the following gradient calculation.")
+        inds = np.where(np.abs(overlaps[:, 1]) > thresh**0.5)
+        ov_perc_str = ", ".join([f"{nr:.0f}: {ov**2:.2%}" for nr, ov in overlaps[inds]])
+        self.log(
+            f"Overlaps between previous root {initial_root} and "
+            f"new roots bigger {thresh:.0%}:  {ov_perc_str}. Will "
+            f"use root {new_root} for the following gradient calculation."
+        )
         if new_root != initial_root:
             self.log("Found a root flip!")
         self.mdrlxroot = new_root
--- tests/test_openmolcas/test_openmolcas.py
+++ tests/test_openmolcas/test_openmolcas.py
@@ -1,8 +1,10 @@
 import pytest
+import yaml
 
+from pysisyphus.calculators import OpenMolcas
 from pysisyphus.helpers import geom_loader
 from pysisyphus.optimizers.RFOptimizer import RFOptimizer
-from pysisyphus.calculators import OpenMolcas
+from pysisyphus.run import run_from_dict
 from pysisyphus.testing import using
 
 
@@ -16,8 +18,10 @@ def test_openmolcas_s1_opt(this_dir):
         "inporb": inporb_fn,
         "charge": 0,
         "mult": 1,
-        "roots": 5,
-        "mdrlxroot": 2,
+        "rasscf": {
+            "ciroot": "5 5 1",
+            "mdrlxroot": 2,
+        },
     }
     calc = OpenMolcas(**kwargs)
     geom.set_calculator(calc)
@@ -31,3 +35,13 @@ def test_openmolcas_s1_opt(this_dir):
     assert opt.is_converged
     assert opt.cur_cycle == 6
     assert geom.energy == pytest.approx(-154.8758862347)
+
+
+@using("openmolcas")
+def test_cmspdft(this_dir):
+    with open(this_dir / "cms_pdft_opt.yaml") as handle:
+        run_dict = yaml.safe_load(handle)
+    # Overwrite hardcoded path in yaml with path to test directory
+    run_dict["calc"]["inporb"] = this_dir / "cms_pdft.RasOrb"
+    results = run_from_dict(run_dict)
+    assert results.opt_geom.energy == pytest.approx(-437.1389)
