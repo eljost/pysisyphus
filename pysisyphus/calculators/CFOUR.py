c3b30e2b70b4e9f0001a5905a018ca26c349aa24
Greg Jones <ghjones@caltech.edu>
2023-02-19 17:50:12 -0800

    Attempt to rotate gradient from CFOUR frame to pysisyphus frame.

--- pysisyphus/calculators/CFOUR.py
+++ pysisyphus/calculators/CFOUR.py
@@ -62,8 +62,6 @@ class CFOUR(Calculator):
 
         *CFOUR(UNITS=BOHR
         COORDINATES=CARTESIAN
-        FIXGEOM=ON
-        SYM=OFF
         {grad_string}{cfour_keyword_string})
 
         """).format(xyz_string=xyz_string, grad_string=grad_string, cfour_keyword_string=cfour_keyword_string)
@@ -107,15 +105,66 @@ class CFOUR(Calculator):
             if not mobj:
                 continue
             gradient.append(mobj.groups())
-        gradient = np.array(gradient, dtype=float).flatten()
+        gradient = np.array(gradient, dtype=float)
+        gradient_rotated = self.rotate_gradient(text, gradient).flatten()
 
         energy = self.parse_energy(path)
         results["energy"] = energy
-        results["forces"] = -gradient
+        results["forces"] = -gradient_rotated
 
         return results
 
+    def rotate_gradient(self, text, gradient):
+        cfour_coords_3d = self.read_geom(text)
+        pysis_coords_3d = np.reshape(self.input_coords, (-1,3))
+
+        cfour_centroid = self.calc_centroid(cfour_coords_3d)
+        pysis_centroid = self.calc_centroid(pysis_coords_3d)
+
+        rot_matrix = self.calc_rot_matrix(cfour_coords_3d, pysis_coords_3d, cfour_centroid, pysis_centroid)
+        t = self.calc_translation(rot_matrix, cfour_centroid, pysis_centroid)
+
+        return (gradient @ rot_matrix.T) + t
+
+    def read_geom(self, text):
+        regex = r"Coordinates used in calculation \(QCOMP\)(.+)Interatomic distance matrix"
+        floats =  [self.float_regex for i in range(3)]
+        line_regex = r"\s*[A-Z]+\s*\d+\s*" + r"\s*".join(floats) ## Element symbol, atomic number, then x, y, z in bohr
+
+        mobj = re.search(regex, text, re.DOTALL)
+        geom = list()
+        for line in mobj.groups()[0].split("\n"):
+            mobj = re.match(line_regex, line.strip())
+            if not mobj:
+                continue
+            geom.append(mobj.groups())
+        geom_3d = np.array(geom, dtype=float)
+
+        return geom_3d
+
+    def calc_centroid(self, coords_3d):
+        centroid = np.sum(coords_3d, axis=0)/coords_3d.shape[0]
+        return centroid
+
+    def calc_rot_matrix(self, cfour_coords_3d, pysis_coords_3d, cfour_centroid, pysis_centroid):
+        H = (cfour_coords_3d - cfour_centroid).T @ (pysis_coords_3d - pysis_centroid)
+        assert H.shape == (3,3)
+        U, S, V = np.linalg.svd(H)
+        R = V @ U.T
+
+        ## Cover corner case
+        if np.linalg.det(R) < 0:
+            U, S, V = np.linalg.svd(R)
+            V[:,2] = V[:,2]*-1
+            R = V @ U.T
+
+        return R
+
+    def calc_translation(self, rotation_matrix, cfour_centroid, pysis_centroid):
+        return pysis_centroid - rotation_matrix @ cfour_centroid
+
     def run_calculation(self, atoms, coords, calc_type):
+        self.input_coords = coords  ## For use later to rotate CFOUR gradient to the pysisyphus frame
         inp = self.prepare_input(atoms, coords, calc_type)
         results = self.run(inp, calc=calc_type)
         return results
