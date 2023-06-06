c6f6d5a0a41af61d3dda9060d2672dde36cd4c60
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-06-06 10:19:32 +0200

    mod/fix: moved localization tests to own directory

--- tests/test_localization/test_localization.py
+++ tests/test_localization/test_localization.py
@@ -2,8 +2,15 @@ import h5py
 import numpy as np
 import pytest
 
+from pysisyphus.config import WF_LIB_DIR
 from pysisyphus.linalg import matrix_power
-from pysisyphus.wavefunction.localization import cholesky, edmiston_ruedenberg
+from pysisyphus.wavefunction import (
+    cholesky,
+    edmiston_ruedenberg,
+    foster_boys,
+    pipek_mezey,
+    Wavefunction,
+)
 
 
 def from_checkpoint(h5_fn):
@@ -31,3 +38,19 @@ def test_edmiston_ruedenberg(this_dir):
     diag = np.diag(Crot.T @ S @ Crot)
     np.testing.assert_allclose(diag, np.ones_like(diag))
     assert cost_func == pytest.approx(6.086131)
+
+
+def test_pipek_mezey():
+    wf = Wavefunction.from_orca_json(WF_LIB_DIR / "orca_n2o4_sto3g.json")
+    result = pipek_mezey(wf)
+    assert result.is_converged
+    assert result.P == pytest.approx(18.59696423)
+    assert result.C.shape == (30, 23)
+
+
+def test_foster_boys():
+    wf = Wavefunction.from_orca_json(WF_LIB_DIR / "orca_hcho_def2svp.json")
+    result = foster_boys(wf)
+    assert result.is_converged
+    assert result.P == pytest.approx(277.46881)
+    assert result.C.shape == (38, 8)
