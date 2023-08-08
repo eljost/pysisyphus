a2d86b5f515c1b145b9cc3a0362f3ff7c65f5c3d
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-08-08 13:46:32 +0200

    add: enabled parallel COS calculations w/ dask for YAML input
    see `examples/complex/13_orca_parallel_neb` for a more detailed
    discussion.
    
    @Marclie, maybe something of this is intersting for you.

--- tests/test_dask/test_dask.py
+++ tests/test_dask/test_dask.py
@@ -1,6 +1,8 @@
 from distributed import LocalCluster
+import pytest
 
 from pysisyphus.cos.NEB import NEB
+from pysisyphus.drivers import run_opt
 from pysisyphus.helpers import geom_loader
 
 from pysisyphus.calculators import ORCA
@@ -8,9 +10,10 @@ from pysisyphus.optimizers.SteepestDescent import SteepestDescent
 from pysisyphus.testing import using
 
 
+@pytest.mark.skip_ci
 @using("orca")
 def test_dask():
-    with LocalCluster(n_workers=2, threads_per_worker=1) as cluster:
+    with LocalCluster(n_workers=5, threads_per_worker=1) as cluster:
         address = cluster.scheduler_address
 
         geoms = geom_loader("lib:ala_dipeptide_iso_b3lyp_631gd_10_images.trj")
@@ -26,3 +29,47 @@ def test_dask():
         opt.run()
 
     assert opt.cur_cycle == (max_cycles - 1)
+
+
+@pytest.mark.skip_ci
+@pytest.mark.parametrize("is_external", (True, False))
+@using("orca")
+def test_external(is_external):
+    n_workers = 5
+    cos_kwargs = {}
+    if is_external:
+        cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+        cos_kwargs["scheduler"] = cluster.scheduler_address
+    else:
+        cos_kwargs["cluster"] = True
+        cos_kwargs["cluster_kwargs"] = {
+            "n_workers": 5,
+        }
+
+    geoms = geom_loader("lib:ala_dipeptide_iso_b3lyp_631gd_10_images.trj")
+
+    calc_number = 0
+
+    def calc_getter():
+        nonlocal calc_number
+        calc = ORCA("hf-3c", pal=1, calc_number=calc_number)
+        calc_number += 1
+        return calc
+
+    neb = NEB(geoms, **cos_kwargs)
+    ref_address = neb.scheduler
+    print(f"{is_external=}, {ref_address=}, {neb._external_scheduler}")
+
+    opt_result = run_opt(
+        neb,
+        calc_getter,
+        "sd",
+        {
+            "max_cycles": 1,
+        },
+    )
+    opt = opt_result.opt
+
+    if is_external:
+        cluster.close()
+    assert neb.scheduler == ref_address
