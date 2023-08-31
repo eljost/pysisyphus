f4a15d4b1f3edd9747e513fa0ba69d1fb65bb0aa
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-08-31 08:52:15 +0200

    add: projection of residual gradient from Hessian
    add: a geometry with Hessian etc. can now be obtained from a .fchk
    containing a Hessian
    add: driver for calculation of local force constants for stretches

--- tests/test_local_force_constants/test_local_force_constants.py
+++ tests/test_local_force_constants/test_local_force_constants.py
@@ -2,16 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
+from pysisyphus.config import WF_LIB_DIR
 from pysisyphus.constants import AU2MDYNEPERANG
 from pysisyphus.drivers.local_force_constants import (
     get_force_constants_from_complice_mat,
     get_local_force_constants,
     local_mode_overlaps,
+    stretch_constants,
 )
 from pysisyphus.Geometry import Geometry
 from pysisyphus.intcoords.PrimTypes import Bonds, Bends, PrimTypes as PT
+from pysisyphus.io.fchk import geom_with_hessian_from_fchk
 from pysisyphus.io.hessian import geom_from_hessian
-from pysisyphus.helpers import geom_loader
 
 
 @pytest.fixture
@@ -145,7 +147,7 @@ def test_local_force_constants(water_geom):
     # fig, ax = plt.subplots()
     # nuss = [f"{nu:4.2f}" for nu in nus]
     # for i, local_mode in enumerate(C):
-        # ax.bar(nuss, local_mode, bottom=C[:i].sum(axis=0))
+    # ax.bar(nuss, local_mode, bottom=C[:i].sum(axis=0))
     # plt.show()
     np.testing.assert_allclose(C.sum(axis=0), np.ones(len(nus)))
 
@@ -202,6 +204,58 @@ def test_bf3(this_dir):
     # fig, ax = plt.subplots()
     # nuss = [f"{nu:4.2f}" for nu in nus]
     # for i, local_mode in enumerate(C):
-        # ax.bar(nuss, local_mode, bottom=C[:i].sum(axis=0), label=tps[i])
+    # ax.bar(nuss, local_mode, bottom=C[:i].sum(axis=0), label=tps[i])
     # ax.legend()
     # plt.show()
+
+
+@pytest.mark.parametrize(
+    "fchk_fn, ref_consts",
+    (
+        (
+            "01_ch4_freq.fchk",
+            {
+                (PT.BOND, 0, 1): 0.3445713173236299,
+                (PT.BOND, 0, 2): 0.3445724116263941,
+                (PT.BOND, 0, 3): 0.34457241135707334,
+                (PT.BOND, 0, 4): 0.3445693463705333,
+            },
+        ),
+        (
+            "03_ch3f_freq.fchk",
+            {
+                (PT.BOND, 0, 1): 0.34626240686928556,
+                (PT.BOND, 0, 2): 0.33262918333434555,
+                (PT.BOND, 0, 3): 0.33262952711888816,
+                (PT.BOND, 0, 4): 0.3326307722154276,
+            },
+        ),
+        (
+            "05_acn_freq.fchk",
+            {
+                (PT.BOND, 0, 2): 1.2735519829437154,
+                (PT.BOND, 1, 2): 0.32808724468158307,
+                (PT.BOND, 1, 3): 0.34017220767653955,
+                (PT.BOND, 1, 4): 0.34019072729400324,
+                (PT.BOND, 1, 5): 0.3401894660558234,
+            },
+        ),
+        (
+            "06_acn_non_statpt.fchk",
+            {
+                (PT.BOND, 0, 2): 1.2743580248319792,
+                (PT.BOND, 1, 2): 0.33693854795027894,
+                (PT.BOND, 1, 3): 0.3408064569080432,
+                (PT.BOND, 1, 4): 0.34061848807477624,
+                (PT.BOND, 1, 5): 0.34079552137470376,
+            },
+        ),
+    ),
+)
+def test_stretch_constants(fchk_fn, ref_consts):
+    """Tests reproduce values report in 10.1063/1.4747339 ."""
+    geom = geom_with_hessian_from_fchk(WF_LIB_DIR / fchk_fn)
+    results = stretch_constants(geom)
+    # print(repr(results))
+    for rk, rv in ref_consts.items():
+        assert results[rk] == pytest.approx(rv)
