ea289d90d66708ca62ac8f67d1c01f9397e40d8d
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-10-18 11:06:59 +0200

    mod: fully parallel calculations in molcas_scan.py
    previously, the script was restricted to carrying out scans in two
    directions, doing (at most) two calculations at a time. now, if inporbs
    are available for all geometries, all calculations can be carried out in
    parallel.

--- scripts/molcas_scan.py
+++ scripts/molcas_scan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from enum import Enum
 import os
 from pathlib import Path
 from pprint import pprint
@@ -12,15 +13,20 @@ import time
 from distributed import Client, LocalCluster
 import matplotlib.pyplot as plt
 import numpy as np
+import psutil
 import yaml
 
 from pysisyphus.calculators import OpenMolcas
 from pysisyphus.constants import AU2EV
+from pysisyphus.color import red
 from pysisyphus.helpers import geom_loader
 from pysisyphus.init_logging import init_logging
 
 
 init_logging()
+
+
+MODE = Enum("MODE", ("DIRECTIONS", "STANDLONE"))
 
 
 def timef():
@@ -69,6 +75,21 @@ def run_mopicgen(index, calc):
     shutil.copy(mop_path / "montage.0.jpg", f"montage.{index:03d}.jpg")
 
 
+def run_calculation(geom, index, calc):
+    key = f"geom_{index:03d}"
+    print(f"{key} ... started at {timef()}")
+    print(f"{key} ... inporb={calc.inporb}")
+    sys.stdout.flush()
+    start = time.time()
+    results = calc.get_energy(geom.atoms, geom.cart_coords)
+    dur = time.time() - start
+    print(red(f"{key} ... finished at {timef()}"))
+    print(f"{key} ... calculation took {dur: >8.2f} s")
+    run_mopicgen(index, calc)
+    print(f"{key} ... ran mopicgen")
+    return results
+
+
 def run_direction(geoms, indices, inporb, calc_kwargs):
     ngeoms = len(indices)
     print(f"Running direction for {ngeoms} geometries with {indices=}")
@@ -76,27 +97,16 @@ def run_direction(geoms, indices, inporb, calc_kwargs):
     prev_inporb = inporb
     all_results = list()
     for index in indices:
-        geom = geoms[index]
-        key = f"geom_{index:03d}"
         calc = calc_getter(calc_number=index, inporb=prev_inporb)
-        print(f"{key} ... started at {timef()}")
-        print(f"{key} ... inporb={prev_inporb}")
-        sys.stdout.flush()
-        start = time.time()
-        results = calc.get_energy(geom.atoms, geom.cart_coords)
-        dur = time.time() - start
-        print(f"{key} ... finished at {timef()}")
-        print(f"{key} ... calculation took {dur: >8.2f} s")
-        run_mopicgen(index, calc)
-        print(f"{key} ... ran mopicgen")
-
+        geom = geoms[index]
+        results = run_calculation(geom, index, calc)
         all_results.append(results)
         prev_inporb = calc.inporb
 
     return all_results
 
 
-def run_calculations(geoms, start_index: int, calc_kwargs: dict, inporb, client=None):
+def run_directions(geoms, start_index: int, inporb, calc_kwargs: dict, client=None):
     ngeoms = len(geoms)
     end_index = ngeoms - 1
     assert 0 <= start_index < ngeoms
@@ -123,7 +133,7 @@ def run_calculations(geoms, start_index: int, calc_kwargs: dict, inporb, client=
     if client is not None:
         futures = client.map(wrapper, directions)
         results = client.gather(futures)
-    # or in parallel
+    # or in serial
     else:
         results = list()
         for indices in directions:
@@ -134,6 +144,25 @@ def run_calculations(geoms, start_index: int, calc_kwargs: dict, inporb, client=
         for i, index in enumerate(indices):
             all_results[index] = res[i]
 
+    return all_results
+
+
+def run_calculations(geoms, indices, inporbs, calc_kwargs, client):
+    calc_getter = get_calc_getter(calc_kwargs)
+    all_results = list()
+
+    def wrapper(i):
+        inporb = inporbs[i]
+        geom = geoms[i]
+        calc = calc_getter(calc_number=i, inporb=inporb)
+        results = run_calculation(geom, i, calc)
+        return results
+
+    if client is not None:
+        futures = client.map(wrapper, indices)
+        all_results = client.gather(futures)
+    else:
+        all_results = list(map(wrapper, indices))
     return all_results
 
 
@@ -166,6 +195,7 @@ def parse_args(args):
 
     parser.add_argument("yaml")
     parser.add_argument("--cluster", action="store_true")
+    parser.add_argument("--nthreads", type=int, default=psutil.cpu_count(logical=False))
     return parser.parse_args(args)
 
 
@@ -173,6 +203,7 @@ def run():
     args = parse_args(sys.argv[1:])
     yaml_fn = args.yaml
     cluster = args.cluster
+    nthreads = args.nthreads
 
     loader = yaml.SafeLoader
     with open(yaml_fn) as handle:
@@ -183,6 +214,7 @@ def run():
 
     trj_fn = run_dict["geom"]["fn"]
     geoms = geom_loader(trj_fn)
+    ngeoms = len(geoms)
 
     calc_kwargs = run_dict["calc"]
     calc_type = calc_kwargs.pop("type")
@@ -191,28 +223,59 @@ def run():
     pprint(calc_kwargs)
     print()
 
-    inporb = calc_kwargs.pop("inporb")
-    start_index = run_dict["start_index"]
+    start_index = run_dict.get("start_index", None)
+    inporb_glob = run_dict.get("inporb_glob", None)
 
     cwd = Path(".")
     calc_kwargs["out_dir"] = cwd / "qm_calcs"
 
+    if inporb_glob is not None:
+        inporbs = list(sorted(Path(".").glob(inporb_glob)))
+        assert ngeoms == len(inporbs)
+        indices = np.arange(ngeoms)
+    elif start_index is not None:
+        # TODO: Give inporb in calc section or outside, next to start_index?
+        inporb = calc_kwargs.pop("inporb")
+        inporbs = [inporb]
+        indices = [start_index]
+    else:
+        raise Exception("Either start_index or inporb_glob must be present!")
+
+    mode = (
+        MODE.DIRECTIONS if (len(indices) == 1 and len(inporbs) == 1) else MODE.STANDLONE
+    )
+
     if cluster:
+        n_workers = 1
+        # TODO: threads depend on run_mode
+        threads_per_worker = 2 if mode == MODE.DIRECTIONS else nthreads
         cluster_kwargs = {
-            "threads_per_worker": 2,
-            "n_workers": 1,
+            "threads_per_worker": threads_per_worker,
+            "n_workers": n_workers,
         }
         cluster = LocalCluster(**cluster_kwargs)
         link = cluster.dashboard_link
-        print(f"Started cluster. Dashboard link is {link}")
+        print(
+            f"Started cluster with {n_workers=} and {threads_per_worker=}. "
+            f"Dashboard link is {link}"
+        )
         client = Client(cluster)
     else:
         client = None
 
     start = time.time()
-    all_results = run_calculations(geoms, start_index, calc_kwargs, inporb, client)
+    # Direction mode, where active space orbitals are propgagated along a coordinate
+    if mode == MODE.DIRECTIONS:
+        all_results = run_directions(geoms, indices[0], inporbs[0], calc_kwargs, client)
+    # Parallel mode, where each geometry already has active space orbitals and
+    # calculations can run in parallel.
+    elif mode == MODE.STANDLONE:
+        all_results = run_calculations(geoms, indices, inporbs, calc_kwargs, client)
+    else:
+        raise Exception("How did I end up here?")
     dur = time.time() - start
     print(f"Calculations took {dur:.2f} s")
+
     dump_all_energies(all_results, cwd)
 
 
