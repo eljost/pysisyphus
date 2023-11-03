0847660e690a44e30517e8570f6e6620b98ba38a
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-11-03 22:06:25 +0100

    mod: check if marcus dim fit & scan were already done

--- pysisyphus/marcus_dim/run.py
+++ pysisyphus/marcus_dim/run.py
@@ -8,18 +8,29 @@
 #     Šrut, Lear, Krewald, 2023, actual published version
 
 
+from pathlib import Path
+
 from distributed import LocalCluster
 import matplotlib.pyplot as plt
+import numpy as np
 import psutil
 
 from pysisyphus.helpers_pure import highlight_text
+from pysisyphus.marcus_dim.config import FIT_RESULTS_FN, SCAN_RESULTS_FN
 from pysisyphus.marcus_dim.fit import epos_from_wf, fit_marcus_dim
 from pysisyphus.marcus_dim.param import param_marcus
 from pysisyphus.marcus_dim.scan import plot_scan, scan
 
 
 def run_marcus_dim(
-    geom, fragments, calc_getter, fit_kwargs=None, cluster=True, scan_kwargs=None
+    geom,
+    fragments,
+    calc_getter,
+    fit_kwargs=None,
+    cluster=True,
+    scan_kwargs=None,
+    cwd=".",
+    force=False,
 ):
     """Fit Marucs dimension, scan along it and try to parametrize Marcus models."""
     if fit_kwargs is None:
@@ -28,23 +39,34 @@ def run_marcus_dim(
         scan_kwargs = {}
     fit_kwargs = fit_kwargs.copy()
     scan_kwargs = scan_kwargs.copy()
+    cwd = Path(cwd)
 
     # 1.) Fit Marcus dimension
     print(highlight_text("Fitting of Marcus Dimension"))
 
-    # Use scheduler/cluster/cluster_kwargs as in ChainOfStates.py?
-    if cluster:
-        n_workers = psutil.cpu_count(logical=False)
-        scheduler = LocalCluster(n_workers=n_workers, threads_per_worker=1)
-        fit_kwargs["scheduler"] = scheduler
-
-    marcus_dim = fit_marcus_dim(geom, calc_getter, fragments, **fit_kwargs)
-
-    if cluster:
-        scheduler.close()
-
-    # with np.load("marcusdim_results.npz") as handle:
-    # marcus_dim = handle["marcus_dim"]
+    fit_results_path = cwd / FIT_RESULTS_FN
+    rms_converged = False
+    if fit_results_path.exists():
+        md_results = np.load(fit_results_path)
+        try:
+            rms_converged = bool(md_results["rms_converged"])
+        except KeyError:
+            print("Could not determine if Marcus dimension already converged!")
+    # When rms_converged is True md_results will always be present
+    if not force and rms_converged:
+        marcus_dim = md_results["marcus_dim"]
+        print(f"Loaded Marcus dimension from '{fit_results_path}'. Skipping fit.")
+        # TODO: report dimension?
+    else:
+        # Use scheduler/cluster/cluster_kwargs as in ChainOfStates.py?
+        if cluster:
+            n_workers = psutil.cpu_count(logical=False)
+            scheduler = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+            fit_kwargs["scheduler"] = scheduler
+        # Carry out fitting of Marcus dimension
+        marcus_dim = fit_marcus_dim(geom, calc_getter, fragments, **fit_kwargs)
+        if cluster:
+            scheduler.close()
 
     # 2.) Scan along Marcus dimension
     print()
@@ -62,12 +84,22 @@ def run_marcus_dim(
         tot_epos, alpha_epos = epos_from_wf(wf, fragments)
         return energy, alpha_epos
 
-    scan_factors, scan_coords, scan_energies, scan_properties = scan(
-        coords_init=geom.cart_coords.copy(),
-        direction=marcus_dim,
-        get_properties=get_properties,
-        **scan_kwargs,
-    )
+    scan_results_path = cwd / SCAN_RESULTS_FN
+    scan_converged = False
+    if scan_results_path.exists():
+        scan_results = np.load(scan_results_path)
+        scan_factors = scan_results["factors"]
+        # scan_coords = scan_results["coords"]
+        scan_energies = scan_results["energies"]
+        scan_properties = scan_results["properties"]
+        print(f"Loaded scan data from '{scan_results_path}'. Skipping scan.")
+    else:
+        scan_factors, scan_coords, scan_energies, scan_properties = scan(
+            coords_init=geom.cart_coords.copy(),
+            direction=marcus_dim,
+            get_properties=get_properties,
+            **scan_kwargs,
+        )
     scan_fig, scan_axs = plot_scan(scan_factors, scan_energies, scan_properties)
     scan_fig.savefig("scan.png")
     plt.close(scan_fig)
