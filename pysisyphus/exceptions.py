7be8cd0b1f452be71281b79bb219acd424d5fab6
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-11-10 14:23:20 +0100

    mod: raise exception for returncode != 0

--- pysisyphus/exceptions.py
+++ pysisyphus/exceptions.py
@@ -1,2 +1,8 @@
+class CalculationFailedException(Exception):
+    def __init__(self, msg, path):
+        super().__init__(msg)
+        self.path = path
+
+
 class HEIIsFirstOrLastException(Exception):
     pass
