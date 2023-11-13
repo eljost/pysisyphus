75bc8ca9b41890f84abf9b6a3197962285df3c58
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-11-13 13:35:40 +0100

    mod: raise more specific exception when run_after fails

--- pysisyphus/exceptions.py
+++ pysisyphus/exceptions.py
@@ -1,7 +1,13 @@
 class CalculationFailedException(Exception):
-    def __init__(self, msg, path):
+    def __init__(self, msg="", path=None):
         super().__init__(msg)
         self.path = path
+
+
+class RunAfterCalculationFailedException(Exception):
+    def __init__(self, msg="", err=None):
+        super().__init__(msg)
+        self.err = err
 
 
 class HEIIsFirstOrLastException(Exception):
