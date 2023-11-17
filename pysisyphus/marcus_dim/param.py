951867e741c75624c0b02566005803b75c8e1cea
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2023-11-17 09:48:34 +0100

    fix: coupling lacked a **2 in Marcus Model adiabatic states
    also report force constant in MarcusModel.pretty() and added axis label
    in MarcusModel.plot()

--- pysisyphus/marcus_dim/param.py
+++ pysisyphus/marcus_dim/param.py
@@ -43,13 +43,17 @@ class MarcusModel:
         Gb = self.f * (x - self.d) ** 2
         return Ga, Gb
 
-    def G_adiabatic(self, x):
+    def G_adiabatic(self, x, shift=True):
         Ga, Gb = self.G_diabatic(x)
         plus = Ga + Gb
         minus2 = (Ga - Gb) ** 2
-        sqrt = np.sqrt(minus2 + 4 * self.coupling)
+        sqrt = np.sqrt(minus2 + 4 * self.coupling**2)
         G1 = 0.5 * (plus - sqrt)
         G2 = 0.5 * (plus + sqrt)
+        if shift:
+            shift = G1.min()
+            G1 -= shift
+            G2 -= shift
         return G1, G2
 
     def plot(self, x, show=False):
@@ -60,18 +64,22 @@ class MarcusModel:
             ax.plot(x, state, label=label)
         for label, state in (("$G_1$", G1), ("$G_2$", G2)):
             ax.plot(x, state, label=label)
+        ax.set_xlabel("Marcus dimension / $a_0$")
+        ax.set_ylabel("Energy / $E_\mathrm{H}$")
         ax.legend()
+        ax.set_title("Marcus Model")
         if show:
             plt.show()
         return fig, ax
 
     def pretty(self):
-        reorg_en, dG, coupling, *_, d = self.as_wavenums_and_ang_tuple()
+        reorg_en, dG, coupling, _, f, d = self.as_wavenums_and_ang_tuple()
         reorg_en = f"{reorg_en: >5.0f} cm⁻¹"
         dG = f"{dG: >5.0f} cm⁻¹"
         _2coupling = f"{2*coupling: >5.0f} cm⁻¹"
         d = f"{d: >6.4f} Å"
-        return f"MarcusModel(λ={reorg_en}, ΔG={dG}, 2Vab={_2coupling}, d={d})"
+        f = f"{f: >10.1f} cm⁻¹ Å⁻²"
+        return f"MarcusModel(λ={reorg_en}, ΔG={dG}, 2Vab={_2coupling}, d={d}, f={f})"
 
 
 def solve_marcus(R, Vab, dG=None, en_exc_min=None):
