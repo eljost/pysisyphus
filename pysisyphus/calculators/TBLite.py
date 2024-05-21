61b7f59652c07bd39524a23e407a39ea5573f27c
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2024-05-21 16:06:31 +0200

    add/fix: updated PyXTB to use TBLite project

--- pysisyphus/calculators/TBLite.py
+++ pysisyphus/calculators/TBLite.py
@@ -1,7 +1,9 @@
 import numpy as np
+
 try:
-    from xtb.interface import Environment, Param, Calculator as XTBCalculator
-    from xtb.libxtb import VERBOSITY_MINIMAL, VERBOSITY_FULL, VERBOSITY_MUTED
+    from tblite.interface import Calculator as tbCalculator
+
+    can_pyxtb = True
 except ModuleNotFoundError:
     can_pyxtb = False
 
@@ -9,30 +11,30 @@ from pysisyphus.calculators.Calculator import Calculator
 from pysisyphus.elem_data import ATOMIC_NUMBERS
 
 
-class PyXTB(Calculator):
+class TBLite(Calculator):
 
-    def __init__(self, *args, gfn=2, acc=None, verbosity=0, keep_calculator=False, **kwargs):
+    def __init__(
+        self,
+        *args,
+        gfn: int = 2,
+        acc: float = 0.1,
+        verbosity: int = 0,
+        keep_calculator: bool = False,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
 
-        self.env = Environment()
         self.gfn = str(gfn)
-        self.acc = acc
-        avail_verbosities = {
-            0: VERBOSITY_MUTED,
-            1: VERBOSITY_MINIMAL,
-            2: VERBOSITY_FULL,
-        }
-        self.verbosity = avail_verbosities[verbosity]
-        avail_params = {
-            "0": Param.GFN0xTB,
-            "1": Param.GFN1xTB,
-            "2": Param.GFN2xTB,
-            "ff": Param.GFNFF,
-        }
-        self.keep_calculator = keep_calculator
+        self.acc = float(acc)
+        self.verbosity = int(verbosity)
+        self.keep_calculator = bool(keep_calculator)
         self._calculator = None
 
-        self.param = avail_params[self.gfn]
+        self.param = {
+            "1": "GFN1-xTB",
+            "2": "GFN2-xTB",
+        }
+        self.method = self.param[self.gfn]
         self.uhf = self.mult - 1
 
     def get_calculator(self, atoms, coords):
@@ -43,12 +45,15 @@ class PyXTB(Calculator):
 
         # Create new calculator
         numbers = np.array([ATOMIC_NUMBERS[atom.lower()] for atom in atoms])
-        calc = XTBCalculator(
-            self.param, numbers, coords.reshape(-1, 3), charge=self.charge, uhf=self.uhf
+        calc = tbCalculator(
+            self.method,
+            numbers,
+            coords.reshape(-1, 3),
+            charge=self.charge,
+            uhf=self.uhf,
         )
-        calc.set_verbosity(self.verbosity)
-        if self.acc:
-            calc.set_accuracy(self.acc)
+        calc.set("verbosity", self.verbosity)
+        calc.set("accuracy", self.acc)
 
         # Keep calculator, if requested
         if self.keep_calculator and (self._calculator is None):
@@ -59,7 +64,7 @@ class PyXTB(Calculator):
         calc = self.get_calculator(atoms, coords)
         xtb_result = calc.singlepoint(copy=True)
         results = {
-            "energy": xtb_result.get_energy(),
+            "energy": xtb_result.get("energy"),
         }
         return results
 
@@ -67,7 +72,7 @@ class PyXTB(Calculator):
         calc = self.get_calculator(atoms, coords)
         xtb_result = calc.singlepoint(copy=True)
         results = {
-            "energy": xtb_result.get_energy(),
-            "forces": -xtb_result.get_gradient().flatten(),
+            "energy": xtb_result.get("energy"),
+            "forces": -xtb_result.get("gradient").flatten(),
         }
         return results
--- tests/test_tblite/test_tblite.py
+++ tests/test_tblite/test_tblite.py
@@ -1,23 +1,24 @@
 import pytest
 
-from pysisyphus.calculators.PyXTB import PyXTB
+from pysisyphus.calculators.TBLite import TBLite
 from pysisyphus.helpers import geom_loader
 from pysisyphus.testing import using
 
 
-@using("xtb")
-@using("pyxtb")
+@using("tblite")
 @pytest.mark.parametrize(
-    "gfn, ref_energy", [
-        (0, -4.36679493),
+    "gfn, ref_energy",
+    [
+        # GFN0 and GFNFF don't seem to be supported by tblite
+        # (0, -4.36679493),
         (1, -5.76865989),
         (2, -5.07043090),
-        ("ff", -0.327181905),
-    ]
+        # ("ff", -0.327181905),
+    ],
 )
-def test_pyxtb(gfn, ref_energy):
+def test_tblite(gfn, ref_energy):
     geom = geom_loader("lib:h2o.xyz")
-    calc = PyXTB(gfn=gfn)
+    calc = TBLite(gfn=gfn)
     geom.set_calculator(calc)
 
     energy = geom.energy
