7a364b3bd52279e00b929b662f4d58855e3f6073
Johannes Steinmetzer <johannes.steinmetzer@uni-jena.de>
2024-08-26 09:15:08 +0200

    mod: updated limits in FourWellAnaPot plots

--- pysisyphus/calculators/FourWellAnaPot.py
+++ pysisyphus/calculators/FourWellAnaPot.py
@@ -4,16 +4,19 @@ from pysisyphus.calculators.AnaPotBase import AnaPotBase
 # https://doi.org/10.1063/1.1885467
 # Eq. (11)
 
-class FourWellAnaPot(AnaPotBase):
 
-    def __init__(self): 
+class FourWellAnaPot(AnaPotBase):
+    def __init__(self):
         V_str = "x**4 + y**4 - 2*x**2 - 4*y**2 + x*y + 0.3*x + 0.1*y"
-        xlim = (-1.75, 1.75)
-        ylim = (-1.75, 1.75)
+        # xlim = (-1.75, 1.75)
+        # ylim = (-1.75, 1.75)
+        lim = 2.0
+        xlim = (-lim, lim)
+        ylim = (-lim, lim)
         minima = (
             (1.12410175, -1.48527428, 0.0),
             (-0.82190767, -1.36672971, 0.0),
-            (-1.17405609,  1.47708706, 0.0),
+            (-1.17405609, 1.47708706, 0.0),
         )
         super().__init__(V_str=V_str, xlim=xlim, ylim=ylim, minima=minima)
 
